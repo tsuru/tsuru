0975765a0120b4cdf995bb2277805b11f17eb1bf
Flavia Missi <flaviamissi@gmail.com>
2012-03-19 19:17:21 -0300

    Testing with some json throught the request

--- api/services.go
+++ api/services.go
@@ -5,6 +5,19 @@ import (
 	"net/http"
 )
 
+type ServiceBindings struct {
+	ServiceConfigId int
+	AppId int
+	UserId int
+	BindingToken int
+	Name string
+	Configuration string
+	Credentials string
+	BindingOptions string
+	CreatedAt string //fix
+	UpdatedAt string //fix
+}
+
 func CreateService(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "success")
 }
--- api/services_test.go
+++ api/services_test.go
@@ -4,6 +4,8 @@ import (
 	"github.com/timeredbull/tsuru/api"
 	. "launchpad.net/gocheck"
 	"testing"
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 )
@@ -13,7 +15,7 @@ func Test(t *testing.T) { TestingT(t) }
 type ServicesSuite struct{}
 var _ = Suite(&ServicesSuite{})
 
-func (s *ServicesSuite) TestShouldRequestCreate(c *C) {
+func (s *ServicesSuite) TestShouldRequestCreateAndBeSuccess(c *C) {
 	request, err := http.NewRequest("POST", "services/create", nil)
 	recorder := httptest.NewRecorder()
 	c.Assert(err, IsNil)
@@ -22,4 +24,24 @@ func (s *ServicesSuite) TestShouldRequestCreate(c *C) {
 	status := recorder.Code
 
 	c.Assert(200, Equals, status)
+}
+
+func (s *ServicesSuite) TestShouldRequestCreateAndInsertInTheDatabase(c *C) {
+	service_binding := api.ServiceBindings{
+		ServiceConfigId: 1,
+		AppId: 1,
+		UserId: 1,
+		BindingToken: 123,
+		Name: "mysql",
+	}
+	jsonData, err := json.Marshal(service_binding)
+	c.Assert(err, IsNil)
+
+	request, err := http.NewRequest("POST", "services/create", bytes.NewBuffer(jsonData))
+	recorder := httptest.NewRecorder()
+	c.Assert(err, IsNil)
+
+	api.CreateService(recorder, request)
+	body := recorder.Body
+	c.Assert(body.String(), Equals, "success")
 }
