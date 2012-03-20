cd0ec0993dbb07bb0aa36c6fed0d33d64539351c
Andrews Medina <andrewsmedina@gmail.com>
2012-03-20 02:50:42 -0300

    added destroy unit

--- api/unit/unit.go
+++ api/unit/unit.go
@@ -2,11 +2,17 @@ package unit
 
 import "os/exec"
 
-type Unit struct{
+type Unit struct {
 	Type string
+	Name string
 }
 
-func (u *Unit) Create(Name string) error {
-	cmd := exec.Command("juju", "deploy", "--repository=/home/charms", "local:oneiric/" + u.Type, Name)
+func (u *Unit) Create() error {
+	cmd := exec.Command("juju", "deploy", "--repository=/home/charms", "local:oneiric/"+u.Type, u.Name)
+	return cmd.Start()
+}
+
+func (u *Unit) Destroy() error {
+	cmd := exec.Command("juju", "destroy-service", u.Name)
 	return cmd.Start()
 }
