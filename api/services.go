c3a0f383f1a9e7488ff40550f4751e65f14fd66c
Flavia Missi <flaviamissi@gmail.com>
2012-03-19 21:34:28 -0300

    Formatting with gofmt

--- api/services.go
+++ api/services.go
@@ -7,15 +7,15 @@ import (
 
 type ServiceBindings struct {
 	ServiceConfigId int
-	AppId int
-	UserId int
-	BindingToken int
-	Name string
-	Configuration string
-	Credentials string
-	BindingOptions string
-	CreatedAt string //fix
-	UpdatedAt string //fix
+	AppId           int
+	UserId          int
+	BindingToken    int
+	Name            string
+	Configuration   string
+	Credentials     string
+	BindingOptions  string
+	CreatedAt       string //fix
+	UpdatedAt       string //fix
 }
 
 func CreateService(w http.ResponseWriter, r *http.Request) {
--- api/services_test.go
+++ api/services_test.go
@@ -1,18 +1,19 @@
 package api_test
 
 import (
-	"github.com/timeredbull/tsuru/api"
-	. "launchpad.net/gocheck"
-	"testing"
 	"bytes"
 	"encoding/json"
+	"github.com/timeredbull/tsuru/api"
+	. "launchpad.net/gocheck"
 	"net/http"
 	"net/http/httptest"
+	"testing"
 )
 
 func Test(t *testing.T) { TestingT(t) }
 
 type ServicesSuite struct{}
+
 var _ = Suite(&ServicesSuite{})
 
 func (s *ServicesSuite) TestShouldRequestCreateAndBeSuccess(c *C) {
@@ -29,10 +30,10 @@ func (s *ServicesSuite) TestShouldRequestCreateAndBeSuccess(c *C) {
 func (s *ServicesSuite) TestShouldRequestCreateAndInsertInTheDatabase(c *C) {
 	service_binding := api.ServiceBindings{
 		ServiceConfigId: 1,
-		AppId: 1,
-		UserId: 1,
-		BindingToken: 123,
-		Name: "mysql",
+		AppId:           1,
+		UserId:          1,
+		BindingToken:    123,
+		Name:            "mysql",
 	}
 	jsonData, err := json.Marshal(service_binding)
 	c.Assert(err, IsNil)
