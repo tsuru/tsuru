4bce052ec923bd843997b25b021dc13d5a373faa
Flavia Missi <flaviamissi@gmail.com>
2012-03-20 18:54:04 -0300

    Added url to CreateServiceHandler

--- api/webserverd/main.go
+++ api/webserverd/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/services/create", app.CreateServiceHandler)
 	http.HandleFunc("/apps", app.CreateAppHandler)
 	log.Fatal(http.ListenAndServe(":4000", nil))
 }
