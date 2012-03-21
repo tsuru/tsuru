23ed4a3718064b5be28e9483e142795860efca4c
Andrews Medina <andrewsmedina@gmail.com>
2012-03-21 17:25:47 -0300

    imported service in webserverd

--- api/webserverd/main.go
+++ api/webserverd/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"github.com/timeredbull/tsuru/api/app"
+	"github.com/timeredbull/tsuru/api/service"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/services/create", app.CreateServiceHandler)
+	http.HandleFunc("/services/create", service.CreateServiceHandler)
 	http.HandleFunc("/apps", app.CreateAppHandler)
 	log.Fatal(http.ListenAndServe(":4000", nil))
 }
