d618d49e6f954084b93dd1aa48baa25346060e8d
Flavia Missi <flaviamissi@gmail.com>
2012-03-21 13:10:32 -0300

    Using global connection for service test suite

--- api/service/handler_test.go
+++ api/service/handler_test.go
@@ -14,23 +14,26 @@ import (
 
 func Test(t *testing.T) { TestingT(t) }
 
-type ServiceSuite struct{}
+type ServiceSuite struct {
+	db *sql.DB
+}
 
 var _ = Suite(&ServiceSuite{})
-var db, _ = sql.Open("sqlite3", "./tsuru.db")
 
 func (s *ServiceSuite) SetUpSuite(c *C) {
-	_, err := db.Exec("CREATE TABLE 'service_bindings' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'service_config_id' integer, 'app_id' integer, 'user_id' integer, 'binding_token_id' integer, 'name' varchar(255), 'configuration' text, 'credentials' text, 'binding_options' text, 'created_at' datetime, 'updated_at' datetime)")
+	s.db, _ = sql.Open("sqlite3", "./tsuru.db")
+
+	_, err := s.db.Exec("CREATE TABLE 'service_bindings' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'service_config_id' integer, 'app_id' integer, 'user_id' integer, 'binding_token_id' integer, 'name' varchar(255), 'configuration' text, 'credentials' text, 'binding_options' text, 'created_at' datetime, 'updated_at' datetime)")
 	c.Check(err, IsNil)
 }
 
 func (s *ServiceSuite) TearDownSuite(c *C) {
-	db.Close()
 	os.Remove("./tsuru.db")
+	s.db.Close()
 }
 
 func (s *ServiceSuite) TearDownTest(c *C) {
-	db.Exec("DELETE FROM service_bindings")
+	s.db.Exec("DELETE FROM service_bindings")
 }
 
 func (s *ServiceSuite) TestShouldRequestCreateAndBeSuccess(c *C) {
@@ -62,7 +65,7 @@ func (s *ServiceSuite) TestShouldRequestCreateAndInsertInTheDatabase(c *C) {
 	body := recorder.Body
 	c.Assert(body.String(), Equals, "success")
 
-	rows, err := db.Query("SELECT count(*) FROM service_bindings WHERE name = 'my_mysql'")
+	rows, err := s.db.Query("SELECT count(*) FROM service_bindings WHERE name = 'my_mysql'")
 
 	c.Check(err, IsNil)
 	var qtd int
