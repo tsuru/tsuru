b7e740a2ef026962257da89563cdf6913c45e1bb
Andrews Medina <andrewsmedina@gmail.com>
2012-03-21 14:24:27 -0300

    added logs on create and destroy unit

--- api/unit/unit.go
+++ api/unit/unit.go
@@ -1,6 +1,9 @@
 package unit
 
-import "os/exec"
+import (
+	"log"
+	"os/exec"
+)
 
 type Unit struct {
 	Type string
@@ -9,10 +12,12 @@ type Unit struct {
 
 func (u *Unit) Create() error {
 	cmd := exec.Command("juju", "deploy", "--repository=/home/charms", "local:oneiric/"+u.Type, u.Name)
+	log.Printf("deploying %s with name %s", u.Type, u.Name)
 	return cmd.Start()
 }
 
 func (u *Unit) Destroy() error {
 	cmd := exec.Command("juju", "destroy-service", u.Name)
+	log.Printf("destroying %s with name %s", u.Type, u.Name)
 	return cmd.Start()
 }
