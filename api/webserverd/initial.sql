b8bd8683a563b52810e168de24a1f187726dc447
Flavia Missi <flaviamissi@gmail.com>
2012-03-23 00:37:54 -0300

    Creating tables in initial.sql

--- api/webserverd/initial.sql
+++ api/webserverd/initial.sql
@@ -1,1 +1,6 @@
-INSERT INTO service_type (name, charm) VALUES ("Mysql", "mysql");
+CREATE TABLE 'services' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'service_type_id' integer,'name' varchar(255))
+CREATE TABLE 'service_types' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'name' varchar(255), 'charm' varchar(255))
+CREATE TABLE 'service_apps' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'service_id' integer, 'app_id' integer)
+CREATE TABLE 'apps' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'name' varchar(255), 'framework' varchar(255), 'state' varchar(255))
+
+INSERT INTO service_types (name, charm) VALUES ("Mysql", "mysql");
