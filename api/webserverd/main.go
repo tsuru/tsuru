bbd855820f5a632c203bcfcc8a5ef878f765997a
Flavia Missi <flaviamissi@gmail.com>
2012-03-22 22:04:42 -0300

    Added delete, bind and unbind urls to server

--- api/webserverd/main.go
+++ api/webserverd/main.go
@@ -10,10 +10,15 @@ import (
 
 func main() {
 	m := pat.New()
-	m.Get("/services/:name", http.HandlerFunc(service.DeleteHandler))
+
 	m.Post("/services", http.HandlerFunc(service.CreateHandler))
+	m.Get("/services/:name", http.HandlerFunc(service.DeleteHandler))
+	m.Post("/services/bind", http.HandlerFunc(service.BindHandler))
+	m.Post("/services/unbind", http.HandlerFunc(service.BindHandler))
+
 	m.Post("/apps", http.HandlerFunc(app.CreateAppHandler))
 	m.Get("/apps/:name", http.HandlerFunc(app.AppInfo))
 	m.Post("/apps/:name/application", http.HandlerFunc(app.Upload))
+
 	log.Fatal(http.ListenAndServe(":4000", m))
 }
