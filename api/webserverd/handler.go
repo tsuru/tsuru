8869d209a698ccee912f3d50a41a563c1be0261e
Andrews Medina <andrewsmedina@gmail.com>
2012-03-26 01:25:23 -0300

    createad a custom handler to simplifing repetitive error handling

---
+++ api/webserverd/handler.go
@@ -0,0 +1,13 @@
+package webserverd
+
+import (
+	"net/http"
+)
+
+type Handler func(http.ResponseWriter, *http.Request) error
+
+func (fn Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := fn(w, r); err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+}
