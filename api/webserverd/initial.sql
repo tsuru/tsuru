75671b9ca98571413ea09b0d441699efd88d88c5
Francisco Souza <f@souza.cc>
2012-03-29 18:55:35 -0300

    api/webserverd: added user table creation

--- api/webserverd/initial.sql
+++ api/webserverd/initial.sql
@@ -2,5 +2,6 @@ CREATE TABLE 'services' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'servi
 CREATE TABLE 'service_types' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'name' varchar(255), 'charm' varchar(255));
 CREATE TABLE 'service_apps' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'service_id' integer, 'app_id' integer);
 CREATE TABLE 'apps' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'name' varchar(255), 'framework' varchar(255), 'state' varchar(255), 'ip' varchar(100));
+CREATE TABLE 'users' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'email' VARCHAR(255) UNIQUE, 'password' VARCHAR(255));
 
 INSERT INTO service_types (name, charm) VALUES ("Mysql", "mysql");
