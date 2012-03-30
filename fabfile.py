ce4632a2a5252d724b2651317afb55efe546d37e
Andrews Medina <andrewsmedina@gmail.com>
2012-03-30 11:19:50 -0300

    script to deploy tsuru collector and webserverd

---
+++ fabfile.py
@@ -0,0 +1,35 @@
+from fabric.api import run, env, cd
+
+env.user = 'ubuntu'
+env.tsuru_path = '/home/ubuntu/.go/src/github.com/timeredbull/tsuru'
+env.collector_path = '%s/collector' % env.tsuru_path
+env.webserverd_path = '%s/api/webserverd' % env.tsuru_path
+
+def stop():
+    run('killall -9 webserverd')
+    run('killall -9 collector')
+
+
+def update():
+    run('go get -u github.com/timeredbull/tsuru/collector')
+    run('go get -u github.com/timeredbull/tsuru/api/webserverd')
+
+
+def build():
+    with cd(env.collector_path):
+        run("go build -o collector main.go")
+
+    with cd(env.webserverd_path):
+        run("go build -o webserverd main.go")
+
+
+def start():
+    run("nohup %s/collector >& /dev/null < /dev/null &" % env.collector_path, pty=False)
+    run("nohup %s/webserverd >& /dev/null < /dev/null &" % env.webserverd_path, pty=False)
+
+
+def deploy():
+    stop()
+    update()
+    build()
+    start()
