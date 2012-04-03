99f5002f010826a3a3d0ed096d3534708994080b
Francisco Souza <f@souza.cc>
2012-04-02 22:18:48 -0300

    webserver: renamed webserverd package to webserver
    webserverd is redundant.

--- fabfile.py
+++ fabfile.py
@@ -4,30 +4,30 @@ from fabric.api import cd, env, run, settings
 env.user = 'ubuntu'
 env.tsuru_path = '/home/ubuntu/.go/src/github.com/timeredbull/tsuru'
 env.collector_path = '%s/collector' % env.tsuru_path
-env.webserverd_path = '%s/api/webserverd' % env.tsuru_path
+env.webserver_path = '%s/api/webserver' % env.tsuru_path
 
 
 def stop():
     with settings(warn_only=True):
-        run('killall -KILL webserverd collector')
+        run('killall -KILL webserver collector')
 
 
 def update():
     run('go get -u github.com/timeredbull/tsuru/collector')
-    run('go get -u github.com/timeredbull/tsuru/api/webserverd')
+    run('go get -u github.com/timeredbull/tsuru/api/webserver')
 
 
 def build():
     with cd(env.collector_path):
         run("go build -o collector main.go")
 
-    with cd(env.webserverd_path):
-        run("go build -o webserverd main.go")
+    with cd(env.webserver_path):
+        run("go build -o webserver main.go")
 
 
 def start():
     run("nohup %s/collector >& /dev/null < /dev/null &" % env.collector_path, pty=False)
-    run("nohup %s/webserverd >& /dev/null < /dev/null &" % env.webserverd_path, pty=False)
+    run("nohup %s/webserver >& /dev/null < /dev/null &" % env.webserver_path, pty=False)
 
 
 def deploy():
