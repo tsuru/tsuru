7d8e06848ca47068019256e094965686864d22c5
Andrews Medina <andrewsmedina@gmail.com>
2012-04-19 15:20:29 -0300

    removing old imports

--- api/webserver/main.go
+++ api/webserver/main.go
@@ -4,9 +4,7 @@ package main
 
 import (
 	"."
-	"database/sql"
 	"github.com/bmizerany/pat"
-	_ "github.com/mattn/go-sqlite3"
 	"github.com/timeredbull/tsuru/api/app"
 	"github.com/timeredbull/tsuru/api/service"
 	"github.com/timeredbull/tsuru/api/user"
