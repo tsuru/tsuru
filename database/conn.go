e4583838cb6f18c21e99b6e602e29a88d4ca715f
Francisco Souza <f@souza.cc>
2012-04-24 16:40:22 -0300

    database: refactoring database access
    The Storage struct encapsulates collections and indexes on MongoDB.

--- database/conn.go
+++ database/conn.go
@@ -1,9 +1,3 @@
 package database
 
-import (
-	"database/sql"
-	"launchpad.net/mgo"
-)
-
-var Db *sql.DB
-var Mdb *mgo.Database
+var Db *Storage
---
+++ database/storage.go
@@ -0,0 +1,56 @@
+package database
+
+import (
+	"launchpad.net/mgo"
+	"sync"
+)
+
+type Storage struct {
+	collections map[string]*mgo.Collection
+	session     *mgo.Session
+	sync.Mutex
+}
+
+func Open(addr string) (*Storage, error) {
+	session, err := mgo.Dial(addr)
+	if err != nil {
+		return nil, err
+	}
+	s := &Storage{
+		session:     session,
+		collections: make(map[string]*mgo.Collection),
+	}
+	return s, nil
+}
+
+func (s *Storage) Close() {
+	s.session.Close()
+}
+
+func (s *Storage) getCollection(name string) *mgo.Collection {
+	collection, ok := s.collections[name]
+	if !ok {
+		collection = s.session.DB("tsuru").C(name)
+		s.collections[name] = collection
+	}
+	return collection
+}
+
+func (s *Storage) Apps() *mgo.Collection {
+	return s.getCollection("apps")
+}
+
+func (s *Storage) Services() *mgo.Collection {
+	return s.getCollection("services")
+}
+
+func (s *Storage) Units() *mgo.Collection {
+	return s.getCollection("units")
+}
+
+func (s *Storage) Users() *mgo.Collection {
+	emailIndex := mgo.Index{Key: []string{"email"}, Unique: true}
+	c := s.getCollection("users")
+	c.EnsureIndex(emailIndex)
+	return c
+}
