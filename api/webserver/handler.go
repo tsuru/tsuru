ce31ccec5916e7c5a36cc08b9ca1c2b41a9fd7de
Andrews Medina <andrewsmedina@gmail.com>
2012-04-26 17:20:00 -0300

    using log for handlers

--- api/webserver/handler.go
+++ api/webserver/handler.go
@@ -1,6 +1,7 @@
 package webserver
 
 import (
+	"github.com/timeredbull/tsuru/log"
 	"net/http"
 )
 
@@ -9,5 +10,6 @@ type Handler func(http.ResponseWriter, *http.Request) error
 func (fn Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err := fn(w, r); err != nil {
 		http.Error(w, err.Error(), 500)
+		log.Print(err.Error())
 	}
 }
