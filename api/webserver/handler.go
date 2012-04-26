b5c9736e8851e75f1cb3f83eb529787580637f32
Francisco Souza <f@souza.cc>
2012-04-26 15:59:18 -0300

    api/webserver: added AuhtorizationRequestHandler
    This handler should be used for HandlerFunc's that require
    authorization.
    
    For instance, using pat, we are used to add routes this way:
    
        m.Get("/apps", webserver.Handler(apps.AppList))
    
    If the /apps action requires authorization, we should use the
    AuthorizationRequestHandler:
    
        m.Get("/apps", webserver.AuthorizationRequestHandler(apps.AppList))

--- api/webserver/handler.go
+++ api/webserver/handler.go
@@ -1,6 +1,7 @@
 package webserver
 
 import (
+	"github.com/timeredbull/tsuru/api/auth"
 	"net/http"
 )
 
@@ -9,5 +10,18 @@ type Handler func(http.ResponseWriter, *http.Request) error
 func (fn Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err := fn(w, r); err != nil {
 		http.Error(w, err.Error(), 500)
+	}
+}
+
+type AuthorizationRequiredHandler func(http.ResponseWriter, *http.Request) error
+
+func (fn AuthorizationRequiredHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	token := r.Header.Get("Authorization")
+	if token == "" {
+		http.Error(w, "You must provide the Authorization header", http.StatusBadRequest)
+	} else if _, err := auth.CheckToken(token); err != nil {
+		http.Error(w, "Invalid token", http.StatusUnauthorized)
+	} else {
+		Handler(fn).ServeHTTP(w, r)
 	}
 }
--- api/webserver/handler_test.go
+++ api/webserver/handler_test.go
@@ -3,6 +3,8 @@ package webserver
 import (
 	"errors"
 	"fmt"
+	"github.com/timeredbull/tsuru/api/auth"
+	"github.com/timeredbull/tsuru/db"
 	. "launchpad.net/gocheck"
 	"net/http"
 	"net/http/httptest"
@@ -11,7 +13,10 @@ import (
 
 func Test(t *testing.T) { TestingT(t) }
 
-type S struct{}
+type S struct{
+	u *auth.User
+	t *auth.Token
+}
 
 var _ = Suite(&S{})
 
@@ -22,6 +27,18 @@ func errorHandler(w http.ResponseWriter, r *http.Request) error {
 func simpleHandler(w http.ResponseWriter, r *http.Request) error {
 	fmt.Fprint(w, "success")
 	return nil
+}
+
+func (s *S) SetUpSuite(c *C) {
+	db.Session, _ = db.Open("127.0.0.1:27017", "tsuru_handler_test")
+	s.u = &auth.User{Email: "handler@tsuru.globo.com", Password: "123"}
+	s.u.Create()
+	s.t, _ = s.u.CreateToken()
+}
+
+func (s *S) TearDownSuite(c *C) {
+	defer db.Session.Close()
+	db.Session.DropDB()
 }
 
 func (s *S) TestHandlerReturns500WhenInternalHandlerReturnsAnError(c *C) {
@@ -42,4 +59,47 @@ func (s *S) TestHandlerShouldPassAnHandlerWithoutError(c *C) {
 	Handler(simpleHandler).ServeHTTP(recorder, request)
 	c.Assert(recorder.Code, Equals, 200)
 	c.Assert(recorder.Body.String(), Equals, "success")
+}
+
+func (s *S) TestAuthorizationRequiredHandlerShouldReturnBadRequestIfTheAuthorizationHeadIsNotPresent(c *C) {
+	recorder := httptest.NewRecorder()
+	request, err := http.NewRequest("GET", "/apps", nil)
+	c.Assert(err, IsNil)
+
+	AuthorizationRequiredHandler(simpleHandler).ServeHTTP(recorder, request)
+	c.Assert(recorder.Code, Equals, http.StatusBadRequest)
+	c.Assert(recorder.Body.String(), Equals, "You must provide the Authorization header\n")
+}
+
+func (s *S) TestAuthorizationRequiredHandlerShouldReturnUnauthorizedIfTheTokenIsInvalid(c *C) {
+	recorder := httptest.NewRecorder()
+	request, err := http.NewRequest("GET", "/apps", nil)
+	c.Assert(err, IsNil)
+	request.Header.Set("Authorization", "what the token?!")
+
+	AuthorizationRequiredHandler(simpleHandler).ServeHTTP(recorder, request)
+	c.Assert(recorder.Code, Equals, http.StatusUnauthorized)
+	c.Assert(recorder.Body.String(), Equals, "Invalid token\n")
+}
+
+func (s *S) TestAuthorizationRequiredHandlerShouldReturnTheHandlerResultIfTheTokenIsOk(c *C) {
+	recorder := httptest.NewRecorder()
+	request, err := http.NewRequest("GET", "/apps", nil)
+	c.Assert(err, IsNil)
+	request.Header.Set("Authorization", s.t.Token)
+
+	AuthorizationRequiredHandler(simpleHandler).ServeHTTP(recorder, request)
+	c.Assert(recorder.Code, Equals, http.StatusOK)
+	c.Assert(recorder.Body.String(), Equals, "success")
+}
+
+func (s *S) TestAuthorizationRequiredHandlerShouldReturnTheHandlerErrorIfAnyHappen(c *C) {
+	recorder := httptest.NewRecorder()
+	request, err := http.NewRequest("GET", "/apps", nil)
+	c.Assert(err, IsNil)
+	request.Header.Set("Authorization", s.t.Token)
+
+	AuthorizationRequiredHandler(errorHandler).ServeHTTP(recorder, request)
+	c.Assert(recorder.Code, Equals, http.StatusInternalServerError)
+	c.Assert(recorder.Body.String(), Equals, "some error\n")
 }
