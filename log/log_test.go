63fe614495fb9e439b13e2de229427f14d4bc56e
Andrews Medina <andrewsmedina@gmail.com>
2012-04-26 19:04:36 -0300

    added log.Printf

--- log/log_test.go
+++ log/log_test.go
@@ -32,6 +32,14 @@ func (s *S) TestLogPrint(c *C) {
 	c.Assert(buf.String(), Equals, "log anything\n")
 }
 
+func (s *S) TestLogPrintf(c *C) {
+	buf := &bytes.Buffer{}
+	defer buf.Reset()
+	log.Target = stdlog.New(buf, "", 0)
+	log.Printf("log anything %d", 1)
+	c.Assert(buf.String(), Equals, "log anything 1\n")
+}
+
 func (s *S) TestLogFatalWithoutTarget(c *C) {
 	log.Target = nil
 	defer func() {
@@ -54,4 +62,12 @@ func (s *S) TestLogPrintWithoutTarget(c *C) {
 		c.Assert(recover(), IsNil)
 	}()
 	log.Print("log anything")
+}
+
+func (s *S) TestLogPrintfWithoutTarget(c *C) {
+	log.Target = nil
+	defer func() {
+		c.Assert(recover(), IsNil)
+	}()
+	log.Printf("log anything %d", 1)
 }
