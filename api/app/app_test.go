4dc1ccb94d0b30d1be13f49d3d4d288cc67122e2
Francisco Souza <f@souza.cc>
2012-04-27 18:23:52 -0300

    api/app: fix imports and package utilisation in tests

--- api/app/app_test.go
+++ api/app/app_test.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"fmt"
-	"github.com/timeredbull/tsuru/api/app"
 	"github.com/timeredbull/tsuru/db"
 	. "launchpad.net/gocheck"
 	"launchpad.net/mgo"
@@ -37,11 +36,11 @@ func (s *S) TearDownTest(c *C) {
 }
 
 func (s *S) TestNewRepository(c *C) {
-	a := app.App{Name: "foobar"}
-	err := app.NewRepository(&a)
+	a := App{Name: "foobar"}
+	err := NewRepository(&a)
 	c.Assert(err, IsNil)
 
-	repoPath := app.GetRepositoryPath(&a)
+	repoPath := GetRepositoryPath(&a)
 	_, err = os.Open(repoPath) // test if repository dir exists
 	c.Assert(err, IsNil)
 
@@ -53,55 +52,55 @@ func (s *S) TestNewRepository(c *C) {
 }
 
 func (s *S) TestDeleteGitRepository(c *C) {
-	a := &app.App{Name: "someApp"}
-	repoPath := app.GetRepositoryPath(a)
+	a := &App{Name: "someApp"}
+	repoPath := GetRepositoryPath(a)
 
-	err := app.NewRepository(a)
+	err := NewRepository(a)
 	c.Assert(err, IsNil)
 
 	_, err = os.Open(path.Join(repoPath, "config"))
 	c.Assert(err, IsNil)
 
-	app.DeleteRepository(a)
+	DeleteRepository(a)
 	_, err = os.Open(repoPath)
 	c.Assert(err, NotNil)
 }
 
 func (s *S) TestGetRepositoryUrl(c *C) {
-	a := app.App{Name: "foobar"}
-	url := app.GetRepositoryUrl(&a)
+	a := App{Name: "foobar"}
+	url := GetRepositoryUrl(&a)
 	expected := fmt.Sprintf("git@tsuru.plataformas.glb.com:%s.git", a.Name)
 	c.Assert(url, Equals, expected)
 }
 
 func (s *S) TestGetRepositoryName(c *C) {
-	a := app.App{Name: "someApp"}
-	obtained := app.GetRepositoryName(&a)
+	a := App{Name: "someApp"}
+	obtained := GetRepositoryName(&a)
 	expected := fmt.Sprintf("%s.git", a.Name)
 	c.Assert(obtained, Equals, expected)
 }
 
 func (s *S) TestGetRepositoryPath(c *C) {
-	a := app.App{Name: "someApp"}
+	a := App{Name: "someApp"}
 	home := os.Getenv("HOME")
-	obtained := app.GetRepositoryPath(&a)
-	expected := path.Join(home, "../git", app.GetRepositoryName(&a))
+	obtained := GetRepositoryPath(&a)
+	expected := path.Join(home, "../git", GetRepositoryName(&a))
 	c.Assert(obtained, Equals, expected)
 }
 
 func (s *S) TestAll(c *C) {
-	expected := make([]app.App, 0)
-	app1 := app.App{Name: "app1"}
+	expected := make([]App, 0)
+	app1 := App{Name: "app1"}
 	app1.Create()
 	expected = append(expected, app1)
-	app2 := app.App{Name: "app2"}
+	app2 := App{Name: "app2"}
 	app2.Create()
 	expected = append(expected, app2)
-	app3 := app.App{Name: "app3"}
+	app3 := App{Name: "app3"}
 	app3.Create()
 	expected = append(expected, app3)
 
-	appList, err := app.AllApps()
+	appList, err := AllApps()
 	c.Assert(err, IsNil)
 	c.Assert(expected, DeepEquals, appList)
 
@@ -111,11 +110,11 @@ func (s *S) TestAll(c *C) {
 }
 
 func (s *S) TestGet(c *C) {
-	newApp := app.App{Name: "myApp", Framework: "django"}
+	newApp := App{Name: "myApp", Framework: "django"}
 	err := newApp.Create()
 	c.Assert(err, IsNil)
 
-	myApp := app.App{Name: "myApp"}
+	myApp := App{Name: "myApp"}
 	err = myApp.Get()
 	c.Assert(err, IsNil)
 	c.Assert(myApp, Equals, newApp)
@@ -125,7 +124,7 @@ func (s *S) TestGet(c *C) {
 }
 
 func (s *S) TestDestroy(c *C) {
-	a := app.App{
+	a := App{
 		Name:      "aName",
 		Framework: "django",
 	}
@@ -141,20 +140,20 @@ func (s *S) TestDestroy(c *C) {
 }
 
 func (s *S) TestCreate(c *C) {
-	a := app.App{}
+	a := App{}
 	a.Name = "appName"
 	a.Framework = "django"
 
 	err := a.Create()
 	c.Assert(err, IsNil)
 
-	repoPath := app.GetRepositoryPath(&a)
+	repoPath := GetRepositoryPath(&a)
 	_, err = os.Open(repoPath) // test if repository dir exists
 	c.Assert(err, IsNil)
 
 	c.Assert(a.State, Equals, "Pending")
 
-	var retrievedApp app.App
+	var retrievedApp App
 	err = db.Session.Apps().Find(bson.M{"name": a.Name}).One(&retrievedApp)
 	c.Assert(err, IsNil)
 	c.Assert(retrievedApp.Name, Equals, a.Name)
@@ -168,7 +167,7 @@ func (s *S) TestCreate(c *C) {
 }
 
 func (s *S) TestCantCreateTwoAppsWithTheSameName(c *C) {
-	a := app.App{Name: "appName", Framework: "django"}
+	a := App{Name: "appName", Framework: "django"}
 	err := a.Create()
 	c.Assert(err, IsNil)
 
