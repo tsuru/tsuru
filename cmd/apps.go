9fb518913d71e1784d6013f0eeec671e6d92ef21
Andrews Medina <andrewsmedina@gmail.com>
2012-05-07 18:39:44 -0300

    removing prints

--- cmd/apps.go
+++ cmd/apps.go
@@ -26,14 +26,12 @@ func (c AppsCommand) Show(result []byte, context *Context) error {
 	var apps []app.App
 	err := json.Unmarshal(result, &apps)
 	if err != nil {
-		/* fmt.Println(err, string(result)) */
 		context.Stderr.Write([]byte(err.Error()))
 		return err
 	}
 	context.Stdout.Write([]byte("Application - State - Ip\n"))
 	for _, app := range apps {
 		context.Stdout.Write([]byte(fmt.Sprintf("%s - %s - %s\n", app.Name, app.State, app.Ip)))
-		fmt.Println(app)
 	}
 	return nil
 }
