f41889ee70a4b7ca8c75af4d6f5769a286827a51
Flavia Missi <flaviamissi@gmail.com>
2012-05-08 15:47:00 -0300

    Adding project session to gitosis.conf

--- api/repository/gitosis/gitosis.go
+++ api/repository/gitosis/gitosis.go
@@ -1,5 +1,35 @@
 package gitosis
 
+import (
+	"fmt"
+	"path"
+	ini "github.com/kless/goconfig/config"
+	"github.com/timeredbull/tsuru/config"
+	"github.com/timeredbull/tsuru/log"
+)
+
 func AddProject(name string) error {
+	err := config.ReadConfigFile("/etc/tsuru/tsuru.conf")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	repoPath, err := config.GetString("git:gitosis-repo")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	confPath := path.Join(repoPath, "gitosis.conf")
+	c, err := ini.ReadDefault(confPath)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	sName := fmt.Sprintf("group %s", name)
+	ok := c.AddSection(sName)
+	if !ok {
+		log.Panic(fmt.Sprintf(`Could not add section "group %s" in gitosis.conf`, name))
+	}
+	err = c.WriteFile(confPath, 0744, "gitosis configuration file")
 	return nil
 }
--- api/repository/gitosis/gitosis_test.go
+++ api/repository/gitosis/gitosis_test.go
@@ -2,12 +2,24 @@ package gitosis
 
 import (
 	. "launchpad.net/gocheck"
+	"os"
+	"path"
 	/* "github.com/timeredbull/tsuru/config" */
+	"github.com/kless/goconfig/config"
 )
 
 func (s *S) TestAddProject(c *C) {
-	err := AddProject("someProject")
+	pwd := os.Getenv("PWD")
+	err := os.Chdir(path.Join(s.gitRoot, "gitosis-admin"))
+	_, err = os.Create("gitosis.conf")
+	c.Assert(err, IsNil)
+	err = os.Chdir(pwd)
+
+	err = AddProject("someProject")
 	c.Assert(err, IsNil)
 
-	//should write to gitosis.conf
+	conf, err := config.ReadDefault(path.Join(s.gitRoot, "gitosis-admin/gitosis.conf"))
+	c.Assert(err, IsNil)
+	//ensures that project have been added to gitosis.conf
+	c.Assert(conf.HasSection("group someProject"), Equals, true)
 }
