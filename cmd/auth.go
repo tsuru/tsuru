4d26d608703e341f4c7636b9f3b0d7d1737279ed
Andrews Medina <andrewsmedina@gmail.com>
2012-05-11 15:23:47 -0300

    moving users, login, auth cmd  to auth.go

--- cmd/auth.go
+++ cmd/auth.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -49,4 +51,36 @@ func (c *CreateTeamCommand) Run(context *Context, client Doer) error {
 	}
 	io.WriteString(context.Stdout, "OK")
 	return nil
+}
+
+type LoginCommand struct{}
+
+func (c *LoginCommand) Run(context *Context, client Doer) error {
+	email, password := context.Args[0], context.Args[1]
+	b := bytes.NewBufferString(`{"password":"` + password + `"}`)
+	request, err := http.NewRequest("POST", "http://tsuru.plataformas.glb.com:8080/users/"+email+"/tokens", b)
+	if err != nil {
+		return err
+	}
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	result, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+	out := make(map[string]string)
+	err = json.Unmarshal(result, &out)
+	if err != nil {
+		return err
+	}
+	io.WriteString(context.Stdout, "Successfully logged!")
+	WriteToken(out["token"])
+	return nil
+}
+
+func (c *LoginCommand) Info() *Info {
+	return &Info{Name: "login"}
 }
