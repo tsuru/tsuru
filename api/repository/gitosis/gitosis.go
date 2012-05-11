a75d188a8e544e503fd0d7e2a9024da188f25450
Francisco Souza <f@souza.cc>
2012-05-11 19:01:07 -0300

    gitosis: using Read instead of ReadDefault to read gitosis.conf
    ReadDefault uses default parameters of goconf. So,
    
        ReadDefault(filename)
    
    is equivalent to
    
        Read(filename, "# ", ":", true, false)
    
    but we don't want this, we want = as separator ;)

--- api/repository/gitosis/gitosis.go
+++ api/repository/gitosis/gitosis.go
@@ -17,7 +17,7 @@ func AddProject(group, project string) error {
 		log.Print(err)
 		return err
 	}
-	c, err := ini.ReadDefault(confPath)
+	c, err := ini.Read(confPath, ini.DEFAULT_COMMENT, ini.ALTERNATIVE_SEPARATOR, true, true)
 	if err != nil {
 		log.Print(err)
 		return err
@@ -183,5 +183,5 @@ func getConfig() (*ini.Config, error) {
 	if err != nil {
 		return nil, err
 	}
-	return ini.ReadDefault(confPath)
+	return ini.Read(confPath, ini.DEFAULT_COMMENT, ini.ALTERNATIVE_SEPARATOR, true, true)
 }
