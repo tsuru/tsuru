f4ec0a0a9d141f405d32959142ae8a85c0d4675c
Francisco Souza <f@souza.cc>
2012-05-16 18:14:50 -0300

    gitosis: merging packages repository and gitosis
    repository does not need to exist anymore, nor live in api.

--- gitosis/git.go
+++ gitosis/git.go
@@ -38,15 +38,20 @@ func runGit(args ...string) (string, error) {
 func pushToGitosis(cMsg string) error {
 	Lock()
 	defer Unlock()
-	_, err := runGit("add", ".")
+	out, err := runGit("add", ".")
 	if err != nil {
+		log.Print(out)
 		return err
 	}
-	_, err = runGit("commit", "-am", cMsg)
+	out, err = runGit("commit", "-am", cMsg)
 	if err != nil {
+		log.Print(out)
 		return err
 	}
-	_, err = runGit("push", "origin", "master")
+	out, err = runGit("push", "origin", "master")
+	if err != nil {
+		log.Print(out)
+	}
 	return err
 }
 
