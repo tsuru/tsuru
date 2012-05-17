abad2559173cfe9bf6a69c95a6b05c70b66d01c4
Flavia Missi <flaviamissi@gmail.com>
2012-05-17 13:50:10 -0300

    Removed distro from juju deploy command

--- api/unit/unit.go
+++ api/unit/unit.go
@@ -12,7 +12,7 @@ type Unit struct {
 }
 
 func (u *Unit) Create() error {
-	cmd := exec.Command("juju", "deploy", "--repository=/home/charms", "local:oneiric/"+u.Type, u.Name)
+	cmd := exec.Command("juju", "deploy", "--repository=/home/charms", "local:"+u.Type, u.Name)
 	log.Printf("deploying %s with name %s", u.Type, u.Name)
 	return cmd.Start()
 }
