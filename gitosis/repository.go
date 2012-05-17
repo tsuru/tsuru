4b86c0bc667481a1c8d92d672e5741596732239d
Francisco Souza <f@souza.cc>
2012-05-17 11:09:13 -0300

    gitosis: removed log from repository

--- gitosis/repository.go
+++ gitosis/repository.go
@@ -3,7 +3,6 @@ package gitosis
 import (
 	"fmt"
 	"github.com/timeredbull/tsuru/api/unit"
-	"github.com/timeredbull/tsuru/log"
 )
 
 const gitServer = "tsuru.plataformas.glb.com"
@@ -11,12 +10,10 @@ const gitServer = "tsuru.plataformas.glb.com"
 func CloneRepository(appName string) (err error) {
 	u := unit.Unit{Name: appName}
 	cmd := fmt.Sprintf("git clone %s /home/application/%s", GetRepositoryUrl(appName), appName)
-	output, err := u.Command(cmd)
+	_, err = u.Command(cmd)
 	if err != nil {
-		log.Print(err)
 		return
 	}
-	log.Print(output)
 	return
 }
 
