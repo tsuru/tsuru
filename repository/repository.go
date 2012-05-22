88271c71492a0479316f4df1cb380e456988ab87
Flavia Missi <flaviamissi@gmail.com>
2012-05-22 18:31:33 -0300

    Removing wrong dammit quotes

--- repository/repository.go
+++ repository/repository.go
@@ -10,7 +10,7 @@ const gitServer = "tsuru.plataformas.glb.com"
 
 func Clone(app string, machine int) (err error) {
 	u := unit.Unit{Name: app, Machine: machine}
-	cmd := fmt.Sprintf(`"git clone %s /home/application/%s"`, GetReadOnlyUrl(app), app)
+	cmd := fmt.Sprintf("git clone %s /home/application/%s", GetReadOnlyUrl(app), app)
 	output, err := u.Command(cmd)
 	log.Printf("Command output: " + string(output))
 	if err != nil {
