4e2c39083ed97b883eff816b5cae0fc572bf94ec
Francisco Souza <f@souza.cc>
2012-06-20 14:04:32 -0300

    cmd: drop cmd package
    Everything in cmd directory is part of the tsuru command line interface,
    the cmd package is not used anywhere.
    
    This allow us to test if commands are registered (in a easy way).

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,21 @@
-// +build ignore
-
 package main
 
 import (
-	"github.com/timeredbull/tsuru/cmd"
 	"os"
 )
 
 func main() {
-	name := cmd.ExtractProgramName(os.Args[0])
-	manager := cmd.NewManager(name, os.Stdout, os.Stderr)
-	manager.Register(&cmd.Login{})
-	manager.Register(&cmd.Logout{})
-	manager.Register(&cmd.User{})
-	manager.Register(&cmd.App{})
-	manager.Register(&cmd.Key{})
-	manager.Register(&cmd.Team{})
-	manager.Register(&cmd.Target{})
-	manager.Register(&cmd.Env{})
-	manager.Register(&cmd.AppRun{})
+	name := ExtractProgramName(os.Args[0])
+	manager := NewManager(name, os.Stdout, os.Stderr)
+	manager.Register(&Login{})
+	manager.Register(&Logout{})
+	manager.Register(&User{})
+	manager.Register(&App{})
+	manager.Register(&Key{})
+	manager.Register(&Team{})
+	manager.Register(&Target{})
+	manager.Register(&Env{})
+	manager.Register(&AppRun{})
 	//removing the command name from args
 	args := os.Args[1:]
 	manager.Run(args)
