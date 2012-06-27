7b2dd7c0405cebba394d5cb458bde71ffb7eb330
Flavia Missi <flaviamissi@gmail.com>
2012-06-27 13:59:53 -0300

    Created main file for service cli

---
+++ cmd/servicecli/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/timeredbull/tsuru/cmd"
+	"os"
+)
+
+func buildManager(name string) cmd.Manager {
+	m := cmd.NewManager(name, os.Stdout, os.Stderr)
+	m.Register(&cmd.Login{})
+	m.Register(&cmd.Logout{})
+	m.Register(&cmd.User{})
+	m.Register(&cmd.Team{})
+	m.Register(&cmd.Target{})
+	m.Register(&ServiceCreate{})
+	return m
+}
+
+func main() {
+	name := cmd.ExtractProgramName(os.Args[0])
+	manager := buildManager(name)
+	args := os.Args[1:]
+	manager.Run(args)
+}
---
+++ cmd/servicecli/main_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/timeredbull/tsuru/cmd"
+	. "launchpad.net/gocheck"
+)
+
+func (s *S) TestLoginIsRegistered(c *C) {
+	manager := buildManager("tsuru")
+	login, ok := manager.Commands["login"]
+	c.Assert(ok, Equals, true)
+	c.Assert(login, FitsTypeOf, &cmd.Login{})
+}
+
+func (s *S) TestLogoutIsRegistered(c *C) {
+	manager := buildManager("tsuru")
+	logout, ok := manager.Commands["logout"]
+	c.Assert(ok, Equals, true)
+	c.Assert(logout, FitsTypeOf, &cmd.Logout{})
+}
+
+func (s *S) TestUserIsRegistered(c *C) {
+	manager := buildManager("tsuru")
+	user, ok := manager.Commands["user"]
+	c.Assert(ok, Equals, true)
+	c.Assert(user, FitsTypeOf, &cmd.User{})
+}
+
+func (s *S) TestTeamIsRegistered(c *C) {
+	manager := buildManager("tsuru")
+	team, ok := manager.Commands["team"]
+	c.Assert(ok, Equals, true)
+	c.Assert(team, FitsTypeOf, &cmd.Team{})
+}
+
+func (s *S) TestTargetIsRegistered(c *C) {
+	manager := buildManager("tsuru")
+	target, ok := manager.Commands["target"]
+	c.Assert(ok, Equals, true)
+	c.Assert(target, FitsTypeOf, &cmd.Target{})
+}
+
+func (s *S) TestCreateIsRegistered(c *C) {
+	manager := buildManager("tsuru")
+	target, ok := manager.Commands["create"]
+	c.Assert(ok, Equals, true)
+	c.Assert(target, FitsTypeOf, &ServiceCreate{})
+}
