fbdb9a3ff659c2c914f00339d1cee20014e4ff44
Francisco Souza <f@souza.cc>
2012-07-02 18:19:04 -0300

    api/service: added Client with Create method
    The create method is going to be called whenever a new service instance
    is added, and will speak to the endpoint specified.

--- api/service/endpoint.go
+++ api/service/endpoint.go
@@ -1,1 +1,43 @@
 package service
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type Client struct {
+	endpoint string
+}
+
+func (c *Client) issue(path, method string, params map[string][]string) (*http.Response, error) {
+	v := url.Values(params)
+	body := strings.NewReader(v.Encode())
+	url := strings.TrimRight(c.endpoint, "/") + "/" + strings.TrimLeft(path, "/")
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	return http.DefaultClient.Do(req)
+}
+
+func (c *Client) Create(instance *ServiceInstance) (envVars map[string]string, err error) {
+	var resp *http.Response
+	params := map[string][]string{
+		"name": []string{instance.Name},
+	}
+	resp, err = c.issue("/resources", "POST", params)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	var body []byte
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(body, &envVars)
+	return
+}
--- api/service/endpoint_test.go
+++ api/service/endpoint_test.go
@@ -1,8 +1,54 @@
 package service
 
 import (
+	"io/ioutil"
 	. "launchpad.net/gocheck"
+	"net/http"
+	"net/http/httptest"
 )
 
-func (s *S) TestCallApiOnServiceInstanceCreation(c *C) {
+type TestHandler struct {
+	body   []byte
+	method string
+	url    string
+}
+
+func (h *TestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	content := `{"MYSQL_DATABASE_NAME": "CHICO", "MYSQL_HOST": "localhost", "MYSQL_PORT": "3306"}`
+	h.method = r.Method
+	h.url = r.URL.String()
+	h.body, _ = ioutil.ReadAll(r.Body)
+	w.Write([]byte(content))
+}
+
+func (s *S) TestCreateShouldSendTheNameOfTheResourceToTheEndpoint(c *C) {
+	h := TestHandler{}
+	ts := httptest.NewServer(&h)
+	defer ts.Close()
+	instance := ServiceInstance{Name: "my-redis", ServiceName: "redis"}
+	client := &Client{endpoint: ts.URL}
+	_, err := client.Create(&instance)
+	c.Assert(err, IsNil)
+	expectedBody := "name=my-redis"
+	c.Assert(err, IsNil)
+	c.Assert(string(h.body), Equals, expectedBody)
+	expectedUrl := "/resources"
+	c.Assert(h.url, Equals, expectedUrl)
+	c.Assert(h.method, Equals, "POST")
+}
+
+func (s *S) TestCreateShouldReturnTheMapWithTheEnvironmentVariables(c *C) {
+	expected := map[string]string{
+		"MYSQL_DATABASE_NAME": "CHICO",
+		"MYSQL_HOST": "localhost",
+		"MYSQL_PORT": "3306",
+	}
+	h := TestHandler{}
+	ts := httptest.NewServer(&h)
+	defer ts.Close()
+	instance := ServiceInstance{Name: "your-redis", ServiceName: "redis"}
+	client := &Client{endpoint: ts.URL}
+	env, err := client.Create(&instance)
+	c.Assert(err, IsNil)
+	c.Assert(env, DeepEquals, expected)
 }
