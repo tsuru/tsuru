370b2e6dc5148e1b45985c309645bf41a3a354c9
Flavia Missi <flaviamissi@gmail.com>
2012-07-05 10:46:51 -0300

    Starting ec2 test server in collector tests

--- collector/suite_test.go
+++ collector/suite_test.go
@@ -13,12 +13,15 @@ func Test(t *testing.T) { TestingT(t) }
 type S struct {
 	session *mgo.Session
 	tmpdir  string
+	ec2Srv  *ec2test.Server
 }
 
 var _ = Suite(&S{})
 
 func (s *S) SetUpSuite(c *C) {
 	var err error
+	s.ec2Srv, err = ec2test.NewServer()
+	c.Assert(err, IsNil)
 	s.tmpdir, err = commandmocker.Add("juju", "")
 	c.Assert(err, IsNil)
 	db.Session, err = db.Open("127.0.0.1:27017", "tsuru_collector_test")
@@ -28,6 +31,7 @@ func (s *S) SetUpSuite(c *C) {
 func (s *S) TearDownSuite(c *C) {
 	defer commandmocker.Remove(s.tmpdir)
 	defer db.Session.Close()
+	s.ec2Srv.Quit()
 	db.Session.Apps().Database.DropDatabase()
 }
 
@@ -36,4 +40,10 @@ func (s *S) TearDownTest(c *C) {
 	c.Assert(err, IsNil)
 	_, err = db.Session.Units().RemoveAll(nil)
 	c.Assert(err, IsNil)
+}
+
+func (s *S) reconfEc2Srv(c *C) {
+	region := aws.Region{EC2Endpoint: s.ec2Srv.URL()}
+	auth := aws.Auth{AccessKey: "blaa", SecretKey: "blee"}
+	tsuruEC2.EC2 = ec2.New(auth, region)
 }
