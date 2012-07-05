bc3e669f569c49afcefc565c0a09460cbdf02105
Flavia Missi <flaviamissi@gmail.com>
2012-07-05 17:14:30 -0300

    Created update method for ec2 collector

--- collector/ec2_collector.go
+++ collector/ec2_collector.go
@@ -5,10 +5,13 @@ import (
 	"github.com/timeredbull/tsuru/db"
 	tEC2 "github.com/timeredbull/tsuru/ec2"
 	"github.com/timeredbull/tsuru/log"
+	"labix.org/v2/mgo/bson"
 	"launchpad.net/goamz/ec2"
 )
 
-func CollectEc2() ([]ec2.Instance, error) {
+type Ec2Collector struct{}
+
+func (ec *Ec2Collector) Collect() ([]ec2.Instance, error) {
 	log.Print("Collecting ec2 instances state...")
 	var srvInsts []service.ServiceInstance
 	db.Session.ServiceInstances().Find(nil).All(&srvInsts)
@@ -29,4 +32,29 @@ func CollectEc2() ([]ec2.Instance, error) {
 		insts = append(insts, rsvts.Instances...)
 	}
 	return insts, nil
+}
+
+func (ec *Ec2Collector) Update(insts []ec2.Instance) error {
+	var srvInsts []service.ServiceInstance
+	q := bson.M{"instance": bson.M{"$in": instancesIds(insts)}}
+	db.Session.ServiceInstances().Find(q).All(&srvInsts)
+	for _, inst := range insts {
+		for _, srvInst := range srvInsts {
+			if srvInst.Instance == inst.InstanceId {
+				srvInst.State = inst.State.Name
+				srvInst.Host = inst.DNSName
+				q = bson.M{"_id": srvInst.Name, "service_name": srvInst.ServiceName}
+				db.Session.ServiceInstances().Update(q, srvInst)
+			}
+		}
+	}
+	return nil
+}
+
+func instancesIds(insts []ec2.Instance) []string {
+	instIds := make([]string, len(insts))
+	for i, inst := range insts {
+		instIds[i] = inst.InstanceId
+	}
+	return instIds
 }
--- collector/ec2_collector_test.go
+++ collector/ec2_collector_test.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bytes"
 	"github.com/timeredbull/tsuru/api/service"
+	"github.com/timeredbull/tsuru/db"
 	tec2 "github.com/timeredbull/tsuru/ec2"
 	"github.com/timeredbull/tsuru/log"
+	"labix.org/v2/mgo/bson"
 	"launchpad.net/goamz/ec2"
 	. "launchpad.net/gocheck"
 	stdlog "log"
@@ -19,12 +21,17 @@ func (s *S) TestCollectShouldRetrieveCreatedInstances(c *C) {
 	si2.Create()
 	defer si.Delete()
 	defer si2.Delete()
-	instances, err := CollectEc2()
+	ec := Ec2Collector{}
+	instances, err := ec.Collect()
 	c.Assert(err, IsNil)
 	c.Assert(len(instances), Equals, 2)
 	c.Assert(instances[0].ImageId, Equals, "ami-0000007")
 	c.Assert(instances[1].ImageId, Equals, "ami-0000007")
 	_, err = tec2.EC2.TerminateInstances(s.instances)
+	if err != nil {
+		c.Fail()
+	}
+	_, err = tec2.EC2.DeleteSecurityGroup(s.secGroup)
 	if err != nil {
 		c.Fail()
 	}
@@ -34,10 +41,43 @@ func (s *S) TestCollectShouldSkipIfNoServiceInstancesAreFound(c *C) {
 	w := bytes.NewBuffer([]byte{})
 	l := stdlog.New(w, "", stdlog.LstdFlags)
 	log.Target = l
-	instances, err := CollectEc2()
+	ec := Ec2Collector{}
+	instances, err := ec.Collect()
 	c.Assert(err, IsNil)
 	c.Assert(instances, FitsTypeOf, []ec2.Instance{})
 	c.Assert(len(instances), Equals, 0)
 	logStr := strings.Replace(w.String(), "\n", "", -1)
 	c.Assert(logStr, Matches, ".*no service instances found for collect. Skipping....*")
+}
+
+func (s *S) TestUpdateShouldUpdateInstancesStateAndHostInDatabase(c *C) {
+	s.createTestInstances(c)
+	si := service.ServiceInstance{Name: "instance 1", ServiceName: "mysql", Instance: s.instances[0]}
+	si.Create()
+	si2 := service.ServiceInstance{Name: "instance 2", ServiceName: "mysql", Instance: s.instances[1]}
+	si2.Create()
+	defer si.Delete()
+	defer si2.Delete()
+	ec := Ec2Collector{}
+	instances, err := ec.Collect()
+	c.Assert(err, IsNil)
+	err = ec.Update(instances)
+	c.Assert(err, IsNil)
+	db.Session.ServiceInstances().Find(bson.M{"_id": si.Name, "service_name": si.ServiceName}).One(&si)
+	c.Assert(si.Host, Not(Equals), "")
+	c.Assert(si.State, Not(Equals), "creating")
+	_, err = tec2.EC2.TerminateInstances(s.instances)
+	if err != nil {
+		c.Fail()
+	}
+	_, err = tec2.EC2.DeleteSecurityGroup(s.secGroup)
+	if err != nil {
+		c.Fail()
+	}
+}
+
+func (s *S) TestInstanceIdsReturnsInstancesIdsOnly(c *C) {
+	inst := ec2.Instance{InstanceId: "i-0"}
+	instIds := instancesIds([]ec2.Instance{inst})
+	c.Assert(instIds, DeepEquals, []string{"i-0"})
 }
