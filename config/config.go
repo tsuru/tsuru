91b0522a2ebd6f69bb5e32994226f8e771801233
Francisco Souza <f@souza.cc>
2012-07-06 13:52:35 -0300

    config: some docs for the package

--- config/config.go
+++ config/config.go
@@ -1,3 +1,5 @@
+// Package config provide configuration facilities, handling configuration
+// files in yaml format.
 package config
 
 import (
@@ -10,10 +12,20 @@ import (
 
 var Configs map[interface{}]interface{}
 
+// ReadConfigBytes receives a slice of bytes and builds the internal
+// configuration map.
+//
+// If the given slice is not a valid yaml file, ReadConfigBytes returns a
+// non-nil error.
 func ReadConfigBytes(data []byte) error {
 	return goyaml.Unmarshal(data, &Configs)
 }
 
+// ReadConfigFile reads the content of a file and calls ReadConfigBytes to
+// build the internal configuration map.
+//
+// It returns error if it can not read the given file or if the file contents
+// is not valid yaml.
 func ReadConfigFile(filePath string) error {
 	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
@@ -22,6 +34,18 @@ func ReadConfigFile(filePath string) error {
 	return ReadConfigBytes(data)
 }
 
+// Get returns the value for the given key, or an eror if the key is undefined.
+//
+// The key is composed by all the key names separated by :, in case of nested
+// keys. For example, suppose we have the following configuration yaml:
+//
+//   databases:
+//     mysql:
+//       host: localhost
+//       port: 3306
+//
+// The key "databases:mysql:host" would return "localhost", while the key
+// "port" would return an error.
 func Get(key string) (interface{}, error) {
 	keys := strings.Split(key, ":")
 	conf, ok := Configs[keys[0]]
@@ -37,6 +61,10 @@ func Get(key string) (interface{}, error) {
 	return conf, nil
 }
 
+// GetString works like Get, but doing a string type assertion before return
+// the value.
+//
+// It returns error if the key is undefined or if it is not a string.
 func GetString(key string) (string, error) {
 	value, err := Get(key)
 	if err != nil {
