9d668dfc3f3529b0279c21ab13dd30adc5389e80
Francisco Souza <f@souza.cc>
2012-07-06 16:00:37 -0300

    errors: added basic docs to the package

--- errors/http.go
+++ errors/http.go
@@ -1,7 +1,14 @@
+// Package errors provides facilities with error handling.
 package errors
 
+// Http represents an HTTP error. It implements the error interface.
+//
+// Each HTTP error has a Code and a message explaining what went wrong.
 type Http struct {
+	// Status code.
 	Code    int
+
+	// Message explaining what went wrong.
 	Message string
 }
 
