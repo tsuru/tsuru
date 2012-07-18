afddf17182639f935c7a802a40dc0d4eea0b1a7f
Francisco Souza <f@souza.cc>
2012-07-18 11:21:42 -0300

    api/app: using app's suite in bind_test.go

--- api/app/bind_test.go
+++ api/app/bind_test.go
@@ -1,56 +1,29 @@
-package app_test
+package app
 
 import (
-	"github.com/timeredbull/tsuru/api/app"
 	"github.com/timeredbull/tsuru/api/bind"
 	"github.com/timeredbull/tsuru/api/service"
 	"github.com/timeredbull/tsuru/db"
 	"labix.org/v2/mgo/bson"
-	"reflect"
-	"testing"
+	. "launchpad.net/gocheck"
 )
 
-func TestAppIsABinderApp(t *testing.T) {
-	var a bind.App
-	iface := reflect.ValueOf(&a)
-	obj := reflect.ValueOf(&app.App{})
-	if !obj.Type().Implements(iface.Elem().Type()) {
-		t.Errorf("app.App should implement bind.App")
-	}
+func (s *S) TestAppIsABinderApp(c *C) {
+	var app bind.App
+	c.Assert(&App{}, Implements, &app)
 }
 
-func TestDestroyShouldUnbindAppFromInstance(t *testing.T) {
-	db.Session, _ = db.Open("127.0.0.1:27017", "tsuru_app_bind_test")
-	defer func() {
-		db.Session.Apps().Database.DropDatabase()
-		db.Session.Close()
-	}()
-	instance := service.ServiceInstance{
-		Name: "MyInstance",
-		Apps: []string{"myApp"},
-	}
+func (s *S) TestDestroyShouldUnbindAppFromInstance(c *C) {
+	instance := service.ServiceInstance{Name: "MyInstance", Apps: []string{"myApp"}}
 	err := instance.Create()
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
+	c.Assert(err, IsNil)
 	defer db.Session.ServiceInstances().Remove(bson.M{"_id": instance.Name})
-	a := app.App{
-		Name: "myApp",
-	}
+	a := App{Name: "myApp"}
 	err = a.Create()
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
+	c.Assert(err, IsNil)
 	defer db.Session.Apps().Remove(bson.M{"name": a.Name})
 	err = a.Destroy()
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
+	c.Assert(err, IsNil)
 	n, _ := db.Session.ServiceInstances().Find(bson.M{"apps": bson.M{"$in": []string{a.Name}}}).Count()
-	if n != 0 {
-		t.Errorf("Should unbind apps when destroying them, but did not.\nExpected 0 apps to be binded to the instance %s, got %d.", instance.Name, n)
-	}
+	c.Assert(n, Equals, 0)
 }
