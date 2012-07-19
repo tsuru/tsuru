1b01557d81078c48b9ef2e8acc425d3785fd589d
Francisco Souza <f@souza.cc>
2012-07-19 17:23:30 -0300

    cmd/appcli: gofmt -w .

--- cmd/appcli/apps.go
+++ cmd/appcli/apps.go
@@ -150,10 +150,10 @@ type AppCreate struct{}
 func (c *AppCreate) Run(context *cmd.Context, client cmd.Doer) error {
 	appName := context.Args[0]
 	framework := "django"
-	if (len(context.Args) > 1) {
+	if len(context.Args) > 1 {
 		framework = context.Args[1]
 	}
-	
+
 	b := bytes.NewBufferString(fmt.Sprintf(`{"name":"%s", "framework":"%s"}`, appName, framework))
 	request, err := http.NewRequest("POST", cmd.GetUrl("/apps"), b)
 	request.Header.Set("Content-Type", "application/json")
