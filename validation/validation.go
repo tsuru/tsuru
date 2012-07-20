0d36b387eeadbec20596868361cc56f255ddb463
Francisco Souza <f@souza.cc>
2012-07-20 11:38:48 -0300

    validation: added package
    This package will be used for data validation. It provides now two
    functions for email validation and length validation. It can be used by
    server and cmd.

---
+++ validation/validation.go
@@ -0,0 +1,31 @@
+// Package validation provide utilities functions for data validation.
+package validation
+
+import (
+	"regexp"
+)
+
+// ValidateEmail checks whether a given email is valid.
+func ValidateEmail(email string) bool {
+	re := regexp.MustCompile(`^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$`)
+	return re.MatchString(email)
+}
+
+// ValidateLength checks whether the given data match the given rules.
+//
+// It checks if the value has more or equal `min` chars and less or equal `max`
+// chars. If you don't want to check both, just pass a zero value:
+//
+//     ValidateLength(value, 0, 100) // Checks if value has at most 100 characters
+//     ValidateLength(value, 100, 0) // Checks if value has at least 100 characters
+//     ValidateLength(value, 20, 100) // Checks if value has at least 20 characters and at most 100 characters
+func ValidateLength(value string, min, max int) bool {
+	l := len(value)
+	if min > 0 && l < min {
+		return false
+	}
+	if max > 0 && l > max {
+		return false
+	}
+	return true
+}
---
+++ validation/validation_test.go
@@ -0,0 +1,51 @@
+package validation
+
+import (
+	. "launchpad.net/gocheck"
+	"testing"
+)
+
+type S struct{}
+
+var _ = Suite(&S{})
+
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+func (s *S) TestValidateEmail(c *C) {
+	var data = []struct {
+		input    string
+		expected bool
+	}{
+		{"gopher.golang@corp.globo.com", true},
+		{"gopher@corp.globo.com", true},
+		{"gopher@souza.cc", true},
+		{"gopher@acm.org", true},
+		{"gopher@golang.com.br", true},
+		{"gopher@gmail.com", true},
+		{"gopher@live.com", true},
+		{"invalid-gopher", false},
+		{"invalid@validate.c", false},
+		{"invalid@validate", false},
+	}
+	for _, d := range data {
+		c.Assert(ValidateEmail(d.input), Equals, d.expected)
+	}
+}
+
+func (s *S) TestValidateLength(c *C) {
+	var data = []struct {
+		input    string
+		min      int
+		max      int
+		expected bool
+	}{
+		{"abc", 10, -1, false},
+		{"abc", -1, -1, true},
+		{"gopher", -1, 3, false},
+	}
+	for _, d := range data {
+		c.Assert(ValidateLength(d.input, d.min, d.max), Equals, d.expected)
+	}
+}
