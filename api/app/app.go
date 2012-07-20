1faabef956de2724a9c2b85f36982cb3b8aa3d3d
Bruno Torres <brunolct@live.com>
2012-07-20 11:07:26 -0300

    Fixed small typo from last commit

--- api/app/app.go
+++ api/app/app.go
@@ -73,7 +73,7 @@ func (a *App) Create() error {
 	out, err := cmd.CombinedOutput()
 	a.Log(string(out))
 	if err != nil {
-		return errors.new(out)
+		return errors.New(out)
 	}
 	a.Log(fmt.Sprintf("app %s successfully created", a.Name))
 	return nil
