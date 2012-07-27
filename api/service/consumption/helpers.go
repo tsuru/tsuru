2c0f177093c2fced153888ba338e0eb753afd19e
Flavia Missi <flaviamissi@gmail.com>
2012-07-27 15:59:17 -0300

    api.service.consumption: refactoring helpers

--- api/service/consumption/helpers.go
+++ api/service/consumption/helpers.go
@@ -7,11 +7,19 @@ import (
 	"net/http"
 )
 
-func GetServiceOrError(name string, u *auth.User) (service.Service, error) {
+func GetServiceOr404(name string) (service.Service, error) {
 	s := service.Service{Name: name}
 	err := s.Get()
 	if err != nil {
 		return s, &errors.Http{Code: http.StatusNotFound, Message: "Service not found"}
+	}
+	return s, nil
+}
+
+func GetServiceOrError(name string, u *auth.User) (service.Service, error) {
+	s, err := GetServiceOr404(name)
+	if err != nil {
+		return s, nil
 	}
 	if !auth.CheckUserAccess(s.Teams, u) {
 		msg := "This user does not have access to this service"
--- api/service/consumption/helpers_test.go
+++ api/service/consumption/helpers_test.go
@@ -13,3 +13,16 @@ func (s *S) TestGetServiceOrError(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(rSrv.Name, Equals, srv.Name)
 }
+
+func (s *S) TestGetServiceOrErrorShouldReturnErrorWhenUserHaveNoAccessToService(c *C) {
+	srv := Service{Name: "foo"}
+	err := srv.Create()
+	c.Assert(err, IsNil)
+	_, err = GetServiceOrError("foo", s.user)
+	c.Assert(err, ErrorMatches, "^This user does not have access to this service$")
+}
+
+func (s *S) TestGetServiceOr404(c *C) {
+	_, err := GetServiceOr404("foo")
+	c.Assert(err, ErrorMatches, "^Service not found$")
+}
