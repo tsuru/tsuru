66d8389f656a82f45f63f0c4be089c2179eb8cf9
Francisco Souza <f@souza.cc>
2012-08-14 13:59:04 -0300

    repository: improving package architecture
    Some improvements:
    
        * using an interface "Unit" to remove dependency from api/unit
        package
        * using a fake unit in tests, removing dependency from commandmocker
        in tests
        * improved tests in general
    
    The goal of this initial refactoring is to delete the api/unit package
    (merging it with api/app).

--- repository/repository_test.go
+++ repository/repository_test.go
@@ -1,27 +1,85 @@
 package repository
 
 import (
-	"github.com/timeredbull/commandmocker"
+	"errors"
+	"fmt"
 	. "launchpad.net/gocheck"
+	"strings"
 )
 
+type FakeUnit struct {
+	name     string
+	commands []string
+}
+
+func (u *FakeUnit) RanCommand(cmd string) bool {
+	for _, c := range u.commands {
+		if c == cmd {
+			return true
+		}
+	}
+	return false
+}
+
+func (u *FakeUnit) GetName() string {
+	return u.name
+}
+
+func (u *FakeUnit) Command(cmd string) ([]byte, error) {
+	u.commands = append(u.commands, cmd)
+	return []byte("success"), nil
+}
+
+type FailingCloneUnit struct {
+	FakeUnit
+}
+
+func (u *FailingCloneUnit) Command(cmd string) ([]byte, error) {
+	if strings.HasPrefix(cmd, "git clone") {
+		u.commands = append(u.commands, cmd)
+		return nil, errors.New("Failed to clone repository, it already exists!")
+	}
+	return u.FakeUnit.Command(cmd)
+}
+
 func (s *S) TestCloneRepository(c *C) {
-	_, err := Clone("barfoo", 1)
+	u := FakeUnit{name: "my-unit"}
+	out, err := Clone(&u)
 	c.Assert(err, IsNil)
+	c.Assert(string(out), Equals, "success")
+	expectedCommand := fmt.Sprintf("git clone %s /home/application/current --depth 1", GetReadOnlyUrl(u.GetName()))
+	c.Assert(u.RanCommand(expectedCommand), Equals, true)
 }
 
 func (s *S) TestPullRepository(c *C) {
-	dir, err := commandmocker.Add("juju", "$*")
+	u := FakeUnit{name: "your-unit"}
+	out, err := Pull(&u)
 	c.Assert(err, IsNil)
-	defer commandmocker.Remove(dir)
-	out, err := Pull("barfoo", 1)
-	c.Assert(err, IsNil)
-	c.Assert(string(out), Matches, ".*cd /home/application/current && git pull origin master$")
+	c.Assert(string(out), Equals, "success")
+	expectedCommand := fmt.Sprintf("cd /home/application/current && git pull origin master")
+	c.Assert(u.RanCommand(expectedCommand), Equals, true)
 }
 
-func (s *S) TestCloneOrPullRepository(c *C) {
-	_, err := CloneOrPull("someapp", 2)
+func (s *S) TestCloneOrPullRepositoryRunsClone(c *C) {
+	u := FakeUnit{name: "my-unit"}
+	out, err := CloneOrPull(&u)
 	c.Assert(err, IsNil)
+	c.Assert(string(out), Equals, "success")
+	clone := fmt.Sprintf("git clone %s /home/application/current --depth 1", GetReadOnlyUrl(u.GetName()))
+	pull := fmt.Sprintf("cd /home/application/current && git pull origin master")
+	c.Assert(u.RanCommand(clone), Equals, true)
+	c.Assert(u.RanCommand(pull), Equals, false)
+}
+
+func (s *S) TestCloneOrPullRepositoryRunsPullIfCloneFail(c *C) {
+	u := FailingCloneUnit{FakeUnit{name: "my-unit"}}
+	out, err := CloneOrPull(&u)
+	c.Assert(err, IsNil)
+	c.Assert(string(out), Equals, "success")
+	clone := fmt.Sprintf("git clone %s /home/application/current --depth 1", GetReadOnlyUrl(u.GetName()))
+	pull := fmt.Sprintf("cd /home/application/current && git pull origin master")
+	c.Assert(u.RanCommand(clone), Equals, true)
+	c.Assert(u.RanCommand(pull), Equals, true)
 }
 
 func (s *S) TestGetRepositoryUrl(c *C) {
