1be8f498179acaf09202e54dc69de04f3d1061c0
Francisco Souza <f@souza.cc>
2012-08-15 17:50:22 -0300

    cmd/appcli: make AddKey and RemoveKey support other keys format
    tsuru user can now specify the key file.
    
    Closes #92.

--- cmd/appcli/key.go
+++ cmd/appcli/key.go
@@ -25,9 +25,18 @@ func (r *keyReader) fs() fs.Fs {
 	return r.fsystem
 }
 
-func (r *keyReader) readKey() (string, error) {
+func getKeyPath(args []string) (string, error) {
+	if len(args) > 0 {
+		return args[0], nil
+	}
 	user, err := user.Current()
-	keyPath := user.HomeDir + "/.ssh/id_rsa.pub"
+	if err != nil {
+		return "", err
+	}
+	return user.HomeDir + "/.ssh/id_rsa.pub", nil
+}
+
+func (r *keyReader) readKey(keyPath string) (string, error) {
 	f, err := r.fs().Open(keyPath)
 	if err != nil {
 		return "", err
@@ -62,13 +71,17 @@ type RemoveKey struct {
 func (c *RemoveKey) Info() *cmd.Info {
 	return &cmd.Info{
 		Name:  "remove",
-		Usage: "key remove",
-		Desc:  "remove your public key ($HOME/.id_rsa.pub).",
+		Usage: "key remove [path/to/key/file.pub]",
+		Desc:  "remove your public key ($HOME/.id_rsa.pub by default).",
 	}
 }
 
 func (c *RemoveKey) Run(context *cmd.Context, client cmd.Doer) error {
-	key, err := c.readKey()
+	keyPath, err := getKeyPath(context.Args)
+	if err != nil {
+		return err
+	}
+	key, err := c.readKey(keyPath)
 	if os.IsNotExist(err) {
 		io.WriteString(context.Stderr, "You don't have a public key\n")
 		io.WriteString(context.Stderr, "To generate a key use 'ssh-keygen' command\n")
@@ -94,13 +107,17 @@ type AddKeyCommand struct {
 func (c *AddKeyCommand) Info() *cmd.Info {
 	return &cmd.Info{
 		Name:  "add",
-		Usage: "key add",
-		Desc:  "add your public key ($HOME/.ssh/id_rsa.pub).",
+		Usage: "key add [path/to/key/file.pub]",
+		Desc:  "add your public key ($HOME/.ssh/id_rsa.pub by default).",
 	}
 }
 
 func (c *AddKeyCommand) Run(context *cmd.Context, client cmd.Doer) error {
-	key, err := c.readKey()
+	keyPath, err := getKeyPath(context.Args)
+	if err != nil {
+		return err
+	}
+	key, err := c.readKey(keyPath)
 	if os.IsNotExist(err) {
 		io.WriteString(context.Stderr, "You don't have a public key\n")
 		io.WriteString(context.Stderr, "To generate a key use 'ssh-keygen' command\n")
--- cmd/appcli/key_test.go
+++ cmd/appcli/key_test.go
@@ -29,6 +29,26 @@ func (s *S) TestAddKey(c *C) {
 	c.Assert(fs.HasAction("open "+p), Equals, true)
 }
 
+func (s *S) TestAddKeySpecifyingKeyFile(c *C) {
+	u, err := user.Current()
+	c.Assert(err, IsNil)
+	p := path.Join(u.HomeDir, ".ssh", "id_dsa.pub")
+	expected := "Key successfully added!\n"
+	context := cmd.Context{
+		Cmds:   []string{},
+		Args:   []string{p},
+		Stdout: manager.Stdout,
+		Stderr: manager.Stderr,
+	}
+	client := cmd.NewClient(&http.Client{Transport: &transport{msg: "success", status: http.StatusOK}})
+	fs := RecordingFs{fileContent: "user-key"}
+	command := AddKeyCommand{keyReader{fsystem: &fs}}
+	err = command.Run(&context, client)
+	c.Assert(err, IsNil)
+	c.Assert(manager.Stdout.(*bytes.Buffer).String(), Equals, expected)
+	c.Assert(fs.HasAction("open "+p), Equals, true)
+}
+
 func (s *S) TestAddKeyReturnErrorIfTheKeyDoesNotExist(c *C) {
 	context := cmd.Context{
 		Cmds:   []string{},
@@ -43,6 +63,15 @@ func (s *S) TestAddKeyReturnErrorIfTheKeyDoesNotExist(c *C) {
 	c.Assert(err.Error(), Equals, "You need to have a public rsa key")
 }
 
+func (s *S) TestInfoAddKey(c *C) {
+	expected := &cmd.Info{
+		Name:  "add",
+		Usage: "key add [path/to/key/file.pub]",
+		Desc:  "add your public key ($HOME/.ssh/id_rsa.pub by default).",
+	}
+	c.Assert((&AddKeyCommand{}).Info(), DeepEquals, expected)
+}
+
 func (s *S) TestRemoveKey(c *C) {
 	u, err := user.Current()
 	c.Assert(err, IsNil)
@@ -51,6 +80,26 @@ func (s *S) TestRemoveKey(c *C) {
 	context := cmd.Context{
 		Cmds:   []string{},
 		Args:   []string{},
+		Stdout: manager.Stdout,
+		Stderr: manager.Stderr,
+	}
+	client := cmd.NewClient(&http.Client{Transport: &transport{msg: "success", status: http.StatusOK}})
+	fs := RecordingFs{fileContent: "user-key"}
+	command := RemoveKey{keyReader{fsystem: &fs}}
+	err = command.Run(&context, client)
+	c.Assert(err, IsNil)
+	c.Assert(manager.Stdout.(*bytes.Buffer).String(), Equals, expected)
+	c.Assert(fs.HasAction("open "+p), Equals, true)
+}
+
+func (s *S) TestRemoveKeySpecifyingKeyFile(c *C) {
+	u, err := user.Current()
+	c.Assert(err, IsNil)
+	p := path.Join(u.HomeDir, ".ssh", "id_dsa.pub")
+	expected := "Key successfully removed!\n"
+	context := cmd.Context{
+		Cmds:   []string{},
+		Args:   []string{p},
 		Stdout: manager.Stdout,
 		Stderr: manager.Stderr,
 	}
@@ -75,6 +124,15 @@ func (s *S) TestRemoveKeyReturnErrorIfTheKeyDoesNotExist(c *C) {
 	err := command.Run(&context, nil)
 	c.Assert(err, NotNil)
 	c.Assert(err.Error(), Equals, "You need to have a public rsa key")
+}
+
+func (s *S) TestInfoRemoveKey(c *C) {
+	expected := &cmd.Info{
+		Name:  "remove",
+		Usage: "key remove [path/to/key/file.pub]",
+		Desc:  "remove your public key ($HOME/.id_rsa.pub by default).",
+	}
+	c.Assert((&RemoveKey{}).Info(), DeepEquals, expected)
 }
 
 func (s *S) TestKey(c *C) {
