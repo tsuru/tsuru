e40978e43d28bae38b62e8b082aa8b84d401bf95
Andrews Medina <andrewsmedina@gmail.com>
2012-10-04 16:38:46 -0300

    Added output for install deps.
    Related to #89.

--- api/app/handler.go
+++ api/app/handler.go
@@ -102,6 +102,10 @@ func CloneRepositoryHandler(w http.ResponseWriter, r *http.Request) error {
 		write(w, out)
 		return err
 	}
+	err = write(w, out)
+	if err != nil {
+		return err
+	}
 	err = write(w, []byte(" ---> Restarting your app\n"))
 	if err != nil {
 		return err
