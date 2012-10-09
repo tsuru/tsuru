3511e3426d35d9fd71f56283d8f6c83e935f2be3
Francisco Souza <fss@corp.globo.com>
2012-10-09 15:29:08 -0300

    docs: fix copyright

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'tsuru'
-copyright = u'2012, timeredbull'
+copyright = u'2012, Globo.com'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
