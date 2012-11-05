f3962c6e46ca2ea8f698594b94d679ad3b07a4cd
Evandro Flores <eof@eof.com.br>
2012-11-05 10:03:11 -0200

    go fmt ./... colors

--- cmd/color.go
+++ cmd/color.go
@@ -4,43 +4,44 @@
 
 package cmd
 
-import ("fmt")
-
-const (
-    pattern = "\033[%d;%d;%dm%s\033[0m"
-    bg_factor = 10
+import (
+	"fmt"
 )
 
-var fontcolors = map[string] int {
-    "black":   30,
-    "red":     31,
-    "green":   32,
-    "yellow":  33,
-    "blue":    34,
-    "magenta": 35,
-    "cyan":    36,
-    "white":   37,
+const (
+	pattern   = "\033[%d;%d;%dm%s\033[0m"
+	bg_factor = 10
+)
+
+var fontcolors = map[string]int{
+	"black":   30,
+	"red":     31,
+	"green":   32,
+	"yellow":  33,
+	"blue":    34,
+	"magenta": 35,
+	"cyan":    36,
+	"white":   37,
 }
 
-var effects = map[string] int {
-    "reset":   0,
-    "bold":    1,
-    "inverse": 7,
+var effects = map[string]int{
+	"reset":   0,
+	"bold":    1,
+	"inverse": 7,
 }
 
-func colorfy(msg string, fontcolor string, background string, effect string) string{
-    return fmt.Sprintf(pattern, effects[effect], fontcolors[fontcolor], fontcolors[background]+bg_factor, msg)
+func colorfy(msg string, fontcolor string, background string, effect string) string {
+	return fmt.Sprintf(pattern, effects[effect], fontcolors[fontcolor], fontcolors[background]+bg_factor, msg)
 }
 
 func red(msg string) string {
-    return colorfy(msg, "red", "", "")
+	return colorfy(msg, "red", "", "")
 }
 
 func green(msg string) string {
-    return colorfy(msg, "green", "", "")
+	return colorfy(msg, "green", "", "")
 }
 
 func bold_white(msg string) string {
-    return colorfy(msg, "white", "", "bold")
+	return colorfy(msg, "white", "", "bold")
 }
-
--- cmd/color_test.go
+++ cmd/color_test.go
@@ -5,25 +5,25 @@
 package cmd
 
 import (
-    ."launchpad.net/gocheck"
+	. "launchpad.net/gocheck"
 )
 
-func (s *S) TestRed(c *C){
-    output := red("must return a red font pattern")
-    c.Assert(output, Equals, "\033[0;31;10mmust return a red font pattern\033[0m")
+func (s *S) TestRed(c *C) {
+	output := red("must return a red font pattern")
+	c.Assert(output, Equals, "\033[0;31;10mmust return a red font pattern\033[0m")
 }
 
-func (s *S) TestGreen(c *C){
-    output := green("must return a green font pattern")
-    c.Assert(output, Equals, "\033[0;32;10mmust return a green font pattern\033[0m")
+func (s *S) TestGreen(c *C) {
+	output := green("must return a green font pattern")
+	c.Assert(output, Equals, "\033[0;32;10mmust return a green font pattern\033[0m")
 }
 
 func (s *S) TestBoldWhite(c *C) {
-    output := bold_white("must return a bold white font pattern")
-    c.Assert(output, Equals, "\033[1;37;10mmust return a bold white font pattern\033[0m")
+	output := bold_white("must return a bold white font pattern")
+	c.Assert(output, Equals, "\033[1;37;10mmust return a bold white font pattern\033[0m")
 }
 
 func (s *S) TestBoldYellowGreenBG(c *C) {
-    output := colorfy("must return a bold yellow with green background", "yellow", "green", "bold")
-    c.Assert(output, Equals, "\033[1;33;42mmust return a bold yellow with green background\033[0m")
+	output := colorfy("must return a bold yellow with green background", "yellow", "green", "bold")
+	c.Assert(output, Equals, "\033[1;33;42mmust return a bold yellow with green background\033[0m")
 }
