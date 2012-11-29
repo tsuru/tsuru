e7fa05bc6d354e6cb9bce5948badf380300f85e7
Andrews Medina <andrewsmedina@gmail.com>
2012-11-29 16:54:19 -0200

    FilteredWriter uses filter package.
    Related to #184 and #186.

--- api/webserver/writer.go
+++ api/webserver/writer.go
@@ -5,9 +5,8 @@
 package main
 
 import (
-	"bytes"
+	"github.com/globocom/tsuru/api/filter"
 	"net/http"
-	"regexp"
 )
 
 // FilteredWriter is a custom writer
@@ -23,7 +22,7 @@ func (w *FilteredWriter) Header() http.Header {
 
 // Write writes and flushes the data, filtering the juju warnings.
 func (w *FilteredWriter) Write(data []byte) (int, error) {
-	_, err := w.writer.Write(filterOutput(data))
+	_, err := w.writer.Write(filter.FilterOutput(data))
 	if f, ok := w.writer.(http.Flusher); ok {
 		f.Flush()
 	}
@@ -34,33 +33,4 @@ func (w *FilteredWriter) Write(data []byte) (int, error) {
 // WriteHeader calls the w.WriteHeader
 func (w *FilteredWriter) WriteHeader(code int) {
 	w.writer.WriteHeader(code)
-}
-
-// filterOutput filters output from juju.
-//
-// It removes all lines that does not represent useful output, like juju's
-// logging and Python's deprecation warnings.
-func filterOutput(output []byte) []byte {
-	var result [][]byte
-	var ignore bool
-	deprecation := []byte("DeprecationWarning")
-	regexLog := regexp.MustCompile(`^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}`)
-	regexSshWarning := regexp.MustCompile(`^Warning: Permanently added`)
-	regexPythonWarning := regexp.MustCompile(`^.*warnings.warn`)
-	regexUserWarning := regexp.MustCompile(`^.*UserWarning`)
-	lines := bytes.Split(output, []byte{'\n'})
-	for _, line := range lines {
-		if ignore {
-			ignore = false
-			continue
-		}
-		if bytes.Contains(line, deprecation) {
-			ignore = true
-			continue
-		}
-		if !regexSshWarning.Match(line) && !regexLog.Match(line) && !regexPythonWarning.Match(line) && !regexUserWarning.Match(line) {
-			result = append(result, line)
-		}
-	}
-	return bytes.Join(result, []byte{'\n'})
 }
