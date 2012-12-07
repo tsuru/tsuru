1b00526de0257818db43cac395ba3e9cdc6d4d95
Andrews Medina <andrewsmedina@gmail.com>
2012-12-07 01:52:34 -0200

    added a doc for juju package

--- juju/filter.go
+++ juju/filter.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package juju provide utilities functions for handle with juju .
 package juju
 
 import (
