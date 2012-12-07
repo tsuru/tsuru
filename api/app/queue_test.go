497b4548683941561f7a3cca98c9f277499e983f
Francisco Souza <fss@corp.globo.com>
2012-12-07 11:19:08 -0200

    api/app: added FakeQueueServer for testing purposes.
    Related to #203.

---
+++ api/app/queue_test.go
@@ -0,0 +1,52 @@
+// Copyright 2012 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package app
+
+import (
+	"encoding/gob"
+	"github.com/globocom/tsuru/queue"
+	"net"
+)
+
+// FakeQueueServer is a very dumb queue server that does not handle connections
+// concurrently.
+type FakeQueueServer struct {
+	listener net.Listener
+	messages []queue.Message
+	closed   bool
+}
+
+func (s *FakeQueueServer) Start(laddr string) error {
+	var err error
+	s.listener, err = net.Listen("tcp", laddr)
+	if err != nil {
+		return err
+	}
+	go s.loop()
+	return nil
+}
+
+func (s *FakeQueueServer) loop() {
+	for !s.closed {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			if e, ok := err.(*net.OpError); ok && !e.Temporary() {
+				return
+			}
+		}
+		decoder := gob.NewDecoder(conn)
+		for err == nil {
+			var msg queue.Message
+			if err := decoder.Decode(&msg); err == nil {
+				s.messages = append(s.messages, msg)
+			}
+		}
+	}
+}
+
+func (s *FakeQueueServer) Stop() error {
+	s.closed = true
+	return s.listener.Close()
+}
