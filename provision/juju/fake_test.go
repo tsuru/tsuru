081755174b19aad805cb91c7affba579b7c123d2
Francisco Souza <fss@corp.globo.com>
2012-12-11 16:11:42 -0200

    provision/juju: added some fake implementation for provision interfaces
    Related to #211.

---
+++ provision/juju/fake_test.go
@@ -0,0 +1,66 @@
+// Copyright 2012 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package juju
+
+import (
+	"github.com/globocom/tsuru/provision"
+)
+
+type FakeUnit struct {
+	name    string
+	machine int
+	actions []string
+}
+
+func (u *FakeUnit) GetName() string {
+	u.actions = append(u.actions, "getname")
+	return u.name
+}
+
+func (u *FakeUnit) GetMachine() int {
+	u.actions = append(u.actions, "getmachine")
+	return u.machine
+}
+
+type FakeApp struct {
+	name      string
+	framework string
+	units     []provision.AppUnit
+	logs      []string
+	actions   []string
+}
+
+func NewFakeApp(name, framework string, units int) *FakeApp {
+	app := FakeApp{
+		name:      name,
+		framework: framework,
+		units:     make([]provision.AppUnit, units),
+	}
+	for i := 0; i < units; i++ {
+		app.units[i] = &FakeUnit{name: name, machine: i + 1}
+	}
+	return &app
+}
+
+func (a *FakeApp) Log(message, source string) error {
+	a.logs = append(a.logs, source+message)
+	a.actions = append(a.actions, "log "+source+" - "+message)
+	return nil
+}
+
+func (a *FakeApp) GetName() string {
+	a.actions = append(a.actions, "getname")
+	return a.name
+}
+
+func (a *FakeApp) GetFramework() string {
+	a.actions = append(a.actions, "getframework")
+	return a.framework
+}
+
+func (a *FakeApp) GetUnits() []provision.AppUnit {
+	a.actions = append(a.actions, "getunits")
+	return a.units
+}
