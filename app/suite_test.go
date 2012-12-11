09586999ab338887aa9884764013967d1c7674ab
Flavia Missi <flaviamissi@gmail.com>
2012-12-11 15:33:01 -0200

    app: using testing package in tests fixtures

--- app/suite_test.go
+++ app/suite_test.go
@@ -6,13 +6,12 @@ package app
 
 import (
 	"fmt"
-	"github.com/fsouza/go-iam/iamtest"
 	"github.com/globocom/config"
 	"github.com/globocom/tsuru/api/auth"
 	"github.com/globocom/tsuru/db"
 	fsTesting "github.com/globocom/tsuru/fs/testing"
+	tsuruTesting "github.com/globocom/tsuru/testing"
 	"io"
-	"launchpad.net/goamz/s3/s3test"
 	. "launchpad.net/gocheck"
 	"net/http"
 	"net/http/httptest"
@@ -26,15 +25,11 @@ import (
 func Test(t *testing.T) { TestingT(t) }
 
 type S struct {
-	team      auth.Team
-	user      *auth.User
-	gitRoot   string
-	rfs       *fsTesting.RecordingFs
-	iamServer *iamtest.Server
-	s3Server  *s3test.Server
-	gitHost   string
-	gitPort   string
-	gitProt   string
+	team    auth.Team
+	user    *auth.User
+	gitRoot string
+	rfs     *fsTesting.RecordingFs
+	t       *tsuruTesting.T
 }
 
 var _ = Suite(&S{})
@@ -64,45 +59,42 @@ func (c *greaterChecker) Check(params []interface{}, names []string) (bool, stri
 	return false, err
 }
 
+func (s *S) createUserAndTeam(c *C) {
+	s.user = &auth.User{Email: "whydidifall@thewho.com", Password: "123"}
+	err := s.user.Create()
+	c.Assert(err, IsNil)
+	s.team = auth.Team{Name: "tsuruteam", Users: []string{s.user.Email}}
+	err = db.Session.Teams().Insert(s.team)
+	c.Assert(err, IsNil)
+}
+
 func (s *S) SetUpSuite(c *C) {
 	var err error
 	err = config.ReadConfigFile("../etc/tsuru.conf")
 	c.Assert(err, IsNil)
 	db.Session, err = db.Open("127.0.0.1:27017", "tsuru_app_test")
 	c.Assert(err, IsNil)
-	s.user = &auth.User{Email: "whydidifall@thewho.com", Password: "123"}
-	s.user.Create()
-	s.team = auth.Team{Name: "tsuruteam", Users: []string{s.user.Email}}
-	db.Session.Teams().Insert(s.team)
 	s.rfs = &fsTesting.RecordingFs{}
 	file, err := s.rfs.Open("/dev/urandom")
 	c.Assert(err, IsNil)
 	file.Write([]byte{16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31})
 	fsystem = s.rfs
-	s.s3Server, err = s3test.NewServer()
-	c.Assert(err, IsNil)
-	config.Set("aws:s3:endpoint", s.s3Server.URL())
-	s.iamServer, err = iamtest.NewServer()
-	c.Assert(err, IsNil)
-	config.Set("aws:iam:endpoint", s.iamServer.URL())
-	config.Unset("aws:s3:bucketEndpoint")
-	s.gitHost, _ = config.GetString("git:host")
-	s.gitPort, _ = config.GetString("git:port")
-	s.gitProt, _ = config.GetString("git:protocol")
+	s.t = &tsuruTesting.T{}
+	s.createUserAndTeam(c)
+	s.t.StartAmzS3AndIAM(c)
+	s.t.SetGitConfs(c)
 }
 
 func (s *S) TearDownSuite(c *C) {
-	defer s.s3Server.Quit()
-	defer s.iamServer.Quit()
+	defer s.t.S3Server.Quit()
+	defer s.t.IamServer.Quit()
 	defer db.Session.Close()
 	db.Session.Apps().Database.DropDatabase()
 	fsystem = nil
 }
 
 func (s *S) TearDownTest(c *C) {
-	config.Set("git:host", s.gitHost)
-	config.Set("git:port", s.gitPort)
-	config.Set("git:protocol", s.gitProt)
+	s.t.RollbackGitConfs(c)
 }
 
 func (s *S) getTestData(p ...string) io.ReadCloser {
