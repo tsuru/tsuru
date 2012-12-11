dc20ce19a5ea4e52fbb2056e6eedab4cff19ee7a
Flavia Missi <flaviamissi@gmail.com>
2012-12-11 15:25:55 -0200

    api: splited fixture stuff into external functions
    Also created tsuru/testing package to hold that functions.

--- api/suite_test.go
+++ api/suite_test.go
@@ -6,35 +6,25 @@ package api
 
 import (
 	"fmt"
-	"github.com/fsouza/go-iam/iamtest"
 	"github.com/globocom/config"
 	"github.com/globocom/tsuru/api/auth"
 	"github.com/globocom/tsuru/db"
 	fsTesting "github.com/globocom/tsuru/fs/testing"
+	tsuruTesting "github.com/globocom/tsuru/testing"
 	"io"
-	"launchpad.net/goamz/s3/s3test"
 	. "launchpad.net/gocheck"
-	"net/http"
-	"net/http/httptest"
 	"os"
 	"path"
-	"strconv"
-	"strings"
 	"testing"
 )
 
 func Test(t *testing.T) { TestingT(t) }
 
 type S struct {
-	team      auth.Team
-	user      *auth.User
-	gitRoot   string
-	rfs       *fsTesting.RecordingFs
-	iamServer *iamtest.Server
-	s3Server  *s3test.Server
-	gitHost   string
-	gitPort   string
-	gitProt   string
+	team auth.Team
+	user *auth.User
+	rfs  *fsTesting.RecordingFs
+	t    *tsuruTesting.T
 }
 
 var _ = Suite(&S{})
@@ -64,32 +54,42 @@ func (c *greaterChecker) Check(params []interface{}, names []string) (bool, stri
 	return false, err
 }
 
+func (s *S) createUserAndTeam(c *C) {
+	s.user = &auth.User{Email: "whydidifall@thewho.com", Password: "123"}
+	err := s.user.Create()
+	c.Assert(err, IsNil)
+	s.team = auth.Team{Name: "tsuruteam", Users: []string{s.user.Email}}
+	err = db.Session.Teams().Insert(s.team)
+	c.Assert(err, IsNil)
+}
+
 func (s *S) SetUpSuite(c *C) {
 	var err error
 	err = config.ReadConfigFile("../etc/tsuru.conf")
 	c.Assert(err, IsNil)
 	db.Session, err = db.Open("127.0.0.1:27017", "tsuru_app_test")
 	c.Assert(err, IsNil)
-	s.createUserAndTeam(c)
 	s.rfs = &fsTesting.RecordingFs{}
 	file, err := s.rfs.Open("/dev/urandom")
 	c.Assert(err, IsNil)
 	file.Write([]byte{16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31})
 	fsystem = s.rfs
-	s.startAmzS3AndIAM(c)
-	s.setGitConfs(c)
+	s.t = &tsuruTesting.T{}
+	s.createUserAndTeam(c)
+	s.t.StartAmzS3AndIAM(c)
+	s.t.SetGitConfs(c)
 }
 
 func (s *S) TearDownSuite(c *C) {
-	defer s.s3Server.Quit()
-	defer s.iamServer.Quit()
+	defer s.t.S3Server.Quit()
+	defer s.t.IamServer.Quit()
 	defer db.Session.Close()
 	db.Session.Apps().Database.DropDatabase()
 	fsystem = nil
 }
 
 func (s *S) TearDownTest(c *C) {
-	s.rollbackGitConfs(c)
+	s.t.RollbackGitConfs(c)
 }
 
 func (s *S) getTestData(p ...string) io.ReadCloser {
@@ -97,50 +97,4 @@ func (s *S) getTestData(p ...string) io.ReadCloser {
 	fp := path.Join(p...)
 	f, _ := os.OpenFile(fp, os.O_RDONLY, 0)
 	return f
-}
-
-func (s *S) startAmzS3AndIAM(c *C) {
-	var err error
-	s.s3Server, err = s3test.NewServer()
-	c.Assert(err, IsNil)
-	config.Set("aws:s3:endpoint", s.s3Server.URL())
-	s.iamServer, err = iamtest.NewServer()
-	c.Assert(err, IsNil)
-	config.Set("aws:iam:endpoint", s.iamServer.URL())
-	config.Unset("aws:s3:bucketEndpoint")
-}
-
-func (s *S) setGitConfs(c *C) {
-	s.gitHost, _ = config.GetString("git:host")
-	s.gitPort, _ = config.GetString("git:port")
-	s.gitProt, _ = config.GetString("git:protocol")
-}
-
-func (s *S) rollbackGitConfs(c *C) {
-	config.Set("git:host", s.gitHost)
-	config.Set("git:port", s.gitPort)
-	config.Set("git:protocol", s.gitProt)
-}
-
-func (s *S) createUserAndTeam(c *C) {
-	s.user = &auth.User{Email: "whydidifall@thewho.com", Password: "123"}
-	s.user.Create()
-	s.team = auth.Team{Name: "tsuruteam", Users: []string{s.user.Email}}
-	db.Session.Teams().Insert(s.team)
-}
-
-// starts a new httptest.Server and returns it
-// Also changes git:host, git:port and git:protocol to match the server's url
-func (s *S) startGandalfTestServer(h http.Handler) *httptest.Server {
-	ts := httptest.NewServer(h)
-	pieces := strings.Split(ts.URL, "://")
-	protocol := pieces[0]
-	hostPart := strings.Split(pieces[1], ":")
-	port := hostPart[1]
-	host := hostPart[0]
-	config.Set("git:host", host)
-	portInt, _ := strconv.ParseInt(port, 10, 0)
-	config.Set("git:port", portInt)
-	config.Set("git:protocol", protocol)
-	return ts
 }
---
+++ testing/setup.go
@@ -0,0 +1,77 @@
+package testing
+
+import (
+	"github.com/fsouza/go-iam/iamtest"
+	"github.com/globocom/config"
+	"launchpad.net/goamz/s3/s3test"
+	. "launchpad.net/gocheck"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+)
+
+type T struct {
+	Admin     user
+	AdminTeam team
+	AdminApp  app
+	S3Server  *s3test.Server
+	IamServer *iamtest.Server
+	GitHost   string
+	GitPort   string
+	GitProt   string
+}
+
+type user struct {
+	Email    string
+	Password string
+}
+
+type team struct {
+	Name  string `bson:"_id"`
+	Users []string
+}
+
+type app struct {
+	Name  string
+	Teams []string
+}
+
+func (t *T) StartAmzS3AndIAM(c *C) {
+	var err error
+	t.S3Server, err = s3test.NewServer()
+	c.Assert(err, IsNil)
+	config.Set("aws:s3:endpoint", t.S3Server.URL())
+	t.IamServer, err = iamtest.NewServer()
+	c.Assert(err, IsNil)
+	config.Set("aws:iam:endpoint", t.IamServer.URL())
+	config.Unset("aws:s3:bucketEndpoint")
+}
+
+func (t *T) SetGitConfs(c *C) {
+	t.GitHost, _ = config.GetString("git:host")
+	t.GitPort, _ = config.GetString("git:port")
+	t.GitProt, _ = config.GetString("git:protocol")
+}
+
+func (t *T) RollbackGitConfs(c *C) {
+	config.Set("git:host", t.GitHost)
+	config.Set("git:port", t.GitPort)
+	config.Set("git:protocol", t.GitProt)
+}
+
+// starts a new httptest.Server and returns it
+// Also changes git:host, git:port and git:protocol to match the server's url
+func (t *T) StartGandalfTestServer(h http.Handler) *httptest.Server {
+	ts := httptest.NewServer(h)
+	pieces := strings.Split(ts.URL, "://")
+	protocol := pieces[0]
+	hostPart := strings.Split(pieces[1], ":")
+	port := hostPart[1]
+	host := hostPart[0]
+	config.Set("git:host", host)
+	portInt, _ := strconv.ParseInt(port, 10, 0)
+	config.Set("git:port", portInt)
+	config.Set("git:protocol", protocol)
+	return ts
+}
