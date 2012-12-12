db3101dd8fece0840ddeea512727ebdb8b107608
Francisco Souza <fss@corp.globo.com>
2012-12-12 16:28:26 -0200

    testing: added copyright header

--- testing/setup.go
+++ testing/setup.go
@@ -1,6 +1,8 @@
-//
-// This package provide test helpers for various actions
-//
+// Copyright 2012 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package testing provide test helpers for various actions.
 package testing
 
 import (
