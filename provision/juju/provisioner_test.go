481ff03997b1578f0dda0596ca1701ad067b18ff
Francisco Souza <fss@corp.globo.com>
2012-12-12 14:59:25 -0200

    provision/juju: added implementation for CollectStatus
    Sorry for this huge commit. I also implemented three helper functions:
    
    	1. runCmd (for running juju commands)
    	2. execWithTimeout (for running commands with timeout)
    	3. unitStatus (for "calculating" the status of the unit, given its
    	agent-state, machine-agent-state and instance-state)
    
    Most stuf was already done, I've just copied them out.
    
    Related to #211.

--- provision/juju/provisioner_test.go
+++ provision/juju/provisioner_test.go
@@ -6,9 +6,11 @@ package juju
 
 import (
 	"bytes"
+	"errors"
 	"github.com/globocom/commandmocker"
 	"github.com/globocom/tsuru/provision"
 	. "launchpad.net/gocheck"
+	"time"
 )
 
 func (s *S) TestShouldBeRegistered(c *C) {
@@ -94,4 +96,94 @@ func (s *S) TestJujuExecuteCommandFailure(c *C) {
 	c.Assert(err, NotNil)
 	c.Assert(err.Error(), Equals, "exit status 2")
 	c.Assert(buf.String(), Equals, "failed")
+}
+
+func (s *S) TestJujuCollectStatus(c *C) {
+	tmpdir, err := commandmocker.Add("juju", collectOutput)
+	c.Assert(err, IsNil)
+	defer commandmocker.Remove(tmpdir)
+	p := JujuProvisioner{}
+	expected := []provision.Unit{
+		{
+			Name:    "i-00000439",
+			AppName: "as_i_rise",
+			Type:    "django",
+			Machine: 105,
+			Ip:      "10.10.10.163",
+			Status:  provision.StatusStarted,
+		},
+		{
+			Name:    "i-0000043e",
+			AppName: "the_infanta",
+			Type:    "gunicorn",
+			Machine: 107,
+			Ip:      "10.10.10.168",
+			Status:  provision.StatusInstalling,
+		},
+	}
+	units, err := p.CollectStatus()
+	c.Assert(err, IsNil)
+	if units[0].Type == "gunicorn" {
+		units[0], units[1] = units[1], units[0]
+	}
+	c.Assert(units, DeepEquals, expected)
+}
+
+func (s *S) TestExecWithTimeout(c *C) {
+	var data = []struct {
+		cmd     []string
+		timeout time.Duration
+		out     string
+		err     error
+	}{
+		{
+			cmd:     []string{"sleep", "2"},
+			timeout: 1e6,
+			out:     "",
+			err:     errors.New(`"sleep 2" ran for more than 1ms.`),
+		},
+		{
+			cmd:     []string{"python", "-c", "import time; time.sleep(1); print 'hello world!'"},
+			timeout: 5e9,
+			out:     "hello world!\n",
+			err:     nil,
+		},
+	}
+	for _, d := range data {
+		out, err := execWithTimeout(d.timeout, d.cmd[0], d.cmd[1:]...)
+		if string(out) != d.out {
+			c.Errorf("Output. Want %q. Got %q.", d.out, out)
+		}
+		if d.err == nil && err != nil {
+			c.Errorf("Error. Want %v. Got %v.", d.err, err)
+		} else if d.err != nil && err.Error() != d.err.Error() {
+			c.Errorf("Error message. Want %q. Got %q.", d.err.Error(), err.Error())
+		}
+	}
+}
+
+func (s *S) TestUnitStatus(c *C) {
+	var tests = []struct {
+		instance     string
+		agent        string
+		machineAgent string
+		expected     string
+	}{
+		{"something", "nothing", "wut", provision.StatusPending},
+		{"", "", "", provision.StatusCreating},
+		{"", "", "pending", provision.StatusCreating},
+		{"", "", "not-started", provision.StatusCreating},
+		{"pending", "", "", provision.StatusCreating},
+		{"", "not-started", "running", provision.StatusCreating},
+		{"error", "install-error", "start-error", provision.StatusError},
+		{"running", "pending", "running", provision.StatusInstalling},
+		{"running", "started", "running", provision.StatusStarted},
+		{"running", "down", "running", provision.StatusDown},
+	}
+	for _, t := range tests {
+		got := unitStatus(t.instance, t.agent, t.machineAgent)
+		if got != t.expected {
+			c.Errorf("unitStatus(%q, %q, %q): Want %q. Got %q.", t.instance, t.agent, t.machineAgent, t.expected, got)
+		}
+	}
 }
