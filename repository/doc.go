2598aaa981147d72e226b43e83a1b5d2712984c8
Francisco Souza <fss@corp.globo.com>
2012-12-14 12:16:56 -0200

    repository: update docs

--- repository/doc.go
+++ repository/doc.go
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package repository contains types and function for repository management.
-//
-// It has some internals interfaces that can be used to generalize the
-// repository manipulating, but at this moment it supports only git (through
-// gitosis).
+// Package repository contains types and function for git repository
+// interaction.
 package repository
