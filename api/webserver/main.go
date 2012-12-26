0217f8fa64cf2c6f076f83c06a9c180e5f25c64b
Francisco Souza <fss@corp.globo.com>
2012-12-26 14:33:49 -0200

    api/webserver: added route for RemoveUnitsHandler
    Related to #250.

--- api/webserver/main.go
+++ api/webserver/main.go
@@ -90,6 +90,7 @@ func main() {
 	m.Get("/apps", AuthorizationRequiredHandler(api.AppList))
 	m.Post("/apps", AuthorizationRequiredHandler(api.CreateAppHandler))
 	m.Put("/apps/:name/units", AuthorizationRequiredHandler(api.AddUnitsHandler))
+	m.Del("/apps/:name/units", AuthorizationRequiredHandler(api.RemoveUnitsHandler))
 	m.Put("/apps/:app/:team", AuthorizationRequiredHandler(api.GrantAccessToTeamHandler))
 	m.Del("/apps/:app/:team", AuthorizationRequiredHandler(api.RevokeAccessFromTeamHandler))
 	m.Get("/apps/:name/log", AuthorizationRequiredHandler(api.AppLog))
