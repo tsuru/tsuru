fd8046f3ac189e7c0952bbf4fac652b489e600dc
Francisco Souza <fss@corp.globo.com>
2012-12-27 17:06:03 -0200

    collector: make update run faster
    Old code updated the database much more than necessary. I've added a
    benchmark for comparison. Here is the result of comparison:
    
    	% $GOROOT/misc/benchcmp old new
    	benchmark          old ns/op    new ns/op    delta
    	BenchmarkUpdate    284675885     27383641  -90,38%
    
    Raw output from benchmarks:
    
    	% cat old
    	PASS
    	BenchmarkUpdate	      20	 284675885 ns/op
    	ok		github.com/globocom/tsuru/collector	24.099s
    
    	% cat new
    	PASS
    	BenchmarkUpdate	     200	  27383641 ns/op
    	ok		github.com/globocom/tsuru/collector	28.448s
    
    Old version took about 284ms per operation, while new version takes 27ms
    per operation.

--- collector/collector.go
+++ collector/collector.go
@@ -10,16 +10,45 @@ import (
 	"github.com/globocom/tsuru/log"
 	"github.com/globocom/tsuru/provision"
 	"labix.org/v2/mgo/bson"
+	"sort"
 )
+
+type AppList []*app.App
+
+func (l AppList) Search(name string) (*app.App, int) {
+	index := sort.Search(len(l), func(i int) bool {
+		return l[i].Name >= name
+	})
+	if index < len(l) && l[index].Name == name {
+		return l[index], -1
+	} else if index < len(l) {
+		return &app.App{Name: name}, index
+	}
+	return &app.App{Name: name}, len(l)
+}
+
+func (l *AppList) Add(a *app.App, index int) {
+	length := len(*l)
+	*l = append(*l, a)
+	if index < length {
+		for i := length; i > index; i-- {
+			(*l)[i] = (*l)[i-1]
+		}
+		(*l)[index] = a
+	}
+}
 
 func update(units []provision.Unit) {
 	log.Print("updating status from provisioner")
+	var l AppList
 	for _, unit := range units {
-		a := app.App{Name: unit.AppName}
-		err := a.Get()
-		if err != nil {
-			log.Printf("collector: app %q not found. Skipping.\n", unit.AppName)
-			continue
+		a, index := l.Search(unit.AppName)
+		if index > -1 {
+			err := a.Get()
+			if err != nil {
+				log.Printf("collector: app %q not found. Skipping.\n", unit.AppName)
+				continue
+			}
 		}
 		u := app.Unit{}
 		u.Name = unit.Name
@@ -30,6 +59,11 @@ func update(units []provision.Unit) {
 		a.State = string(unit.Status)
 		a.Ip = unit.Ip
 		a.AddUnit(&u)
+		if index > -1 {
+			l.Add(a, index)
+		}
+	}
+	for _, a := range l {
 		db.Session.Apps().Update(bson.M{"name": a.Name}, a)
 	}
 }
