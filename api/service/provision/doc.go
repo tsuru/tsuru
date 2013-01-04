65eabbfe4e951aefc7b522972c41ea5e4efe7838
Andrews Medina <andrewsmedina@gmail.com>
2013-01-04 11:22:42 -0200

    Removing the api/service package.

--- api/service/provision/doc.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2012 tsuru authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-/*
-Package api/service/provision implements the api endpoints for all
-actions related to service's provision needed by service administrators.
-*/
-package provision
