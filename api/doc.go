23ee1eb331a5eec0157488f9565974a03f865b2d
Andrews Medina <andrewsmedina@gmail.com>
2013-01-14 13:14:48 -0200

    Inital docs for api.
    Related to #81.

---
+++ api/doc.go
@@ -0,0 +1,9 @@
+// Copyright 2013 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package api implements the REST api for tsuru.
+
+It provides the REST api for manage users, teams, apps and services.
+*/
