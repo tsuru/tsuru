3e2da5cf3ab9d933ce37b01fc5cbe88af0e64968
Francisco Souza <fss@corp.globo.com>
2013-02-02 23:52:40 -0200

    cmd/term: use constant from syscall package instead of magic number

--- cmd/term/termios_darwin.go
+++ cmd/term/termios_darwin.go
@@ -1,10 +1,12 @@
-// Copyright 2012 tsuru authors. All rights reserved.
+// Copyright 2013 tsuru authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package term
 
+import "syscall"
+
 var (
-	TCGETS = 0x40487413
-	TCSETS = 0x80487414
+	TCGETS = syscall.TIOCGETA
+	TCSETS = syscall.TIOCSETA
 )
