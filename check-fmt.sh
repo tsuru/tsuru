2542d2c64fa054e6dc8ebbf3f9baa8c645e4d5bd
Francisco Souza <fss@corp.globo.com>
2013-02-05 09:03:22 -0200

    check-fmt: some changes to the script
    I'm by passing go vet's failures with multiple packages (I'm not
    interested in divingin into it right now).

--- check-fmt.sh
+++ check-fmt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-# Copyright 2012 tsuru authors. All rights reserved.
+# Copyright 2013 tsuru authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
@@ -8,27 +8,37 @@ status=0
 out=`gofmt -s -l .`
 if [ "${out}" != "" ]
 then
-    echo "ERROR: there are files that need to be formatted with gofmt"
-    echo
-    echo "Files:"
-    for file in $out
-    do
-        echo "- ${file}"
-    done
-    status=1
+	echo "ERROR: there are files that need to be formatted with gofmt"
+	echo
+	echo "Files:"
+	for file in $out
+	do
+	    echo "- ${file}"
+	done
+	status=1
 fi
 
-`go vet ./... > .vet 2>&1`
-out=`cat .vet`
-if [ "${out}" != "" ]
+path=$PWD
+for p in `go list ./...`
+do
+	pushd $GOPATH/src/$p > /dev/null
+	go vet >> $path/.vet
+	popd > /dev/null
+done
+
+if [ -f .vet ]
 then
-    echo "ERROR: go vet failures:"
-    echo
-    cat <<END
-${out}
+	out=`cat .vet`
+	if [ "${out}" != "" ]
+	then
+		echo "ERROR: go vet failures:"
+		echo
+		cat <<END
+	${out}
 END
-    status=1
+		status=1
+	fi
+	rm .vet
 fi
 
-rm .vet || /bin/true
 exit $status
