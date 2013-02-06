19f86dcef5667d7aef905ba6730730751ebadd40
Andrews Medina <andrewsmedina@gmail.com>
2013-02-05 23:34:30 -0200

    misc/git-hooks: added pre-receive hooks.

---
+++ misc/git-hooks/pre-receive.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import urllib2
+import os
+import sys
+
+
+tsuru_host = os.environ.get("TSURU_HOST", "")
+app_name = os.getcwd().split("/")[-1].replace(".git", "")
+timeout = 1800
+
+try:
+    f = urllib2.urlopen("{0}/apps/{1}/avaliable".format(tsuru_host, app_name), timeout=timeout)
+except urllib2.HTTPError as e:
+    print "\n ---> {0}\n".format(e.read())
+    sys.exit(1)
