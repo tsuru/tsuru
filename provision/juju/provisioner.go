b538ff6e5916e92208f34d61369060c8e85e68ca
Andrews Medina <andrewsmedina@gmail.com>
2013-02-07 11:04:10 -0200

    provisioner/juju: fixed config names.

--- provision/juju/provisioner.go
+++ provision/juju/provisioner.go
@@ -93,12 +93,12 @@ func (p *JujuProvisioner) Provision(app provision.App) error {
 		app.Log("Failed to create machine: "+out, "tsuru")
 		return cmdError(out, err, args)
 	}
-	err = setOption(app.GetName(), "TSURU_APPNAME", app.GetName())
+	err = setOption(app.GetName(), "tsuru-appname", app.GetName())
 	if err != nil {
 		return err
 	}
 	host, _ := config.GetString("host")
-	err = setOption(app.GetName(), "TSURU_HOST", host)
+	err = setOption(app.GetName(), "tsuru-host", host)
 	if err != nil {
 		return err
 	}
--- provision/juju/provisioner_test.go
+++ provision/juju/provisioner_test.go
@@ -62,8 +62,8 @@ func (s *S) TestProvision(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(commandmocker.Ran(tmpdir), Equals, true)
 	expected := "deploy --repository /etc/juju/charms local:python trace"
-	expected = expected + "set trace TSURU_APPNAME=trace"
-	expected = expected + "set trace TSURU_HOST=somehost"
+	expected = expected + "set trace tsuru-appname=trace"
+	expected = expected + "set trace tsuru-host=somehost"
 	c.Assert(commandmocker.Output(tmpdir), Equals, expected)
 }
 
