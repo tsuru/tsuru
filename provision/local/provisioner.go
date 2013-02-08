91e30bd9d3319f2f6e31f859226e688010923f4c
Andrews Medina <andrewsmedina@gmail.com>
2013-02-07 22:11:02 -0200

    provision/local: creating and starting lxc container on provision.

--- provision/local/provisioner.go
+++ provision/local/provisioner.go
@@ -5,5 +5,14 @@ import "github.com/globocom/tsuru/provision"
 type LocalProvisioner struct{}
 
 func (*LocalProvisioner) Provision(app provision.App) error {
+	container := container{name: app.GetName()}
+	err := container.create()
+	if err != nil {
+		return err
+	}
+	err = container.start()
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- provision/local/provisioner_test.go
+++ provision/local/provisioner_test.go
@@ -1,12 +1,20 @@
 package local
 
 import (
+	"github.com/globocom/commandmocker"
 	"github.com/globocom/tsuru/testing"
 	. "launchpad.net/gocheck"
 )
 
 func (s *S) TestProvision(c *C) {
+	tmpdir, err := commandmocker.Add("sudo", "$*")
+	c.Assert(err, IsNil)
+	defer commandmocker.Remove(tmpdir)
 	var p LocalProvisioner
 	app := testing.NewFakeApp("myapp", "python", 0)
 	c.Assert(p.Provision(app), IsNil)
+	c.Assert(commandmocker.Ran(tmpdir), Equals, true)
+	expected := "lxc-create -t ubuntu -n myapp"
+	expected += "lxc-start --daemon -n myapp"
+	c.Assert(commandmocker.Output(tmpdir), Equals, expected)
 }
