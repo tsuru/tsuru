2c635f026e4e678651169c607c60b8b8035e276b
Andrews Medina <andrewsmedina@gmail.com>
2013-02-14 18:23:57 -0200

    provision/local: using combinet output for runCmd.

--- provision/local/lxc.go
+++ provision/local/lxc.go
@@ -10,42 +10,31 @@ type container struct {
 	name string
 }
 
+// runCmd executes commands and log the given stdout and stderror.
+func runCmd(cmd string, args ...string) error {
+	command := exec.Command(cmd, args...)
+	out, err := command.CombinedOutput()
+	log.Printf("running the cmd: %s with the args: %s", cmd, args)
+	log.Print(out)
+	return err
+}
+
 // create creates a lxc container with ubuntu template by default.
 func (c *container) create() error {
-	cmd := exec.Command("sudo", "lxc-create", "-t", "ubuntu", "-n", c.name)
-	err := cmd.Run()
-	if err != nil {
-		log.Print(err)
-	}
-	return err
+	return runCmd("sudo", "lxc-create", "-t", "ubuntu", "-n", c.name)
 }
 
 // start starts a lxc container.
 func (c *container) start() error {
-	cmd := exec.Command("sudo", "lxc-start", "--daemon", "-n", c.name)
-	err := cmd.Run()
-	if err != nil {
-		log.Print(err)
-	}
-	return err
+	return runCmd("sudo", "lxc-start", "--daemon", "-n", c.name)
 }
 
 // stop stops a lxc container.
 func (c *container) stop() error {
-	cmd := exec.Command("sudo", "lxc-stop", "-n", c.name)
-	err := cmd.Run()
-	if err != nil {
-		log.Print(err)
-	}
-	return err
+	return runCmd("sudo", "lxc-stop", "-n", c.name)
 }
 
 // destroy destory a lxc container.
 func (c *container) destroy() error {
-	cmd := exec.Command("sudo", "lxc-destroy", "-n", c.name)
-	err := cmd.Run()
-	if err != nil {
-		log.Print(err)
-	}
-	return err
+	return runCmd("sudo", "lxc-destroy", "-n", c.name)
 }
