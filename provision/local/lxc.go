4329be9dff8591a000933c066678dc05d243cdf5
Andrews Medina <andrewsmedina@gmail.com>
2013-02-14 13:55:10 -0200

    provision/local: added logs for lxc actions.

--- provision/local/lxc.go
+++ provision/local/lxc.go
@@ -1,6 +1,9 @@
 package local
 
-import "os/exec"
+import (
+	"github.com/globocom/tsuru/log"
+	"os/exec"
+)
 
 // container represents an lxc container with the given name.
 type container struct {
@@ -10,23 +13,39 @@ type container struct {
 // create creates a lxc container with ubuntu template by default.
 func (c *container) create() error {
 	cmd := exec.Command("sudo", "lxc-create", "-t", "ubuntu", "-n", c.name)
-	return cmd.Run()
+	err := cmd.Run()
+	if err != nil {
+		log.Print(err)
+	}
+	return err
 }
 
 // start starts a lxc container.
 func (c *container) start() error {
 	cmd := exec.Command("sudo", "lxc-start", "--daemon", "-n", c.name)
-	return cmd.Run()
+	err := cmd.Run()
+	if err != nil {
+		log.Print(err)
+	}
+	return err
 }
 
 // stop stops a lxc container.
 func (c *container) stop() error {
 	cmd := exec.Command("sudo", "lxc-stop", "-n", c.name)
-	return cmd.Run()
+	err := cmd.Run()
+	if err != nil {
+		log.Print(err)
+	}
+	return err
 }
 
 // destroy destory a lxc container.
 func (c *container) destroy() error {
 	cmd := exec.Command("sudo", "lxc-destroy", "-n", c.name)
-	return cmd.Run()
+	err := cmd.Run()
+	if err != nil {
+		log.Print(err)
+	}
+	return err
 }
