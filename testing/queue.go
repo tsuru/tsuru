6b26989df77737793bfea27955af9709abdd8159
Francisco Souza <fss@corp.globo.com>
2013-02-19 18:01:09 -0300

    testing: starting to implement fake version of the new queue package
    Related to #305.

--- testing/queue.go
+++ testing/queue.go
@@ -5,29 +5,134 @@
 package testing
 
 import (
+	"errors"
 	"github.com/globocom/tsuru/queue"
 	"sync"
+	"time"
 )
 
-// CleanQueues cleans the given queues.
-func CleanQueues(names ...string) {
-	var wg sync.WaitGroup
-	wg.Add(len(names))
-	factory, err := queue.Factory()
-	if err != nil {
-		panic(err)
+func init() {
+	queue.Register("fake", &fakeQFactory{})
+}
+
+type fakeHandler struct {
+	running bool
+}
+
+func (h *fakeHandler) Start() {
+	h.running = true
+}
+
+func (h *fakeHandler) Stop() error {
+	if !h.running {
+		return errors.New("Not running.")
 	}
-	for _, name := range names {
-		go func(qName string) {
-			var msg *queue.Message
-			q, err := factory.Get(qName)
-			for err == nil {
-				if msg, err = q.Get(1e6); err == nil {
-					err = q.Delete(msg)
-				}
-			}
-			wg.Done()
-		}(name)
+	h.running = false
+	return nil
+}
+
+func (h *fakeHandler) Wait() {}
+
+type FakeQ struct {
+	messages messageQueue
+}
+
+func (q *FakeQ) get(ch chan *queue.Message, stop chan int) {
+	for {
+		select {
+		case <-stop:
+			return
+		default:
+		}
+		if msg := q.messages.dequeue(); msg != nil {
+			ch <- msg
+			return
+		}
+		time.Sleep(1e3)
 	}
-	wg.Wait()
+}
+
+func (q *FakeQ) Get(timeout time.Duration) (*queue.Message, error) {
+	ch := make(chan *queue.Message, 1)
+	stop := make(chan int, 1)
+	defer close(stop)
+	go q.get(ch, stop)
+	select {
+	case msg := <-ch:
+		return msg, nil
+	case <-time.After(timeout):
+	}
+	return nil, errors.New("Timed out.")
+}
+
+func (q *FakeQ) Put(m *queue.Message, delay time.Duration) error {
+	if delay > 0 {
+		go func() {
+			time.Sleep(delay)
+			q.messages.enqueue(m)
+		}()
+	} else {
+		q.messages.enqueue(m)
+	}
+	return nil
+}
+
+func (q *FakeQ) Delete(m *queue.Message) error {
+	return nil
+}
+
+func (q *FakeQ) Release(m *queue.Message, delay time.Duration) error {
+	return q.Put(m, delay)
+}
+
+type fakeQFactory struct{}
+
+func (f *fakeQFactory) Get(name string) (queue.Q, error) {
+	return nil, nil
+}
+
+func (f *fakeQFactory) Handler(fn func(*queue.Message), names ...string) (queue.Handler, error) {
+	return nil, nil
+}
+
+type messageNode struct {
+	m    *queue.Message
+	next *messageNode
+	prev *messageNode
+}
+
+type messageQueue struct {
+	first *messageNode
+	last  *messageNode
+	n     int
+	sync.Mutex
+}
+
+func (q *messageQueue) enqueue(msg *queue.Message) {
+	q.Lock()
+	defer q.Unlock()
+	if q.last == nil {
+		q.last = &messageNode{m: msg}
+		q.first = q.last
+	} else {
+		olast := q.last
+		q.last = &messageNode{m: msg, prev: olast}
+		olast.next = q.last
+	}
+	q.n++
+}
+
+func (q *messageQueue) dequeue() *queue.Message {
+	q.Lock()
+	defer q.Unlock()
+	if q.n == 0 {
+		return nil
+	}
+	msg := q.first.m
+	q.n--
+	q.first = q.first.next
+	if q.n == 0 {
+		q.last = q.first
+	}
+	return msg
 }
---
+++ testing/queue_test.go
@@ -0,0 +1,58 @@
+// Copyright 2013 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package testing
+
+import (
+	"github.com/globocom/tsuru/queue"
+	. "launchpad.net/gocheck"
+)
+
+func (s *S) TestFakeQPutAndGet(c *C) {
+	q := FakeQ{}
+	msg := queue.Message{Action: "do-something"}
+	err := q.Put(&msg, 0)
+	c.Assert(err, IsNil)
+	m, err := q.Get(1e6)
+	c.Assert(err, IsNil)
+	c.Assert(m.Action, Equals, msg.Action)
+}
+
+func (s *S) TestFakeQGetTimeout(c *C) {
+	q := FakeQ{}
+	m, err := q.Get(1e6)
+	c.Assert(m, IsNil)
+	c.Assert(err, NotNil)
+	c.Assert(err.Error(), Equals, "Timed out.")
+}
+
+func (s *S) TestFakeQPutWithTimeout(c *C) {
+	q := FakeQ{}
+	msg := queue.Message{Action: "do-something"}
+	err := q.Put(&msg, 1e6)
+	c.Assert(err, IsNil)
+	_, err = q.Get(1e3)
+	c.Assert(err, NotNil)
+	_, err = q.Get(1e9)
+	c.Assert(err, IsNil)
+}
+
+func (s *S) TestFakeQDelete(c *C) {
+	q := FakeQ{}
+	err := q.Delete(nil)
+	c.Assert(err, IsNil)
+}
+
+func (s *S) TestFakeQRelease(c *C) {
+	q := FakeQ{}
+	msg := queue.Message{Action: "do-something"}
+	err := q.Put(&msg, 0)
+	c.Assert(err, IsNil)
+	m, err := q.Get(1e6)
+	c.Assert(err, IsNil)
+	err = q.Release(m, 0)
+	c.Assert(err, IsNil)
+	_, err = q.Get(1e6)
+	c.Assert(err, IsNil)
+}
