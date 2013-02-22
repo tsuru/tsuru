3b910ee4709f3fdf99f3914414db599518b26188
Francisco Souza <fss@corp.globo.com>
2013-02-22 17:13:32 -0300

    app: add test for custom error types

---
+++ app/error_test.go
@@ -0,0 +1,21 @@
+// Copyright 2013 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package app
+
+import (
+	"errors"
+	. "launchpad.net/gocheck"
+)
+
+func (s *S) TestValidationError(c *C) {
+	e := ValidationError{Message: "something"}
+	c.Assert(e.Error(), Equals, "something")
+}
+
+func (s *S) TestAppCreationError(c *C) {
+	e := appCreationError{app: "myapp", err: errors.New("failure in app")}
+	expected := `Tsuru failed to create the app "myapp": failure in app`
+	c.Assert(e.Error(), Equals, expected)
+}
