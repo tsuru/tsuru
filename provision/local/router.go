e9959b960f68fa8f62eeceaefc8a9e8e5ad42875
Andrews Medina <andrewsmedina@gmail.com>
2013-03-05 15:43:53 -0300

    provision/local: implemented add route.

--- provision/local/router.go
+++ provision/local/router.go
@@ -1,5 +1,10 @@
 package local
 
+import (
+	"fmt"
+	"github.com/globocom/config"
+)
+
 // route represents an route.
 type route struct {
 	name string
@@ -7,5 +12,25 @@ type route struct {
 }
 
 func AddRoute(name, ip string) error {
-	return nil
+	domain, err := config.GetString("local:domain")
+	if err != nil {
+		return err
+	}
+	routesPath, err := config.GetString("local:routes-path")
+	if err != nil {
+		return err
+	}
+	file, _ := filesystem().Open(routesPath + "/" + name)
+	defer file.Close()
+	template := `server {
+	listen 80;
+	%s.%s;
+	location / {
+		proxy_pass http://%s;
+	}
+}`
+	template = fmt.Sprintf(template, name, domain, ip)
+	data := []byte(template)
+	_, err = file.Write(data)
+	return err
 }
--- provision/local/router_test.go
+++ provision/local/router_test.go
@@ -1,10 +1,31 @@
 package local
 
 import (
+	"github.com/globocom/config"
+	"github.com/globocom/tsuru/fs/testing"
+	"io/ioutil"
 	. "launchpad.net/gocheck"
 )
 
 func (s *S) TestAddRoute(c *C) {
+	config.Set("local:domain", "andrewzito.com")
+	config.Set("local:routes-path", "testdata")
+	rfs := &testing.RecordingFs{}
+	fsystem = rfs
+	defer func() {
+		fsystem = nil
+	}()
 	err := AddRoute("name", "127.0.0.1")
 	c.Assert(err, IsNil)
+	file, _ := rfs.Open("testdata/name")
+	data, err := ioutil.ReadAll(file)
+	c.Assert(err, IsNil)
+	expected := `server {
+	listen 80;
+	name.andrewzito.com;
+	location / {
+		proxy_pass http://127.0.0.1;
+	}
+}`
+	c.Assert(string(data), Equals, expected)
 }
