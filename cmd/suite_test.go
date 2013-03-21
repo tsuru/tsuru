93d489e1e4014d465085212548c4c61893ea5682
Francisco Souza <fss@corp.globo.com>
2013-03-21 15:15:07 -0300

    cmd: fix the build

--- cmd/suite_test.go
+++ cmd/suite_test.go
@@ -11,13 +11,15 @@ import (
 	"launchpad.net/gocheck"
 	"net/http"
 	"os"
+	"os/exec"
 	"testing"
 )
 
 func Test(t *testing.T) { gocheck.TestingT(t) }
 
 type S struct {
-	stdin *os.File
+	stdin   *os.File
+	recover []string
 }
 
 type transport struct {
@@ -48,6 +50,26 @@ func (t *conditionalTransport) RoundTrip(req *http.Request) (*http.Response, err
 		return &http.Response{Body: nil, StatusCode: 500}, errors.New("condition failed")
 	}
 	return t.transport.RoundTrip(req)
+}
+
+func (s *S) SetUpSuite(c *gocheck.C) {
+	targetFile := os.Getenv("HOME") + "/.tsuru_target"
+	_, err := os.Stat(targetFile)
+	if err == nil {
+		old := targetFile + ".old"
+		s.recover = []string{"mv", old, targetFile}
+		exec.Command("mv", targetFile, old)
+	} else {
+		s.recover = []string{"rm", targetFile}
+	}
+	f, err := os.Create(targetFile)
+	c.Assert(err, gocheck.IsNil)
+	f.Write([]byte("http://localhost"))
+	f.Close()
+}
+
+func (s *S) TearDownSuite(c *gocheck.C) {
+	exec.Command(s.recover[0], s.recover[1:]...).Run()
 }
 
 func (s *S) SetUpTest(c *gocheck.C) {
