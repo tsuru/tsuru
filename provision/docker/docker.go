18bfa0c256d2edad2c32f5492d660cf0fa37f3c1
Danilo Bardusco <danilo@bardusco.com>
2013-03-29 21:18:14 -0300

    provision: initial work on docker provisioner

---
+++ provision/docker/docker.go
@@ -0,0 +1,89 @@
+// Copyright 2013 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package docker
+
+import (
+	"github.com/globocom/config"
+	"github.com/globocom/tsuru/fs"
+	"github.com/globocom/tsuru/log"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+	"time"
+    //"encoding/json"
+)
+
+var fsystem fs.Fs
+
+func filesystem() fs.Fs {
+	if fsystem == nil {
+		fsystem = fs.OsFs{}
+	}
+	return fsystem
+}
+
+// container represents an docker container with the given name.
+type container struct {
+	name string
+}
+
+// runCmd executes commands and log the given stdout and stderror.
+func runCmd(cmd string, args ...string) error {
+	command := exec.Command(cmd, args...)
+	out, err := command.CombinedOutput()
+	log.Printf("running the cmd: %s with the args: %s", cmd, args)
+	log.Print(string(out))
+	return err
+}
+
+// ip returns the ip for the container.
+func (c *container) ip() string {
+	timeout, err := config.GetInt("local:ip-timeout")
+	if err != nil {
+		timeout = 60
+	}
+	quit := time.After(time.Duration(timeout) * time.Second)
+	tick := time.Tick(2 * time.Second)
+	for {
+		select {
+		case <-tick:
+			file, _ := filesystem().Open("/var/lib/misc/dnsmasq.leases")
+			data, _ := ioutil.ReadAll(file)
+			log.Print("dnsmasq.leases")
+			log.Print(string(data))
+			for _, line := range strings.Split(string(data), "\n") {
+				if strings.Index(line, c.name) != -1 {
+					log.Printf("ip in %s", line)
+					return strings.Split(line, " ")[2]
+				}
+			}
+		case <-quit:
+			return ""
+		default:
+			time.Sleep(1 * time.Second)
+		}
+	}
+	return ""
+}
+
+// create creates a docker container with base template by default.
+func (c *container) create() error {
+	return runCmd("docker", "run", "-d", "base", "/bin/bash", c.name)
+}
+
+// start starts a docker container.
+func (c *container) start() error {
+	return runCmd("docker", "start", c.name)
+}
+
+// stop stops a docker container.
+func (c *container) stop() error {
+	return runCmd("docker", "stop", c.name)
+}
+
+// destroy destory a docker container.
+func (c *container) destroy() error {
+	return runCmd("docker", "rm", c.name)
+}
---
+++ provision/docker/router.go
@@ -0,0 +1,40 @@
+// Copyright 2013 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package docker
+
+import (
+	"fmt"
+	"github.com/globocom/config"
+	"os/exec"
+)
+
+func AddRoute(name, ip string) error {
+	domain, err := config.GetString("local:domain")
+	if err != nil {
+		return err
+	}
+	routesPath, err := config.GetString("local:routes-path")
+	if err != nil {
+		return err
+	}
+	file, _ := filesystem().Create(routesPath + "/" + name)
+	defer file.Close()
+	template := `server {
+	listen 80;
+	%s.%s;
+	location / {
+		proxy_pass http://%s;
+	}
+}`
+	template = fmt.Sprintf(template, name, domain, ip)
+	data := []byte(template)
+	_, err = file.Write(data)
+	return err
+}
+
+func RestartRouter() error {
+	cmd := exec.Command("sudo", "service", "nginx", "restart")
+	return cmd.Run()
+}
