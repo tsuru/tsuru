e16493297ff6bd8e3ff5d83baa5820d998316f01
Francisco Souza <fss@corp.globo.com>
2013-04-02 17:26:37 -0300

    misc/check-license: exit with non-zero status when fail

--- misc/check-license.sh
+++ misc/check-license.sh
@@ -4,11 +4,16 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+status=0
+
 for f in `grep "Copyright 2012" -r . -l | grep -v check-license.sh`
 do
 	date=`git log -1 --format="%ad" --date=short -- $f`
 	if [ `echo "$date" | grep ^2013` ]
 	then
 		echo $f $date
+		status=1
 	fi
 done
+
+exit $status
