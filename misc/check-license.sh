0dfffb452ee494802bbb75ec019d8247e390d596
Francisco Souza <fss@corp.globo.com>
2013-04-02 17:25:15 -0300

    misc/check-license: add license header

--- misc/check-license.sh
+++ misc/check-license.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -e
 
+# Copyright 2013 tsuru authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 for f in `grep "Copyright 2012" -r . -l`
 do
 	date=`git log -1 --format="%ad" --date=short -- $f`
