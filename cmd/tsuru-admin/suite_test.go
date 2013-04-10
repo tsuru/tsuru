b43e6e489796798df9f3f3f84533022df2a2f130
Francisco Souza <fss@corp.globo.com>
2013-04-10 19:05:06 -0300

    cmd/tsuru-admin: fix test suite
    Should fix the build.

--- cmd/tsuru-admin/suite_test.go
+++ cmd/tsuru-admin/suite_test.go
@@ -6,10 +6,34 @@ package main
 
 import (
 	"launchpad.net/gocheck"
+	"os"
+	"os/exec"
 	"testing"
 )
 
-type S struct{}
+type S struct {
+	recover []string
+}
+
+func (s *S) SetUpSuite(c *gocheck.C) {
+	targetFile := os.Getenv("HOME") + "/.tsuru_target"
+	_, err := os.Stat(targetFile)
+	if err == nil {
+		old := targetFile + ".old"
+		s.recover = []string{"mv", old, targetFile}
+		exec.Command("mv", targetFile, old).Run()
+	} else {
+		s.recover = []string{"rm", targetFile}
+	}
+	f, err := os.Create(targetFile)
+	c.Assert(err, gocheck.IsNil)
+	f.Write([]byte("http://localhost"))
+	f.Close()
+}
+
+func (s *S) TearDownSuite(c *gocheck.C) {
+	exec.Command(s.recover[0], s.recover[1:]...).Run()
+}
 
 var _ = gocheck.Suite(&S{})
 
