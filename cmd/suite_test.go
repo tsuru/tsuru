97c8decfa76e64d9ebc000511c594220534ccd18
Francisco Souza <fss@corp.globo.com>
2013-04-10 18:31:35 -0300

    cmd: use Transport and ConditionalTransport from testing pkg

--- cmd/suite_test.go
+++ cmd/suite_test.go
@@ -6,10 +6,7 @@ package cmd
 
 import (
 	"bytes"
-	"errors"
-	"io/ioutil"
 	"launchpad.net/gocheck"
-	"net/http"
 	"os"
 	"os/exec"
 	"testing"
@@ -22,35 +19,8 @@ type S struct {
 	recover []string
 }
 
-type transport struct {
-	msg     string
-	status  int
-	headers map[string][]string
-}
-
-type conditionalTransport struct {
-	transport
-	condFunc func(*http.Request) bool
-}
-
 var _ = gocheck.Suite(&S{})
 var manager *Manager
-
-func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
-	resp = &http.Response{
-		Body:       ioutil.NopCloser(bytes.NewBufferString(t.msg)),
-		StatusCode: t.status,
-		Header:     http.Header(t.headers),
-	}
-	return resp, nil
-}
-
-func (t *conditionalTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	if !t.condFunc(req) {
-		return &http.Response{Body: nil, StatusCode: 500}, errors.New("condition failed")
-	}
-	return t.transport.RoundTrip(req)
-}
 
 func (s *S) SetUpSuite(c *gocheck.C) {
 	targetFile := os.Getenv("HOME") + "/.tsuru_target"
