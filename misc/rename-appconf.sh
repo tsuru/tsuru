6c8be88e0641c8e75dff5ebedfd9194f04d2af33
Francisco Souza <fss@corp.globo.com>
2013-04-15 11:19:53 -0300

    misc: added script to fix app.conf
    This script renames app.conf to app.yaml, and adapt the yaml structure.
    
    Related to #363.

---
+++ misc/rename-appconf.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -f app.conf ]
+then
+	echo hooks: > app.yaml
+	sed -e 's/^/  /' app.conf >> app.yaml
+	git rm -q app.conf
+	echo "File app.conf successfully renamed to app.yaml."
+	echo "Please commit the change and deploy it."
+fi
