5bc432bea193799ec59f322829cc50a293e2ef1c
Francisco Souza <fss@corp.globo.com>
2013-04-15 14:35:12 -0300

    misc/rename-appconf: copyright notice

--- misc/rename-appconf.sh
+++ misc/rename-appconf.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Copyright 2013 tsuru authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 if [ -f app.conf ]
 then
 	echo hooks: > app.yaml
