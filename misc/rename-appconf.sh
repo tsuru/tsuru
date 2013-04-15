f780ecbaa3434a22c0091ef0e629b4807b927681
Francisco Souza <fss@corp.globo.com>
2013-04-15 14:33:21 -0300

    misc/rename-appconf: also add app.yaml

--- misc/rename-appconf.sh
+++ misc/rename-appconf.sh
@@ -5,6 +5,7 @@ then
 	echo hooks: > app.yaml
 	sed -e 's/^/  /' app.conf >> app.yaml
 	git rm -q app.conf
+	git add app.yaml
 	echo "File app.conf successfully renamed to app.yaml."
 	echo "Please commit the change and deploy it."
 fi
