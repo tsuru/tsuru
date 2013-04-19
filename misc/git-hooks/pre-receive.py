68096384aa10d0d0e4cf6c7a9fa3859438cb6cc3
Francisco Souza <fss@corp.globo.com>
2013-04-18 23:30:26 -0300

    misc/git-hooks: remove Token-Owner header

--- misc/git-hooks/pre-receive.py
+++ misc/git-hooks/pre-receive.py
@@ -7,12 +7,11 @@ import sys
 
 tsuru_host = os.environ.get("TSURU_HOST", "")
 token = os.environ.get("TSURU_TOKEN", "")
-owner = os.environ.get("TSURU_TOKEN_OWNER", "")
 app_name = os.getcwd().split("/")[-1].replace(".git", "")
 timeout = 1800
 
 try:
-    headers = {"Authorization": token, "Token-Owner": owner}
+    headers = {"Authorization": token}
     url = "{0}/apps/{1}/avaliable".format(tsuru_host, app_name)
     request = urllib2.Request(url, headers=headers)
     f = urllib2.urlopen(request, timeout=timeout)
