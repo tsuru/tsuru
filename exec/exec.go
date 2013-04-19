2146c2140c7a1a7dc5d790ba410311809d60a9db
Andrews Medina <andrewsmedina@gmail.com>
2013-04-19 17:06:32 -0300

    exec: implemented exec for OsExecutor.

--- exec/exec.go
+++ exec/exec.go
@@ -6,15 +6,22 @@
 // abstraction layer.
 package exec
 
-import "io"
+import (
+	"io"
+	"os/exec"
+)
 
 type Executor interface {
 	// Execute executes the specified command.
-	Execute(cmds []string, stdin io.Reader, stdout, stderr io.Writer) error
+	Execute(cmd string, args []string, stdin io.Reader, stdout, stderr io.Writer) error
 }
 
 type OsExecutor struct{}
 
-func (OsExecutor) Execute(cmds []string, stdin io.Reader, stdout, stderr io.Writer) error {
-	return nil
+func (OsExecutor) Execute(cmd string, args []string, stdin io.Reader, stdout, stderr io.Writer) error {
+	c := exec.Command(cmd, args...)
+	c.Stdin = stdin
+	c.Stdout = stdout
+	c.Stderr = stderr
+	return c.Run()
 }
--- exec/exec_test.go
+++ exec/exec_test.go
@@ -5,6 +5,8 @@
 package exec
 
 import (
+	"bytes"
+	"github.com/globocom/commandmocker"
 	"launchpad.net/gocheck"
 	"testing"
 )
@@ -17,4 +19,31 @@ func Test(t *testing.T) { gocheck.TestingT(t) }
 
 func (s *S) TestOsExecutorImplementsExecutor(c *gocheck.C) {
 	var _ Executor = OsExecutor{}
+}
+
+func (s *S) TestExecute(c *gocheck.C) {
+	tmpdir, err := commandmocker.Add("ls", "ok")
+	c.Assert(err, gocheck.IsNil)
+	defer commandmocker.Remove(tmpdir)
+	var e OsExecutor
+	var b bytes.Buffer
+	err = e.Execute("ls", []string{"-lsa"}, nil, &b, &b)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(commandmocker.Ran(tmpdir), gocheck.Equals, true)
+	expected := []string{"-lsa"}
+	c.Assert(commandmocker.Parameters(tmpdir), gocheck.DeepEquals, expected)
+	c.Assert(b.String(), gocheck.Equals, "ok")
+}
+
+func (s *S) TestExecuteWithoutArgs(c *gocheck.C) {
+	tmpdir, err := commandmocker.Add("ls", "ok")
+	c.Assert(err, gocheck.IsNil)
+	defer commandmocker.Remove(tmpdir)
+	var e OsExecutor
+	var b bytes.Buffer
+	err = e.Execute("ls", nil, nil, &b, &b)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(commandmocker.Ran(tmpdir), gocheck.Equals, true)
+	c.Assert(commandmocker.Parameters(tmpdir), gocheck.IsNil)
+	c.Assert(b.String(), gocheck.Equals, "ok")
 }
