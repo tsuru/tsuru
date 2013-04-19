849e7ef8870d5bdc7338c55f00989440f8cb36ed
Francisco Souza <fss@corp.globo.com>
2013-04-19 16:10:59 -0300

    cmd/tsuru: add platform-list command
    This command will display a list of available platforms.
    
    Related to #85.

---
+++ cmd/tsuru/platform.go
@@ -0,0 +1,58 @@
+// Copyright 2013 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/globocom/tsuru/cmd"
+	"net/http"
+)
+
+type platform struct {
+	Name string
+}
+
+type platformList struct{}
+
+func (platformList) Run(context *cmd.Context, client cmd.Doer) error {
+	url, err := cmd.GetUrl("/platforms")
+	if err != nil {
+		return err
+	}
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	var platforms []platform
+	resp, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	err = json.NewDecoder(resp.Body).Decode(&platforms)
+	if err != nil {
+		return err
+	}
+	if len(platforms) == 0 {
+		fmt.Fprintln(context.Stdout, "No platforms available.")
+		return nil
+	}
+	fmt.Fprintln(context.Stdout, "Available platforms:")
+	fmt.Fprintln(context.Stdout)
+	for _, p := range platforms {
+		fmt.Fprintf(context.Stdout, "- %s\n", p.Name)
+	}
+	return nil
+}
+
+func (platformList) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:    "platform-list",
+		Usage:   "platform-list",
+		Desc:    "Display the list of available platforms.",
+		MinArgs: 0,
+	}
+}
