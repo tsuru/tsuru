62ee86037bc103177a6e7a1da01ea37470edb6fc
Andrews Medina <andrewsmedina@gmail.com>
2013-04-23 20:54:53 -0300

    exec/testing: added ErrorExecutor.

--- exec/testing/exec.go
+++ exec/testing/exec.go
@@ -8,6 +8,7 @@
 package testing
 
 import (
+	"errors"
 	"io"
 	"reflect"
 	"sync"
@@ -32,6 +33,30 @@ func (e *FakeExecutor) Execute(cmd string, args []string, stdin io.Reader, stdou
 }
 
 func (e *FakeExecutor) ExecutedCmd(cmd string, args []string) bool {
+	e.mut.RLock()
+	defer e.mut.RUnlock()
+	for _, c := range e.cmds {
+		if c.name == cmd && reflect.DeepEqual(c.args, args) {
+			return true
+		}
+	}
+	return false
+}
+
+type ErrorExecutor struct {
+	cmds []command
+	mut  sync.RWMutex
+}
+
+func (e *ErrorExecutor) Execute(cmd string, args []string, stdin io.Reader, stdout, stderr io.Writer) error {
+	c := command{name: cmd, args: args}
+	e.mut.Lock()
+	e.cmds = append(e.cmds, c)
+	e.mut.Unlock()
+	return errors.New("")
+}
+
+func (e *ErrorExecutor) ExecutedCmd(cmd string, args []string) bool {
 	e.mut.RLock()
 	defer e.mut.RUnlock()
 	for _, c := range e.cmds {
