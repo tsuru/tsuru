39b258782e976a182dbfc267e661c3d2f5c3996c
Flavia Missi <flaviamissi@gmail.com>
2013-04-24 15:51:13 -0300

    deploy: created git deploy
    Also refactoring the deploy function (moved from juju provisioner).

--- deploy/git.go
+++ deploy/git.go
@@ -4,34 +4,29 @@ import (
 	"errors"
 	"fmt"
 	"github.com/globocom/tsuru/log"
+	"github.com/globocom/tsuru/provision"
 	"github.com/globocom/tsuru/repository"
 	"io"
 )
 
-type GitDeployer struct{}
-
-func (d *GitDeployer) Deploy(app App, w io.Writer) error {
-	if err := log.Write(w, []byte("\n ---> Tsuru receiving push\n")); err != nil {
-		return err
-	}
-	if err := log.Write(w, []byte("\n ---> Replicating the application repository across units\n")); err != nil {
-		return err
-	}
+func Git(app provision.App, w io.Writer) error {
+	log.Write(w, []byte("\n ---> Tsuru receiving push\n"))
+	log.Write(w, []byte("\n ---> Replicating the application repository across units\n"))
 	out, err := repository.CloneOrPull(app)
 	if err != nil {
 		msg := fmt.Sprintf("Got error while clonning/pulling repository: %s -- \n%s", err.Error(), string(out))
+		log.Write(w, []byte(msg))
 		return errors.New(msg)
 	}
-	if err := log.Write(w, out); err != nil {
-		return err
-	}
-	if err := log.Write(w, []byte("\n ---> Installing dependencies\n")); err != nil {
-		return err
-	}
+	log.Write(w, out)
+	log.Write(w, []byte("\n ---> Installing dependencies\n"))
 	if err := app.InstallDeps(w); err != nil {
+		log.Write(w, []byte(err.Error()))
 		return err
 	}
+	log.Write(w, []byte("\n ---> Restarting application\n"))
 	if err := app.Restart(w); err != nil {
+		log.Write(w, []byte(err.Error()))
 		return err
 	}
 	return log.Write(w, []byte("\n ---> Deploy done!\n\n"))
--- deploy/suite_test.go
+++ deploy/suite_test.go
@@ -2,9 +2,7 @@ package deploy
 
 import (
 	"github.com/globocom/config"
-	"io"
 	"launchpad.net/gocheck"
-	"strings"
 	"testing"
 )
 
@@ -22,28 +20,4 @@ func (s *S) SetUpSuite(c *gocheck.C) {
 func (s *S) TearDownSuite(c *gocheck.C) {
 	config.Unset("git:unit-repo")
 	config.Unset("git:host")
-}
-
-type fakeApp struct {
-	name string
-	cmds []string
-}
-
-func (a *fakeApp) Command(stdin io.Writer, stdout io.Writer, cmd ...string) error {
-	a.cmds = append(a.cmds, strings.Join(cmd, " "))
-	return nil
-}
-
-func (a *fakeApp) Restart(w io.Writer) error {
-	a.cmds = append(a.cmds, "restart")
-	return nil
-}
-
-func (a *fakeApp) InstallDeps(w io.Writer) error {
-	a.cmds = append(a.cmds, "install deps")
-	return nil
-}
-
-func (a *fakeApp) GetName() string {
-	return a.name
 }
