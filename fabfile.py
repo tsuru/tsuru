4240a4b4dd2cb82a7ba348554baff1471fcbb7b3
Francisco Souza <fss@corp.globo.com>
2013-04-29 10:42:15 -0300

    fabfile: added command do deploy hooks

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,7 @@
 # license that can be found in the LICENSE file.
 
 import os
-from fabric.api import abort, cd, env, local, put, run
+from fabric.api import abort, cd, env, local, put, run, sudo
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 env.user = 'ubuntu'
@@ -48,3 +48,16 @@ def deploy(flags="", tags=""):
     send()
     restart()
     clean()
+
+
+def deploy_hooks(path, user="git", group="git"):
+    run("mkdir -p /tmp/git-hooks")
+    put("misc/git-hooks/*", "/tmp/git-hooks")
+    sudo("chown -R %s:%s /tmp/git-hooks" % (user, group))
+    sudo("chmod 755 /tmp/git-hooks/*")
+    out = run("find %s -name \*.git -type d" % path)
+    paths = [p.strip() for p in out.split("\n")]
+    for path in paths:
+        sudo("cp -p /tmp/git-hooks/* %s/hooks" % path)
+    sudo("rm /tmp/git-hooks/*")
+    sudo("rmdir /tmp/git-hooks")
