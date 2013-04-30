298e9a9469f271b5927ffd7e91f19222aa425415
Flavia Missi <flaviamissi@gmail.com>
2013-04-30 18:03:46 -0300

    misc/docker-setup.bash: added license

--- misc/docker-setup.bash
+++ misc/docker-setup.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Copyright 2013 tsuru authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+# This script is used to install tsuru with lxc provisioner.
+
 function update_ubuntu() {
     echo "Updating and upgrading"
     sudo apt-get update
