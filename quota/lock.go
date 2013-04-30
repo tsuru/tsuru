e9b6db8e6b576a1aeff7841dc786e36a45b69154
Francisco Souza <fss@corp.globo.com>
2013-04-30 18:24:41 -0300

    quota: added multiLocker type
    We can move it to the safe package later, if we need :)

---
+++ quota/lock.go
@@ -0,0 +1,30 @@
+// Copyright 2013 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package quota
+
+import "sync"
+
+type multiLocker struct {
+	m   map[string]*sync.Mutex
+	mut sync.Mutex
+}
+
+func (l *multiLocker) Lock(name string) {
+	l.mut.Lock()
+	defer l.mut.Unlock()
+	_, ok := l.m[name]
+	if !ok {
+		l.m[name] = new(sync.Mutex)
+	}
+	l.m[name].Lock()
+}
+
+func (l *multiLocker) Unlock(name string) {
+	l.mut.Lock()
+	defer l.mut.Unlock()
+	l.m[name].Unlock()
+}
+
+var locker = multiLocker{m: make(map[string]*sync.Mutex)}
