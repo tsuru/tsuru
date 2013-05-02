92637956c98d55c2e5b40bd436dbcf8de117f275
Francisco Souza <fss@corp.globo.com>
2013-05-02 11:35:18 -0300

    quota: added example for the Set function
    Related to #382.

---
+++ quota/example_test.go
@@ -0,0 +1,25 @@
+// Copyright 2013 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package quota_test
+
+import (
+	"github.com/globocom/tsuru/quota"
+)
+
+func ExampleSet() {
+	err := quota.Create("me@tsuru.io", 3)
+	if err != nil {
+		panic(err)
+	}
+	quota.Reserve("me@tsuru.io", "me/0")
+	quota.Reserve("me@tsuru.io", "me/1")
+	quota.Reserve("me@tsuru.io", "me/2")
+	quota.Set("me@tsuru.io", 2)
+	quota.Reserve("me@tsuru.io", "me/3") // ErrQuotaExceeded
+	quota.Release("me@tsuru.io", "me/2")
+	quota.Reserve("me@tsuru.io", "me/3") // ErrQuotaExceeded
+	quota.Release("me@tsuru.io", "me/1")
+	quota.Reserve("me@tsuru.io", "me/3") // Everything is ok now
+}
