dc833f41bdeb0c676f3549b28bffad9d346a2e86
Francisco Souza <fss@corp.globo.com>
2013-05-08 17:25:26 -0300

    misc/test-hooks: shameless TODO removal

--- misc/test-hooks.bash
+++ misc/test-hooks.bash
@@ -4,8 +4,6 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-# TODO(fss): this script is a hack. I should rewrite it :)
-
 function clean() {
 	rm tsr .copy.conf
 }
