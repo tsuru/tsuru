2547f26919d99734dac295f9f776166ec5dffad8
Francisco Souza <fss@corp.globo.com>
2013-05-10 10:11:57 -0300

    repository: remove file doc.go

--- repository/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2012 tsuru authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package repository contains types and function for git repository
-// interaction.
-package repository
