bc57d69cb29f68a486c22535a04e971c9182cbcb
Francisco Souza <fss@corp.globo.com>
2013-05-10 11:04:28 -0300

    provision/lxc: fix deploy related test

--- provision/lxc/provisioner_test.go
+++ provision/lxc/provisioner_test.go
@@ -6,6 +6,7 @@ package lxc
 
 import (
 	"bytes"
+	"github.com/globocom/commandmocker"
 	"github.com/globocom/config"
 	etesting "github.com/globocom/tsuru/exec/testing"
 	fstesting "github.com/globocom/tsuru/fs/testing"
@@ -115,6 +116,9 @@ func (s *S) TestProvisionerRestart(c *gocheck.C) {
 // }
 
 func (s *S) TestDeploy(c *gocheck.C) {
+	tmpdir, err := commandmocker.Add("ssh", "")
+	c.Assert(err, gocheck.IsNil)
+	defer commandmocker.Remove(tmpdir)
 	config.Set("git:unit-repo", "test/dir")
 	config.Set("git:host", "gandalf.com")
 	defer func() {
@@ -124,42 +128,8 @@ func (s *S) TestDeploy(c *gocheck.C) {
 	app := testing.NewFakeApp("cribcaged", "python", 1)
 	w := &bytes.Buffer{}
 	p := LXCProvisioner{}
-	err := p.Deploy(app, w)
+	err = p.Deploy(app, w)
 	c.Assert(err, gocheck.IsNil)
-	expected := make([]string, 3)
-	// also ensures execution order
-	expected[0] = "git clone git://gandalf.com/cribcaged.git test/dir --depth 1" // the command expected to run on the units
-	expected[1] = "install deps"
-	expected[2] = "restart"
-	c.Assert(app.Commands, gocheck.DeepEquals, expected)
-}
-
-func (s *S) TestDeployLogsActions(c *gocheck.C) {
-	config.Set("git:unit-repo", "test/dir")
-	config.Set("git:host", "gandalf.com")
-	defer func() {
-		config.Unset("git:unit-repo")
-		config.Unset("git:host")
-	}()
-	app := testing.NewFakeApp("cribcaged", "python", 1)
-	w := &bytes.Buffer{}
-	p := LXCProvisioner{}
-	err := p.Deploy(app, w)
-	c.Assert(err, gocheck.IsNil)
-	logs := w.String()
-	expected := `
- ---> Tsuru receiving push
-
- ---> Replicating the application repository across units
-
- ---> Installing dependencies
-
- ---> Restarting application
-
- ---> Deploy done!
-
-`
-	c.Assert(logs, gocheck.Equals, expected)
 }
 
 func (s *S) TestProvisionerDestroy(c *gocheck.C) {
