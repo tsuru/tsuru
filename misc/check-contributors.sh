0ce7a82553d1c7581118d1f9e8f65ceb0e8a9323
Francisco Souza <fss@corp.globo.com>
2013-05-14 12:55:33 -0300

    misc: add check-contributors script

---
+++ misc/check-contributors.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Copyright 2013 tsuru authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+status=0
+
+for email in `git log --format=%ae | sort | uniq`
+do
+	grep -q $email CONTRIBUTORS
+	if [ $? != 0  ]
+	then
+		echo "ERROR: $email is not in the CONTRIBUTORS file."
+		status=1
+	fi
+done
+
+exit $status
