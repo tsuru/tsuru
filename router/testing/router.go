9a266cd4a0362187720d49afcccb41c579a1862d
Francisco Souza <f@souza.cc>
2013-05-23 11:49:37 -0300

    Revert "router/testing: improve implementation"
    This reverts commit a0ec206ad3139079c2ac442d06fdf2761608c008.

--- router/testing/router.go
+++ router/testing/router.go
@@ -10,90 +10,60 @@ import (
 	"sync"
 )
 
-var ErrBackendNotFound = errors.New("Backend not found")
-
 func init() {
-	router.Register("fake", &FakeRouter{backends: make(map[string][]string)})
+	router.Register("fake", &FakeRouter{})
 }
 
 type FakeRouter struct {
-	backends map[string][]string
+	routes   map[string]string
+	backends []string
 	mutex    sync.Mutex
 }
 
-func (r *FakeRouter) HasBackend(name string) bool {
+func (r *FakeRouter) AddBackend(name string) error {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
-	_, ok := r.backends[name]
-	return ok
+	r.backends = append(r.backends, name)
+	return nil
 }
 
-func (r *FakeRouter) HasRoute(name, address string) bool {
+func (r *FakeRouter) RemoveBackend(name string) error {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
-	routes, ok := r.backends[name]
-	if !ok {
-		return false
+	for i, b := range r.backends {
+		if name == b {
+			r.backends[i], r.backends = r.backends[len(r.backends)-1], r.backends[:len(r.backends)-1]
+			break
+		}
 	}
-	for _, route := range routes {
-		if route == address {
+	return nil
+}
+
+func (r *FakeRouter) HasBackend(name string) bool {
+	for _, b := range r.backends {
+		if name == b {
 			return true
 		}
 	}
 	return false
 }
 
-func (r *FakeRouter) AddBackend(name string) error {
-	if r.HasBackend(name) {
-		return errors.New("Backend already exists")
-	}
-	r.mutex.Lock()
-	defer r.mutex.Unlock()
-	r.backends[name] = nil
-	return nil
-}
-
-func (r *FakeRouter) RemoveBackend(name string) error {
-	if !r.HasBackend(name) {
-		return ErrBackendNotFound
-	}
-	r.mutex.Lock()
-	defer r.mutex.Unlock()
-	delete(r.backends, name)
-	return nil
-}
-
 func (r *FakeRouter) AddRoute(name, ip string) error {
-	if !r.HasBackend(name) {
-		return ErrBackendNotFound
-	}
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
-	routes := r.backends[name]
-	routes = append(routes, ip)
-	r.backends[name] = routes
+	if r.routes == nil {
+		r.routes = make(map[string]string)
+	}
+	r.routes[name] = ip
 	return nil
 }
 
 func (r *FakeRouter) RemoveRoute(name, ip string) error {
-	if !r.HasBackend(name) {
-		return ErrBackendNotFound
-	}
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
-	index := -1
-	routes := r.backends[name]
-	for i := range routes {
-		if routes[i] == ip {
-			index = i
-			break
-		}
+	if r.routes != nil {
+		delete(r.routes, name)
 	}
-	if index < 0 {
-		return errors.New("Route not found")
-	}
-	routes[index] = routes[len(routes)-1]
-	r.backends[name] = routes[:len(routes)-1]
 	return nil
 }
 
@@ -105,11 +75,18 @@ func (FakeRouter) RemoveCNAME(cname, address string) error {
 	return nil
 }
 
+func (r *FakeRouter) HasRoute(name string) bool {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	_, ok := r.routes[name]
+	return ok
+}
+
 func (r *FakeRouter) Addr(name string) (string, error) {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
-	if v, ok := r.backends[name]; ok {
-		return v[0], nil
+	if v, ok := r.routes[name]; ok {
+		return v, nil
 	}
-	return "", ErrBackendNotFound
+	return "", errors.New("Route not found")
 }
