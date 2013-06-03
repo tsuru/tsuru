54a179205ec2e977783af4c8d2e607e91162e324
Flavia Missi <flaviamissi@gmail.com>
2013-06-03 14:44:42 -0300

    router/hipache/router: deferring close of redis connections

--- router/hipache/router.go
+++ router/hipache/router.go
@@ -55,6 +55,7 @@ func (hipacheRouter) AddBackend(name string) error {
 	if err != nil {
 		return &routeError{"add", err}
 	}
+	defer conn.Close()
 	_, err = conn.Do("RPUSH", frontend, name)
 	if err != nil {
 		return &routeError{"add", err}
@@ -72,6 +73,7 @@ func (r hipacheRouter) RemoveBackend(name string) error {
 	if err != nil {
 		return &routeError{"remove", err}
 	}
+	defer conn.Close()
 	_, err = conn.Do("DEL", frontend)
 	if err != nil {
 		return &routeError{"remove", err}
@@ -118,6 +120,7 @@ func (hipacheRouter) addRoute(name, address string) error {
 	if err != nil {
 		return &routeError{"add", err}
 	}
+	defer conn.Close()
 	_, err = conn.Do("RPUSH", name, address)
 	if err != nil {
 		return &routeError{"add", err}
@@ -167,6 +170,7 @@ func (hipacheRouter) SetCName(cname, name string) error {
 	if err != nil {
 		return &routeError{"addCName", err}
 	}
+	defer conn.Close()
 	addresses, err := redis.Strings(conn.Do("LRANGE", frontend, 0, -1))
 	if err != nil {
 		return &routeError{"get", err}
@@ -190,6 +194,7 @@ func (r hipacheRouter) UnsetCName(cname, name string) error {
 	if err != nil {
 		return &routeError{"removeCName", err}
 	}
+	defer conn.Close()
 	_, err = conn.Do("DEL", "cname:"+name)
 	if err != nil {
 		return &routeError{"removeCName", err}
@@ -211,6 +216,7 @@ func (hipacheRouter) Addr(name string) (string, error) {
 	if err != nil {
 		return "", &routeError{"get", err}
 	}
+	defer conn.Close()
 	reply, err := conn.Do("LRANGE", frontend, 0, 0)
 	if err != nil {
 		return "", &routeError{"get", err}
@@ -227,6 +233,7 @@ func (hipacheRouter) removeElement(name, address string) error {
 	if err != nil {
 		return &routeError{"remove", err}
 	}
+	defer conn.Close()
 	_, err = conn.Do("LREM", name, 0, address)
 	if err != nil {
 		return &routeError{"remove", err}
