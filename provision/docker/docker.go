fa5c39e090c82477d6f2a512a54f8cd23e093f18
Andrews Medina <andrewsmedina@gmail.com>
2013-06-07 20:19:25 -0300

    docker: removed getSSHCommands.

--- provision/docker/docker.go
+++ provision/docker/docker.go
@@ -15,11 +15,7 @@ import (
 	"github.com/globocom/tsuru/provision"
 	"github.com/globocom/tsuru/repository"
 	"io"
-	"io/ioutil"
 	"labix.org/v2/mgo/bson"
-	"os"
-	"os/user"
-	"path"
 	"strings"
 	"time"
 )
@@ -39,38 +35,6 @@ func runCmd(cmd string, args ...string) (string, error) {
 	err := executor().Execute(cmd, args, nil, &out, &out)
 	log.Printf("running the cmd: %s with the args: %s", cmd, args)
 	return out.String(), err
-}
-
-func getSSHCommands() ([]string, error) {
-	addKeyCommand, err := config.GetString("docker:ssh:add-key-cmd")
-	if err != nil {
-		return nil, err
-	}
-	keyFile, err := config.GetString("docker:ssh:public-key")
-	if err != nil {
-		if u, err := user.Current(); err == nil {
-			keyFile = path.Join(u.HomeDir, ".ssh", "id_rsa.pub")
-		} else {
-			keyFile = os.ExpandEnv("${HOME}/.ssh/id_rsa.pub")
-		}
-	}
-	f, err := filesystem().Open(keyFile)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	keyContent, err := ioutil.ReadAll(f)
-	if err != nil {
-		return nil, err
-	}
-	sshdPath, err := config.GetString("docker:ssh:sshd-path")
-	if err != nil {
-		sshdPath = "/usr/sbin/sshd"
-	}
-	return []string{
-		fmt.Sprintf("%s %s", addKeyCommand, bytes.TrimSpace(keyContent)),
-		sshdPath + " -D",
-	}, nil
 }
 
 func commandToRun(app provision.App) ([]string, error) {
