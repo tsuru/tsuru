cf084b4dbc36b460a4d13db105a259bb22b30057
Andrews Medina <andrewsmedina@gmail.com>
2013-06-11 18:39:07 -0300

    docker: added -d for deploy cmds.

--- provision/docker/commands.go
+++ provision/docker/commands.go
@@ -30,7 +30,7 @@ func deployCmds(app provision.App, version string) ([]string, error) {
 	}
 	imageName := getImage(app)
 	appRepo := repository.ReadOnlyURL(app.GetName())
-	cmds := []string{docker, "run", imageName, deployCmd, appRepo}
+	cmds := []string{docker, "run", "-d", imageName, deployCmd, appRepo}
 	return cmds, nil
 }
 
