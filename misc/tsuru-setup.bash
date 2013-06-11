07478b668d192040cd527b2b7087c42a7b13f962
Andrews Medina <andrewsmedina@gmail.com>
2013-06-11 09:40:33 -0300

    misc/tsuru-setup: preserve environments on add key for mongod.

--- misc/tsuru-setup.bash
+++ misc/tsuru-setup.bash
@@ -16,7 +16,7 @@ function update_ubuntu() {
 
 function install_mongodb() {
     echo "Installing mongodb"
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+    sudo -E apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
     sudo bash -c 'echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" > /etc/apt/sources.list.d/10gen.list'
     sudo apt-get update -y
     sudo apt-get install mongodb-10gen -y --force-yes
