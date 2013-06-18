edff32001a82b171de8e3566233ed498da170734
Francisco Souza <fss@corp.globo.com>
2013-06-18 11:47:02 -0300

    misc/insert-platforms: add license banner and usage

--- misc/insert-platforms.js
+++ misc/insert-platforms.js
@@ -1,4 +1,14 @@
+// Copyright 2013 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This script register some platforms in the database.
+//
+// Usage, from the command line:
+//
+//   % mongo <database-name> misc/insert-platforms.js
+
 var platforms = ["nodejs", "php", "python", "ruby", "static"];
-for(var i in platforms) {
+for(var i = 0; i < platforms.length; i++) {
 	db.platforms.insert({id: platforms[i]});
 }
