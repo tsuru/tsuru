f9b8618e6648dbe3bd4a0d252ad6f89894b7d883
Francisco Souza <fss@corp.globo.com>
2013-06-28 15:40:53 -0300

    misc/build-server: make the script more atomic

--- misc/build-server.bash
+++ misc/build-server.bash
@@ -12,7 +12,11 @@ echo -n "Creating \"$destination_dir\" directory... "
 mkdir -p $destination_dir
 echo "ok"
 
+echo -n "Downloading dependencies... "
+go get -u -d github.com/globocom/tsuru/cmd/tsr
+echo "ok"
+
 echo "Building tsr... "
-go build -a -o $destination_dir/tsr github.com/globocom/tsuru/cmd/tsr
+go build -x -a -o $destination_dir/tsr github.com/globocom/tsuru/cmd/tsr
 tar -C $destination_dir -czf $destination_dir/tsr.tar.gz tsr
 rm $destination_dir/tsr
