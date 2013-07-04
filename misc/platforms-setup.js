eb9fd12ef96ae8b91d62f29e408a35ead6b4b173
Flavia Missi <flaviamissi@gmail.com>
2013-07-04 11:04:44 -0300

    misc/platforms-setup.js: fixing field name

--- misc/platforms-setup.js
+++ misc/platforms-setup.js
@@ -1,10 +1,10 @@
 function addPlatforms() {
     var platforms = new Array(
-        {"name": "static"},
-        {"name": "python"},
-        {"name": "ruby"},
-        {"name": "nodejs"},
-        {"name": "php"}
+        {"_id": "static"},
+        {"_id": "python"},
+        {"_id": "ruby"},
+        {"_id": "nodejs"},
+        {"_id": "php"}
     );
     for (var i=0; i<platforms.length; i++) {
         db["platforms"].save(platforms[i]);
