204ec3666e3fbe897f52cce95d54e49c1a347162
Andrews Medina <andrewsmedina@gmail.com>
2013-07-05 12:00:09 -0300

    router/elb: implemented add backend.

--- router/elb/router.go
+++ router/elb/router.go
@@ -4,7 +4,13 @@
 
 package elb
 
-import "github.com/globocom/tsuru/router"
+import (
+	"github.com/flaviamissi/go-elb/aws"
+	"github.com/flaviamissi/go-elb/elb"
+	"github.com/globocom/config"
+	"github.com/globocom/tsuru/log"
+	"github.com/globocom/tsuru/router"
+)
 
 func init() {
 	router.Register("elb", elbRouter{})
@@ -12,7 +18,45 @@ func init() {
 
 type elbRouter struct{}
 
+func getELBEndpoint() *elb.ELB {
+	access, err := config.GetString("aws:access-key-id")
+	if err != nil {
+		log.Fatal(err)
+	}
+	secret, err := config.GetString("aws:secret-access-key")
+	if err != nil {
+		log.Fatal(err)
+	}
+	endpoint, err := config.GetString("juju:elb-endpoint")
+	if err != nil {
+		log.Fatal(err)
+	}
+	auth := aws.Auth{AccessKey: access, SecretKey: secret}
+	region := aws.Region{ELBEndpoint: endpoint}
+	return elb.New(auth, region)
+}
+
 func (elbRouter) AddBackend(name string) error {
+	zone, err := config.GetList("juju:elb-avail-zones")
+	if err != nil {
+		return err
+	}
+	options := elb.CreateLoadBalancer{
+		Name: name,
+		Listeners: []elb.Listener{
+			{
+				InstancePort:     80,
+				InstanceProtocol: "HTTP",
+				LoadBalancerPort: 80,
+				Protocol:         "HTTP",
+			},
+		},
+		AvailZones: zone,
+	}
+	_, err = getELBEndpoint().CreateLoadBalancer(&options)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
--- router/elb/router_test.go
+++ router/elb/router_test.go
@@ -5,12 +5,59 @@
 package elb
 
 import (
+	"github.com/flaviamissi/go-elb/aws"
+	"github.com/flaviamissi/go-elb/elb"
+	"github.com/flaviamissi/go-elb/elb/elbtest"
+	"github.com/globocom/config"
+	"github.com/globocom/tsuru/app"
+	"github.com/globocom/tsuru/db"
+	"github.com/globocom/tsuru/queue"
 	"github.com/globocom/tsuru/router"
+	"github.com/globocom/tsuru/testing"
 	"launchpad.net/gocheck"
-	"testing"
+	goTesting "testing"
 )
 
-func Test(t *testing.T) {
+type ELBSuite struct {
+	server      *elbtest.Server
+	client      *elb.ELB
+	conn        *db.Storage
+	cName       string
+	provisioner *testing.FakeProvisioner
+}
+
+var _ = gocheck.Suite(&ELBSuite{})
+
+func (s *ELBSuite) SetUpSuite(c *gocheck.C) {
+	var err error
+	s.server, err = elbtest.NewServer()
+	c.Assert(err, gocheck.IsNil)
+	config.Set("juju:elb-endpoint", s.server.URL())
+	config.Set("juju:use-elb", true)
+	region := aws.SAEast
+	region.ELBEndpoint = s.server.URL()
+	s.client = elb.New(aws.Auth{AccessKey: "some", SecretKey: "thing"}, region)
+	c.Assert(err, gocheck.IsNil)
+	s.cName = "juju_test_elbs"
+	config.Set("juju:elb-collection", s.cName)
+	config.Set("juju:elb-avail-zones", []interface{}{"my-zone-1a", "my-zone-1b"})
+	config.Set("aws:access-key-id", "access")
+	config.Set("aws:secret-access-key", "s3cr3t")
+	config.Set("git:ro-host", "git.tsuru.io")
+	config.Set("queue", "fake")
+	config.Set("juju:units-collection", "juju_units_test_elb")
+	s.provisioner = testing.NewFakeProvisioner()
+	app.Provisioner = s.provisioner
+}
+
+func (s *ELBSuite) TearDownSuite(c *gocheck.C) {
+	config.Unset("juju:use-elb")
+	s.conn.Collection("juju_units_test_elb").Database.DropDatabase()
+	s.server.Quit()
+	queue.Preempt()
+}
+
+func Test(t *goTesting.T) {
 	gocheck.TestingT(t)
 }
 
@@ -23,4 +70,27 @@ func (s *S) TestShouldBeRegistered(c *gocheck.C) {
 	c.Assert(err, gocheck.IsNil)
 	_, ok := r.(elbRouter)
 	c.Assert(ok, gocheck.Equals, true)
+}
+
+func (s *S) TestAddBackend(c *gocheck.C) {
+	server, err := elbtest.NewServer()
+	c.Assert(err, gocheck.IsNil)
+	config.Set("juju:elb-endpoint", server.URL())
+	config.Set("juju:elb-avail-zones", []interface{}{"my-zone-1a", "my-zone-1b"})
+	region := aws.SAEast
+	region.ELBEndpoint = server.URL()
+	router := elbRouter{}
+	err = router.AddBackend("tip")
+	c.Assert(err, gocheck.IsNil)
+	client := elb.New(aws.Auth{AccessKey: "some", SecretKey: "thing"}, region)
+	resp, err := client.DescribeLoadBalancers("tip")
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(resp.LoadBalancerDescriptions, gocheck.HasLen, 1)
+	c.Assert(resp.LoadBalancerDescriptions[0].ListenerDescriptions, gocheck.HasLen, 1)
+	listener := resp.LoadBalancerDescriptions[0].ListenerDescriptions[0].Listener
+	c.Assert(listener.InstancePort, gocheck.Equals, 80)
+	c.Assert(listener.LoadBalancerPort, gocheck.Equals, 80)
+	c.Assert(listener.InstanceProtocol, gocheck.Equals, "HTTP")
+	c.Assert(listener.Protocol, gocheck.Equals, "HTTP")
+	c.Assert(listener.SSLCertificateId, gocheck.Equals, "")
 }
