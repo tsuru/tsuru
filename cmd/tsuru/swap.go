450e051495ee7dadcd3e2d9fc1995a744b9cd870
Andrews Medina <andrewsmedina@gmail.com>
2013-07-13 02:01:12 -0300

    impelemented swap cmd.

--- cmd/tsuru/swap.go
+++ cmd/tsuru/swap.go
@@ -5,7 +5,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/globocom/tsuru/cmd"
+	"net/http"
 )
 
 type swap struct{}
@@ -17,4 +19,21 @@ func (swap) Info() *cmd.Info {
 		Desc:    "Swap router between two apps.",
 		MinArgs: 2,
 	}
+}
+
+func (swap) Run(context *cmd.Context, client *cmd.Client) error {
+	url, err := cmd.GetURL(fmt.Sprintf("/swap?app1=%s&app2=%s", context.Args[0], context.Args[1]))
+	if err != nil {
+		return err
+	}
+	request, err := http.NewRequest("PUT", url, nil)
+	if err != nil {
+		return err
+	}
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(context.Stdout, "Apps successfully swapped!")
+	return nil
 }
--- cmd/tsuru/swap_test.go
+++ cmd/tsuru/swap_test.go
@@ -5,8 +5,11 @@
 package main
 
 import (
+	"bytes"
 	"github.com/globocom/tsuru/cmd"
+	"github.com/globocom/tsuru/testing"
 	"launchpad.net/gocheck"
+	"net/http"
 )
 
 func (s *S) TestSwapInfo(c *gocheck.C) {
@@ -17,4 +20,26 @@ func (s *S) TestSwapInfo(c *gocheck.C) {
 		MinArgs: 2,
 	}
 	c.Assert(swap{}.Info(), gocheck.DeepEquals, expected)
+}
+
+func (s *S) TestSwap(c *gocheck.C) {
+	var buf bytes.Buffer
+	var called bool
+	transport := testing.ConditionalTransport{
+		Transport: testing.Transport{Status: http.StatusOK, Message: ""},
+		CondFunc: func(r *http.Request) bool {
+			called = true
+			return r.Method == "PUT" && r.URL.Path == "/swap"
+		},
+	}
+	context := cmd.Context{
+		Args: []string{"app1", "app2"},
+		Stdout: &buf,
+	}
+	client := cmd.NewClient(&http.Client{Transport: &transport}, nil, manager)
+	err := swap{}.Run(&context, client)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(called, gocheck.Equals, true)
+	expected := "Apps successfully swapped!\n"
+	c.Assert(buf.String(), gocheck.Equals, expected)
 }
