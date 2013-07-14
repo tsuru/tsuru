2f798cdf52a22392f150c1540e5c01d9721ac95f
Flavia Missi <flaviamissi@gmail.com>
2013-07-14 18:17:23 -0300

    misc: moving insert-platform.js to platforms-setup.js
    Closes #503.
    
    Thanks @dgryski

--- misc/platforms-setup.js
+++ misc/platforms-setup.js
@@ -1,14 +1,14 @@
-function addPlatforms() {
-    var platforms = new Array(
-        {"_id": "static"},
-        {"_id": "python"},
-        {"_id": "ruby"},
-        {"_id": "nodejs"},
-        {"_id": "php"}
-    );
-    for (var i=0; i<platforms.length; i++) {
-        db["platforms"].save(platforms[i]);
-    }
-}
+// Copyright 2013 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
-addPlatforms();
+// This script register some platforms in the database.
+//
+// Usage, from the command line:
+//
+//   % mongo <database-name> misc/insert-platforms.js
+
+var platforms = ["nodejs", "php", "python", "ruby", "static"];
+for(var i = 0; i < platforms.length; i++) {
+	db.platforms.insert({_id: platforms[i]});
+}
