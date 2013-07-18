ac345e0fabb676a6d58950708c2f0e308a6c1c48
Francisco Souza <fss@corp.globo.com>
2013-07-18 17:10:56 -0300

    cmd/tsr: read configuration file in main
    This code is replicated through all subcommands. I'm not removing it
    from there, yet.

--- cmd/tsr/main.go
+++ cmd/tsr/main.go
@@ -5,13 +5,32 @@
 package main
 
 import (
+	"bytes"
+	"github.com/globocom/config"
 	"github.com/globocom/tsuru/cmd"
+	"github.com/globocom/tsuru/log"
 	_ "github.com/globocom/tsuru/provision/docker"
 	_ "github.com/globocom/tsuru/provision/juju"
+	"launchpad.net/gnuflag"
 	"os"
 )
 
+var configFile string
+
+func init() {
+	var buf bytes.Buffer
+	fs := gnuflag.NewFlagSet("tsr", gnuflag.ContinueOnError)
+	fs.SetOutput(&buf)
+	fs.StringVar(&configFile, "config", "/etc/tsuru/tsuru.conf", "tsr config file.")
+	fs.StringVar(&configFile, "c", "/etc/tsuru/tsuru.conf", "tsr config file.")
+	fs.Parse(true, os.Args[1:])
+}
+
 func buildManager() *cmd.Manager {
+	err := config.ReadAndWatchConfigFile(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
 	m := cmd.NewManager("tsr", "0.1.0", "", os.Stdout, os.Stderr, os.Stdin)
 	m.Register(&apiCmd{})
 	m.Register(&collectorCmd{})
