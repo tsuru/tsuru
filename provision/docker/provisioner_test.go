d8c790c243721d63385d60385b95cc83b9fa4a70
Francisco Souza <fss@corp.globo.com>
2013-08-09 17:49:13 -0300

    provision/docker: split TestCollectStatus in multiple parts
    Fix #509.

--- provision/docker/provisioner_test.go
+++ provision/docker/provisioner_test.go
@@ -6,9 +6,7 @@ package docker
 
 import (
 	"bytes"
-	"fmt"
 	dockerClient "github.com/fsouza/go-dockerclient"
-	"github.com/globocom/docker-cluster/cluster"
 	"github.com/globocom/tsuru/cmd"
 	"github.com/globocom/tsuru/exec"
 	etesting "github.com/globocom/tsuru/exec/testing"
@@ -19,9 +17,6 @@ import (
 	"labix.org/v2/mgo/bson"
 	"launchpad.net/gocheck"
 	stdlog "log"
-	"net"
-	"net/http"
-	"net/http/httptest"
 	"runtime"
 	"strings"
 	"time"
@@ -402,83 +397,16 @@ func (s *S) TestProvisionerExecuteCommandNoContainers(c *gocheck.C) {
 }
 
 func (s *S) TestCollectStatus(c *gocheck.C) {
-	rtesting.FakeRouter.AddBackend("ashamed")
-	defer rtesting.FakeRouter.RemoveBackend("ashamed")
-	rtesting.FakeRouter.AddBackend("make-up")
-	defer rtesting.FakeRouter.RemoveBackend("make-up")
-	listener, err := net.Listen("tcp", "127.0.0.1:0")
-	c.Assert(err, gocheck.IsNil)
+	defer createTestRoutes("ashamed", "make-up")()
+	listener := startTestListener("127.0.0.1:0")
 	defer listener.Close()
 	listenPort := strings.Split(listener.Addr().String(), ":")[1]
 	var calls int
-	c1Output := fmt.Sprintf(`{
-	"NetworkSettings": {
-		"IpAddress": "127.0.0.1",
-		"IpPrefixLen": 8,
-		"Gateway": "10.65.41.1",
-		"PortMapping": {
-			"Tcp": {"8888": "%s"}
-		}
-	}
-}`, listenPort)
-	c2Output := `{
-	"NetworkSettings": {
-		"IpAddress": "127.0.0.1",
-		"IpPrefixLen": 8,
-		"Gateway": "10.65.41.1",
-		"PortMapping": {
-			"Tcp": {"8889": "9024"}
-		}
-	}
-}`
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		calls++
-		if strings.Contains(r.URL.Path, "/containers/") {
-			if strings.Contains(r.URL.Path, "/containers/9930c24f1c4f") {
-				w.Write([]byte(c2Output))
-			}
-			if strings.Contains(r.URL.Path, "/containers/9930c24f1c5f") {
-				w.Write([]byte(c1Output))
-			}
-		}
-		if strings.Contains(r.URL.Path, "/commit") {
-			w.Write([]byte(`{"Id":"i-1"}`))
-		}
-	}))
-	defer server.Close()
-	oldCluster := dockerCluster()
-	dCluster, err = cluster.New(nil,
-		cluster.Node{ID: "server", Address: server.URL},
-	)
-	c.Assert(err, gocheck.IsNil)
-	defer func() {
-		dCluster = oldCluster
-	}()
-	err = collection().Insert(
-		container{
-			ID: "9930c24f1c5f", AppName: "ashamed", Type: "python",
-			Port: "8888", Status: "running", IP: "127.0.0.1",
-			HostPort: "9023", HostAddr: "127.0.0.1",
-		},
-		container{
-			ID: "9930c24f1c4f", AppName: "make-up", Type: "python",
-			Port: "8889", Status: "running", IP: "127.0.0.4",
-			HostPort: "9025", HostAddr: "127.0.0.1",
-		},
-		container{ID: "9930c24f1c6f", AppName: "make-up", Type: "python", Port: "9090", Status: "error", HostAddr: "127.0.0.1"},
-		container{ID: "9930c24f1c7f", AppName: "make-up", Type: "python", Port: "9090", Status: "created", HostAddr: "127.0.0.1"},
-	)
-	rtesting.FakeRouter.AddRoute("ashamed", fmt.Sprintf("http://127.0.0.1:%s", listenPort))
-	rtesting.FakeRouter.AddRoute("make-up", "http://127.0.0.1:9025")
-	c.Assert(err, gocheck.IsNil)
-	defer collection().RemoveAll(bson.M{"appname": "make-up"})
-	defer collection().RemoveAll(bson.M{"appname": "ashamed"})
-	psOutput := `9930c24f1c5f
-9930c24f1c4f
-9930c24f1c3f
-9930c24f1c6f
-9930c24f1c7f
-`
+	var err error
+	defer startTestDockerServer(listenPort, &calls)()
+	defer insertContainers(listenPort)()
+	fexec, restore := mockExecutor()
+	defer restore()
 	expected := []provision.Unit{
 		{
 			Name:    "9930c24f1c5f",
@@ -503,12 +431,6 @@ func (s *S) TestCollectStatus(c *gocheck.C) {
 			Status:  provision.StatusError,
 		},
 	}
-	output := map[string][][]byte{
-		"ps -q": {[]byte(psOutput)},
-	}
-	fexec := &etesting.FakeExecutor{Output: output}
-	setExecut(fexec)
-	defer setExecut(nil)
 	var p dockerProvisioner
 	units, err := p.CollectStatus()
 	c.Assert(err, gocheck.IsNil)
--- provision/docker/stubs_test.go
+++ provision/docker/stubs_test.go
@@ -4,6 +4,18 @@
 
 package docker
 
+import (
+	"fmt"
+	"github.com/globocom/docker-cluster/cluster"
+	etesting "github.com/globocom/tsuru/exec/testing"
+	rtesting "github.com/globocom/tsuru/router/testing"
+	"labix.org/v2/mgo/bson"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+)
+
 var inspectOut = `
 {
 	"State": {
@@ -20,3 +32,114 @@ var inspectOut = `
 		"PortMapping": {"8888": "34233"}
 	}
 }`
+
+func createTestRoutes(names ...string) func() {
+	for _, name := range names {
+		rtesting.FakeRouter.AddBackend(name)
+	}
+	return func() {
+		for _, name := range names {
+			rtesting.FakeRouter.RemoveBackend(name)
+		}
+	}
+}
+
+func startTestListener(addr string) net.Listener {
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+	return listener
+}
+
+func startTestDockerServer(containerPort string, calls *int) func() {
+	c1Output := fmt.Sprintf(`{
+	"NetworkSettings": {
+		"IpAddress": "127.0.0.1",
+		"IpPrefixLen": 8,
+		"Gateway": "10.65.41.1",
+		"PortMapping": {
+			"Tcp": {"8888": "%s"}
+		}
+	}
+}`, containerPort)
+	c2Output := `{
+	"NetworkSettings": {
+		"IpAddress": "127.0.0.1",
+		"IpPrefixLen": 8,
+		"Gateway": "10.65.41.1",
+		"PortMapping": {
+			"Tcp": {"8889": "9024"}
+		}
+	}
+}`
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		(*calls)++
+		if strings.Contains(r.URL.Path, "/containers/") {
+			if strings.Contains(r.URL.Path, "/containers/9930c24f1c4f") {
+				w.Write([]byte(c2Output))
+			}
+			if strings.Contains(r.URL.Path, "/containers/9930c24f1c5f") {
+				w.Write([]byte(c1Output))
+			}
+		}
+		if strings.Contains(r.URL.Path, "/commit") {
+			w.Write([]byte(`{"Id":"i-1"}`))
+		}
+	}))
+	var err error
+	oldCluster := dockerCluster()
+	dCluster, err = cluster.New(nil,
+		cluster.Node{ID: "server", Address: server.URL},
+	)
+	if err != nil {
+		panic(err)
+	}
+	return func() {
+		server.Close()
+		dCluster = oldCluster
+	}
+}
+
+func insertContainers(containerPort string) func() {
+	err := collection().Insert(
+		container{
+			ID: "9930c24f1c5f", AppName: "ashamed", Type: "python",
+			Port: "8888", Status: "running", IP: "127.0.0.1",
+			HostPort: "9023", HostAddr: "127.0.0.1",
+		},
+		container{
+			ID: "9930c24f1c4f", AppName: "make-up", Type: "python",
+			Port: "8889", Status: "running", IP: "127.0.0.4",
+			HostPort: "9025", HostAddr: "127.0.0.1",
+		},
+		container{ID: "9930c24f1c6f", AppName: "make-up", Type: "python", Port: "9090", Status: "error", HostAddr: "127.0.0.1"},
+		container{ID: "9930c24f1c7f", AppName: "make-up", Type: "python", Port: "9090", Status: "created", HostAddr: "127.0.0.1"},
+	)
+	if err != nil {
+		panic(err)
+	}
+	rtesting.FakeRouter.AddRoute("ashamed", fmt.Sprintf("http://127.0.0.1:%s", containerPort))
+	rtesting.FakeRouter.AddRoute("make-up", "http://127.0.0.1:9025")
+	return func() {
+		collection().RemoveAll(bson.M{"appname": "make-up"})
+		collection().RemoveAll(bson.M{"appname": "ashamed"})
+	}
+}
+
+func mockExecutor() (*etesting.FakeExecutor, func()) {
+	psOutput := `9930c24f1c5f
+9930c24f1c4f
+9930c24f1c3f
+9930c24f1c6f
+9930c24f1c7f
+`
+	output := map[string][][]byte{
+		"ps -q": {[]byte(psOutput)},
+	}
+	fexec := &etesting.FakeExecutor{Output: output}
+	setExecut(fexec)
+	return fexec, func() {
+		setExecut(nil)
+	}
+}
