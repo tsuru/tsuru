ff809873ba0ecd7f1aa00889c570cdd7b0f14da6
Flavia Missi <flaviamissi@gmail.com>
2013-09-09 16:30:15 -0300

    provision/docker/flatten: starting implementation of Flatten function
    Relates to #513

--- provision/docker/flatten.go
+++ provision/docker/flatten.go
@@ -7,6 +7,9 @@
 package docker
 
 import (
+	"bytes"
+	"github.com/dotcloud/docker"
+	dcli "github.com/fsouza/go-dockerclient"
 	"github.com/globocom/tsuru/app"
 	"github.com/globocom/tsuru/db"
 	"github.com/globocom/tsuru/log"
@@ -22,14 +25,55 @@ func imagesToFlatten() []string {
 		return nil
 	}
 	filter := bson.M{"deploys": bson.M{"$mod": []int{20, 0}}}
-	if err := conn.Apps().Find(filter).Select(bson.M{"name": 1}).All(&apps); err != nil {
+	if err := conn.Apps().Find(filter).Select(bson.M{"name": 1, "framework": 1}).All(&apps); err != nil {
 		log.Fatalf("Caught error while getting apps from database: %s", err.Error())
 		panic(err)
 		return nil
 	}
 	images := make([]string, len(apps))
 	for i, a := range apps {
-		images[i] = assembleImageName(a.GetName())
+		images[i] = getImage(&a)
 	}
 	return images
+}
+
+func flatten(image string) error {
+	config := docker.Config{
+		Image:        image,
+		Cmd:          []string{"/bin/bash"},
+		AttachStdin:  false,
+		AttachStdout: false,
+		AttachStderr: false,
+	}
+	_, c, err := dockerCluster().CreateContainer(&config)
+	if err != nil {
+		panic(err.Error())
+		return err
+	}
+	var buf *bytes.Buffer
+	if err := dockerCluster().ExportContainer(c.ID, buf); err != nil {
+		log.Printf("Caugh error while exporting container %s: %s", c.ID, err.Error())
+		return err
+	}
+	opts := dcli.ImportImageOptions{Repository: image, Source: "-"}
+	if err := dockerCluster().ImportImage(opts, buf); err != nil {
+		log.Printf("Caugh error while importing image from container %s: %s", c.ID, err.Error())
+		return err
+	}
+	if err := dockerCluster().RemoveContainer(c.ID); err != nil {
+		log.Printf("Caugh error while removing container %s: %s", c.ID, err.Error())
+	}
+	//remove from registry
+	return nil
+}
+
+// Flatten finds the images that need to be flattened and export/import
+// them in order to flatten them. Logs eventual errors.
+func Flatten() {
+	images := imagesToFlatten()
+	for _, image := range images {
+		if err := flatten(image); err != nil {
+			log.Printf("Caugh error while flattening image %s: %s", image, err.Error())
+		}
+	}
 }
--- provision/docker/flatten_test.go
+++ provision/docker/flatten_test.go
@@ -5,22 +5,28 @@
 package docker
 
 import (
+	dtesting "github.com/fsouza/go-dockerclient/testing"
 	"github.com/globocom/config"
+	"github.com/globocom/docker-cluster/cluster"
 	"github.com/globocom/tsuru/app"
 	"github.com/globocom/tsuru/db"
 	"labix.org/v2/mgo/bson"
 	"launchpad.net/gocheck"
+	"net/http"
 )
 
 type FlattenSuite struct {
-	apps []app.App
-	conn *db.Storage
+	apps     []app.App
+	conn     *db.Storage
+	requests []http.Request
+	server   *dtesting.DockerServer
 }
 
 var _ = gocheck.Suite(&FlattenSuite{})
 
 func (s *FlattenSuite) SetUpSuite(c *gocheck.C) {
 	var err error
+	config.Set("docker:collection", "docker")
 	config.Set("docker:repository-namespace", "tsuru")
 	config.Set("database:url", "127.0.0.1:27017")
 	config.Set("database:name", "docker_flatten")
@@ -28,6 +34,8 @@ func (s *FlattenSuite) SetUpSuite(c *gocheck.C) {
 	units := []app.Unit{{Name: "4fa6e0f0c678"}, {Name: "e90e34656806"}}
 	app1 := app.App{Name: "app1", Platform: "python", Deploys: 40, Units: units}
 	err = s.conn.Apps().Insert(app1)
+	c.Assert(err, gocheck.IsNil)
+	err = collection().Insert(&container{ID: "app1id", AppName: "app1", Image: "tsuru/app1"})
 	c.Assert(err, gocheck.IsNil)
 	app2 := app.App{Name: "app2", Platform: "python", Deploys: 20, Units: units}
 	err = s.conn.Apps().Insert(app2)
@@ -39,9 +47,19 @@ func (s *FlattenSuite) SetUpSuite(c *gocheck.C) {
 	err = s.conn.Apps().Insert(app4)
 	c.Assert(err, gocheck.IsNil)
 	s.apps = append(s.apps, []app.App{app1, app2, app3, app4}...)
+	var handler = func(r *http.Request) {
+		s.requests = append(s.requests, *r)
+	}
+	s.server, err = dtesting.NewServer(handler)
+	c.Assert(err, gocheck.IsNil)
+	dCluster, _ = cluster.New(nil,
+		cluster.Node{ID: "server", Address: s.server.URL()},
+	)
 }
 
 func (s *FlattenSuite) TearDownSuite(c *gocheck.C) {
+	s.server.Stop()
+	collection().RemoveAll(nil)
 	names := make([]string, len(s.apps))
 	for i, a := range s.apps {
 		names[i] = a.GetName()
@@ -50,9 +68,18 @@ func (s *FlattenSuite) TearDownSuite(c *gocheck.C) {
 	c.Assert(err, gocheck.IsNil)
 }
 
+func (s *FlattenSuite) TearDownTest(c *gocheck.C) {
+	s.requests = []http.Request{}
+}
+
 func (s *FlattenSuite) TestImagesToFlattenRetrievesOnlyUnitsWith20DeploysOrMore(c *gocheck.C) {
 	images := imagesToFlatten()
 	c.Assert(len(images), gocheck.Equals, 2)
-	expected := []string{"tsuru/app1", "tsuru/app2"}
+	expected := []string{"tsuru/app1", "tsuru/python"}
 	c.Assert(images, gocheck.DeepEquals, expected)
+}
+
+func (s *FlattenSuite) TestFlatten(c *gocheck.C) {
+	Flatten()
+	c.Assert(len(s.requests), gocheck.Equals, 8) //create, export, import, remove old img, remove container
 }
