68e3328c6a7d84f29b513146ab5f0599dc60ea8b
Andrews Medina <andrewsmedina@gmail.com>
2013-09-25 10:47:14 -0300

    provision/docker: moved collector code to collector.go.
    related to #553.

---
+++ provision/docker/collector.go
@@ -0,0 +1,102 @@
+// Copyright 2013 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package docker
+
+import (
+	"github.com/globocom/tsuru/log"
+	"github.com/globocom/tsuru/provision"
+	_ "github.com/globocom/tsuru/router/hipache"
+	_ "github.com/globocom/tsuru/router/testing"
+	"net"
+	"strings"
+	"sync"
+)
+
+func (p *dockerProvisioner) CollectStatus() ([]provision.Unit, error) {
+	var containersGroup sync.WaitGroup
+	var containers []container
+	coll := collection()
+	defer coll.Close()
+	err := coll.Find(nil).All(&containers)
+	if err != nil {
+		return nil, err
+	}
+	if len(containers) == 0 {
+		return nil, nil
+	}
+	units := make(chan provision.Unit, len(containers))
+	result := buildResult(len(containers), units)
+	for _, container := range containers {
+		containersGroup.Add(1)
+		go collectUnit(container, units, &containersGroup)
+	}
+	containersGroup.Wait()
+	close(units)
+	return <-result, nil
+}
+
+func collectUnit(container container, units chan<- provision.Unit, wg *sync.WaitGroup) {
+	defer wg.Done()
+	unit := provision.Unit{
+		Name:    container.ID,
+		AppName: container.AppName,
+		Type:    container.Type,
+	}
+	switch container.Status {
+	case "error":
+		unit.Status = provision.StatusError
+		units <- unit
+		return
+	case "created":
+		return
+	}
+	unit.Ip = container.HostAddr
+	if ip, hostPort, err := container.networkInfo(); err == nil &&
+		(hostPort != container.HostPort || ip != container.IP) {
+		err = fixContainer(&container, ip, hostPort)
+		if err != nil {
+			log.Printf("error on fix container hostport for [container %s]", container.ID)
+			return
+		}
+	}
+	addr := strings.Replace(container.getAddress(), "http://", "", 1)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		unit.Status = provision.StatusInstalling
+	} else {
+		conn.Close()
+		unit.Status = provision.StatusStarted
+	}
+	log.Printf("collected data for [container %s] - [app %s]", container.ID, container.AppName)
+	units <- unit
+}
+
+func buildResult(maxSize int, units <-chan provision.Unit) <-chan []provision.Unit {
+	ch := make(chan []provision.Unit, 1)
+	go func() {
+		result := make([]provision.Unit, 0, maxSize)
+		for unit := range units {
+			result = append(result, unit)
+			log.Printf("result for [container %s] - [app %s]", unit.Name, unit.AppName)
+		}
+		ch <- result
+	}()
+	return ch
+}
+
+func fixContainer(container *container, ip, port string) error {
+	router, err := getRouter()
+	if err != nil {
+		return err
+	}
+	router.RemoveRoute(container.AppName, container.getAddress())
+	container.removeHost()
+	container.IP = ip
+	container.HostPort = port
+	router.AddRoute(container.AppName, container.getAddress())
+	coll := collection()
+	defer coll.Close()
+	return coll.UpdateId(container.ID, container)
+}
