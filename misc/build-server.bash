c8eb7796f84786736fbb06db592b8573ec196b25
Francisco Souza <fss@corp.globo.com>
2013-10-15 11:50:15 -0300

    misc/build-server: support for the REVISION variable
    We will inject it using Jenkins.

--- misc/build-server.bash
+++ misc/build-server.bash
@@ -5,7 +5,6 @@
 # license that can be found in the LICENSE file.
 
 # This script is used to build tsr.
-
 destination_dir="dist-server"
 
 echo -n "Creating \"$destination_dir\" directory... "
@@ -16,7 +15,11 @@ echo -n "Downloading dependencies... "
 go get -u -d github.com/globocom/tsuru/cmd/tsr
 echo "ok"
 
-echo "Building tsr... "
+echo -n "Checking out $REVISION... "
+git checkout $REVISION
+echo "ok"
+
+echo "Building tsr-${REVISION}... "
 go build -x -a -o $destination_dir/tsr github.com/globocom/tsuru/cmd/tsr
-tar -C $destination_dir -czf $destination_dir/tsr.tar.gz tsr
+tar -C $destination_dir -czf $destination_dir/tsr-${REVISION}.tar.gz tsr
 rm $destination_dir/tsr
