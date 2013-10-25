bb9c256abf2528d6ca732c9adc5506d2d94ecf64
Francisco Souza <fss@corp.globo.com>
2013-10-25 17:15:23 -0200

    cmd/tsr: version 0.2.6

--- cmd/tsr/main.go
+++ cmd/tsr/main.go
@@ -16,7 +16,7 @@ import (
 const defaultConfigPath = "/etc/tsuru/tsuru.conf"
 
 func buildManager() *cmd.Manager {
-	m := cmd.NewManager("tsr", "0.2.5", "", os.Stdout, os.Stderr, os.Stdin)
+	m := cmd.NewManager("tsr", "0.2.6", "", os.Stdout, os.Stderr, os.Stdin)
 	m.Register(&tsrCommand{Command: &apiCmd{}})
 	m.Register(&tsrCommand{Command: &collectorCmd{}})
 	m.Register(&tsrCommand{Command: tokenCmd{}})
