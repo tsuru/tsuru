a9f4d5ec2f285860309d390af2ab649d63160562
Francisco Souza <fss@corp.globo.com>
2013-10-28 18:32:23 -0200

    Revert "quota: remove multiLocker"
    Actually, I was right...
    
    This reverts commit 9382f381259683ead26ca4d5fdcc25e7609f923e.

---
+++ quota/lock.go
@@ -0,0 +1,30 @@
+// Copyright 2013 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package quota
+
+import "sync"
+
+type multiLocker struct {
+	m   map[string]*sync.Mutex
+	mut sync.Mutex
+}
+
+func (l *multiLocker) Lock(name string) {
+	l.mut.Lock()
+	mutex, ok := l.m[name]
+	if !ok {
+		mutex = new(sync.Mutex)
+		l.m[name] = mutex
+	}
+	l.mut.Unlock()
+	mutex.Lock()
+}
+
+func (l *multiLocker) Unlock(name string) {
+	l.mut.Lock()
+	mutex := l.m[name]
+	l.mut.Unlock()
+	mutex.Unlock()
+}
--- quota/quota.go
+++ quota/quota.go
@@ -22,7 +22,7 @@ var (
 	ErrQuotaNotFound      = errors.New("Quota not found")
 )
 
-var locker sync.Mutex
+var locker = multiLocker{m: make(map[string]*sync.Mutex)}
 
 // Usage represents the usage of a user/app. It contains information about the
 // limit of items, and the current amount of items in use by the user.
@@ -74,8 +74,8 @@ func Reserve(owner string, items ...string) error {
 		return err
 	}
 	defer conn.Close()
-	locker.Lock()
-	defer locker.Unlock()
+	locker.Lock(owner)
+	defer locker.Unlock(owner)
 	var u usage
 	err = conn.Quota().Find(bson.M{"owner": owner}).One(&u)
 	if err != nil {
