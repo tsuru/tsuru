7df56c1ee23cb756ecd92b5ca286f11e214f1228
Flavia Missi <flaviamissi@gmail.com>
2013-10-30 11:10:13 -0200

    log/file_logger_test.go: refactoring tests to reuse bootstrap code
    Relates to #115

--- log/file_logger_test.go
+++ log/file_logger_test.go
@@ -9,54 +9,49 @@ import (
 	"log"
 )
 
-type FileLoggerSuite struct{}
+type FileLoggerSuite struct {
+	l  Logger
+	fl *fileLogger
+	b  *bytes.Buffer
+}
 
 var _ = gocheck.Suite(&FileLoggerSuite{})
 
+func (s *FileLoggerSuite) SetUpSuite(c *gocheck.C) {
+	s.l = newFileLogger("/dev/null")
+	s.fl, _ = s.l.(*fileLogger)
+}
+
+func (s *FileLoggerSuite) SetUpTest(c *gocheck.C) {
+	s.b = &bytes.Buffer{}
+	s.fl.logger = log.New(s.b, "", log.LstdFlags)
+}
+
 func (s *FileLoggerSuite) TestNewFileLoggerReturnsALogger(c *gocheck.C) {
-	l := newFileLogger("/dev/null")
-	_, ok := l.(Logger)
+	_, ok := s.l.(Logger)
 	c.Assert(ok, gocheck.Equals, true)
 }
 
 func (s *FileLoggerSuite) TestNewFileLoggerInitializesWriter(c *gocheck.C) {
-	l := newFileLogger("/dev/null")
-	fl, _ := l.(*fileLogger)
-	c.Assert(fl.logger, gocheck.FitsTypeOf, &log.Logger{})
+	c.Assert(s.fl.logger, gocheck.FitsTypeOf, &log.Logger{})
 }
 
 func (s *FileLoggerSuite) TestErrorShouldPrefixMessage(c *gocheck.C) {
-	l := newFileLogger("/dev/null")
-	fl, _ := l.(*fileLogger)
-	b := &bytes.Buffer{}
-	fl.logger = log.New(b, "", log.LstdFlags)
-	l.Error("something terrible happened")
-	c.Assert(b.String(), gocheck.Matches, ".* ERROR: something terrible happened\n$")
+	s.l.Error("something terrible happened")
+	c.Assert(s.b.String(), gocheck.Matches, ".* ERROR: something terrible happened\n$")
 }
 
 func (s *FileLoggerSuite) TestErrorfShouldFormatErrorAndPrefixMessage(c *gocheck.C) {
-	l := newFileLogger("/dev/null")
-	fl, _ := l.(*fileLogger)
-	b := &bytes.Buffer{}
-	fl.logger = log.New(b, "", log.LstdFlags)
-	l.Errorf(`this is the error: "%s"`, "something bad happened")
-	c.Assert(b.String(), gocheck.Matches, `.* ERROR: this is the error: "something bad happened"\n$`)
+	s.l.Errorf(`this is the error: "%s"`, "something bad happened")
+	c.Assert(s.b.String(), gocheck.Matches, `.* ERROR: this is the error: "something bad happened"\n$`)
 }
 
 func (s *FileLoggerSuite) TestDebugShouldPrefixMessage(c *gocheck.C) {
-	l := newFileLogger("/dev/null")
-	fl, _ := l.(*fileLogger)
-	b := &bytes.Buffer{}
-	fl.logger = log.New(b, "", log.LstdFlags)
-	l.Debug("doing some stuff here")
-	c.Assert(b.String(), gocheck.Matches, ".* DEBUG: doing some stuff here\n$")
+	s.l.Debug("doing some stuff here")
+	c.Assert(s.b.String(), gocheck.Matches, ".* DEBUG: doing some stuff here\n$")
 }
 
 func (s *FileLoggerSuite) TestDebugfShouldFormatAndPrefixMessage(c *gocheck.C) {
-	l := newFileLogger("/dev/null")
-	fl, _ := l.(*fileLogger)
-	b := &bytes.Buffer{}
-	fl.logger = log.New(b, "", log.LstdFlags)
-	l.Debugf(`message is "%s"`, "some debug message")
-	c.Assert(b.String(), gocheck.Matches, `.* DEBUG: message is "some debug message"\n$`)
+	s.l.Debugf(`message is "%s"`, "some debug message")
+	c.Assert(s.b.String(), gocheck.Matches, `.* DEBUG: message is "some debug message"\n$`)
 }
