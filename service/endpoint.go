587cf38449401bc5690eaf76937530977bf1ed1a
Francisco Souza <fss@corp.globo.com>
2013-11-13 11:53:49 -0200

    service: fix build
    Ops!

--- service/endpoint.go
+++ service/endpoint.go
@@ -93,7 +93,7 @@ func (c *Client) Destroy(instance *ServiceInstance) error {
 
 func (c *Client) Bind(instance *ServiceInstance, app bind.App, unit bind.Unit) (map[string]string, error) {
 	log.Debugf("Calling bind of instance %q and unit %q at %q API",
-		instace.Name, unit.GetIp(), instance.ServiceName)
+		instance.Name, unit.GetIp(), instance.ServiceName)
 	var resp *http.Response
 	params := map[string][]string{
 		"unit-host": {unit.GetIp()},
