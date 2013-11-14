c42d00f8ca66c868467ab2bfcb624ec0ff1828f0
Andrews Medina <andrewsmedina@gmail.com>
2013-11-14 10:45:16 -0800

    service: foward for create service instance action.
    related to #601.

---
+++ service/actions.go
@@ -0,0 +1,49 @@
+// Copyright 2013 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package service
+
+import (
+	"errors"
+	"github.com/globocom/tsuru/action"
+)
+
+// createServiceInstance in an action that calls the service endpoint
+// to creates the service instance.
+//
+// The first argument in the context must be an Service.
+// The second argument in the context must be an ServiceInstance.
+var createServiceInstance = action.Action{
+	Name: "create-service-instance",
+	Forward: func(ctx action.FWContext) (action.Result, error) {
+		var (
+			service  Service
+			instance ServiceInstance
+		)
+		switch ctx.Params[0].(type) {
+		case Service:
+			service = ctx.Params[0].(Service)
+		default:
+			return nil, errors.New("First parameter must be a Service")
+		}
+		switch ctx.Params[1].(type) {
+		case ServiceInstance:
+			instance = ctx.Params[1].(ServiceInstance)
+		default:
+			return nil, errors.New("Second parameter must be a ServiceInstance")
+		}
+		endpoint, err := service.getClient("production")
+		if err != nil {
+			return nil, err
+		}
+		err = endpoint.Create(&instance)
+		if err != nil {
+			return nil, err
+		}
+		return instance, nil
+	},
+	Backward: func(ctx action.BWContext) {
+	},
+	MinParams: 1,
+}
---
+++ service/actions_test.go
@@ -0,0 +1,40 @@
+// Copyright 2013 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package service
+
+import (
+	"github.com/globocom/tsuru/action"
+	"launchpad.net/gocheck"
+	"net/http"
+	"net/http/httptest"
+	"sync/atomic"
+)
+
+func (s *S) TestCreateServiceInstancName(c *gocheck.C) {
+	c.Assert(createServiceInstance.Name, gocheck.Equals, "create-service-instance")
+}
+
+func (s *S) TestCreateServiceInstanceForward(c *gocheck.C) {
+	var requests int32
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNoContent)
+		atomic.AddInt32(&requests, 1)
+	}))
+	defer ts.Close()
+	srv := Service{Name: "mongodb", Endpoint: map[string]string{"production": ts.URL}}
+	err := s.conn.Services().Insert(&srv)
+	c.Assert(err, gocheck.IsNil)
+	defer s.conn.Services().RemoveId(srv.Name)
+	instance := ServiceInstance{Name: "mysql"}
+	ctx := action.FWContext{
+		Params: []interface{}{srv, instance},
+	}
+	r, err := createServiceInstance.Forward(ctx)
+	c.Assert(err, gocheck.IsNil)
+	a, ok := r.(ServiceInstance)
+	c.Assert(ok, gocheck.Equals, true)
+	c.Assert(a.Name, gocheck.Equals, instance.Name)
+	c.Assert(atomic.LoadInt32(&requests), gocheck.Equals, int32(1))
+}
