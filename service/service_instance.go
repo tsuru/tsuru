7366da42231c69bd649cb3bb66dff8077bccd1f3
Andrews Medina <andrewsmedina@gmail.com>
2013-11-28 13:09:10 -0200

    services: validate service instance name.
    closes #570.

--- service/service_instance.go
+++ service/service_instance.go
@@ -20,9 +20,10 @@ import (
 )
 
 var (
-	ErrServiceInstanceNotFound = stderrors.New("Service instance not found")
-	ErrInvalidInstanceName     = stderrors.New("Invalid service instance name")
-	ErrAccessNotAllowed        = stderrors.New("User does not have access to this service instance")
+	ErrServiceInstanceNotFound   = stderrors.New("Service instance not found")
+	ErrInvalidInstanceName       = stderrors.New("Invalid service instance name")
+	ErrInstanceNameAlreadyExists = stderrors.New("Instance name already exists.")
+	ErrAccessNotAllowed          = stderrors.New("User does not have access to this service instance")
 
 	instanceNameRegexp = regexp.MustCompile(`^[A-Za-z][-a-zA-Z0-9_]+$`)
 )
@@ -253,9 +254,26 @@ func genericServiceInstancesFilter(services interface{}, teams []string) (q, f b
 	return
 }
 
-func CreateServiceInstance(name string, service *Service, user *auth.User) error {
+func validateServiceInstanceName(name string) error {
 	if !instanceNameRegexp.MatchString(name) {
 		return ErrInvalidInstanceName
+	}
+	conn, err := db.Conn()
+	if err != nil {
+		return nil
+	}
+	defer conn.Close()
+	length, err := conn.ServiceInstances().Find(bson.M{"name": name}).Count()
+	if length > 0 {
+		return ErrInstanceNameAlreadyExists
+	}
+	return nil
+}
+
+func CreateServiceInstance(name string, service *Service, user *auth.User) error {
+	err := validateServiceInstanceName(name)
+	if err != nil {
+		return err
 	}
 	instance := ServiceInstance{
 		Name:        name,
