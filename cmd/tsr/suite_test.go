5da093c86d72dcb24bee38b1fb72de93f62cb179
Francisco Souza <fss@corp.globo.com>
2013-12-11 09:59:10 -0200

    cmd/tsr/test: delete database on TearDownSuite

--- cmd/tsr/suite_test.go
+++ cmd/tsr/suite_test.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	"github.com/globocom/config"
+	"github.com/globocom/tsuru/db"
 	"launchpad.net/gocheck"
 	"testing"
 )
@@ -18,4 +19,11 @@ func Test(t *testing.T) { gocheck.TestingT(t) }
 
 func (s *S) SetUpSuite(c *gocheck.C) {
 	config.ReadConfigFile("testdata/tsuru.conf")
+}
+
+func (s *S) TearDownSuite(c *gocheck.C) {
+	conn, err := db.Conn()
+	c.Assert(err, gocheck.IsNil)
+	defer conn.Close()
+	conn.Apps().Database.DropDatabase()
 }
