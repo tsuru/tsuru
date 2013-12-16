f49b5f0dc3d5f71061559f141c04f1a3a9f50e43
Flavia Missi <flaviamissi@gmail.com>
2013-12-16 11:07:58 -0200

    api/admin.go: creates function to run admin server
    Relates to #588

--- api/admin.go
+++ api/admin.go
@@ -5,7 +5,14 @@
 package api
 
 import (
+	"fmt"
 	"github.com/bmizerany/pat"
+	"github.com/globocom/config"
+	"github.com/globocom/tsuru/app"
+	"github.com/globocom/tsuru/db"
+	"github.com/globocom/tsuru/log"
+	"github.com/globocom/tsuru/provision"
+	"net"
 	"net/http"
 )
 
@@ -26,4 +33,38 @@ func RegisterHandler(path string, method string, h http.Handler) {
 	}
 }
 
-// RunAdminServer()
+func RunAdminServer(dry bool) {
+	log.Init()
+	connString, err := config.GetString("database:url")
+	if err != nil {
+		connString = db.DefaultDatabaseURL
+	}
+	dbName, err := config.GetString("database:name")
+	if err != nil {
+		dbName = db.DefaultDatabaseName
+	}
+	fmt.Printf("Using the database %q from the server %q.\n\n", dbName, connString)
+	if !dry {
+		provisioner, err := getProvisioner()
+		if err != nil {
+			fmt.Printf("Warning: configuration didn't declare a provisioner, using default provisioner.\n")
+		}
+		app.Provisioner, err = provision.Get(provisioner)
+		if err != nil {
+			fatal(err)
+		}
+		fmt.Printf("Using %q provisioner.\n\n", provisioner)
+
+		listen, err := config.GetString("admin-listen")
+		if err != nil {
+			fatal(err)
+		}
+		listener, err := net.Listen("tcp", listen)
+		if err != nil {
+			fatal(err)
+		}
+		fmt.Printf("tsuru HTTP server listening at %s...\n", listen)
+		http.Handle("/", m)
+		fatal(http.Serve(listener, nil))
+	}
+}
