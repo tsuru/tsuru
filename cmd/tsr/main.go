73420af19745f8c3abd1ae8f2d1c606deb104d25
Andrews Medina <andrewsmedina@gmail.com>
2014-01-08 12:02:05 -0200

    bump to 0.3.1.

--- cmd/tsr/main.go
+++ cmd/tsr/main.go
@@ -16,7 +16,7 @@ import (
 const defaultConfigPath = "/etc/tsuru/tsuru.conf"
 
 func buildManager() *cmd.Manager {
-	m := cmd.NewManager("tsr", "0.3.1-dev", "", os.Stdout, os.Stderr, os.Stdin)
+	m := cmd.NewManager("tsr", "0.3.1", "", os.Stdout, os.Stderr, os.Stdin)
 	m.Register(&tsrCommand{Command: &apiCmd{}})
 	m.Register(&tsrCommand{Command: &adminCmd{}})
 	m.Register(&tsrCommand{Command: &collectorCmd{}})
