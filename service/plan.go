2765ad5421f1bd9057c4ba66c16b70e334d3d5b8
Andrews Medina <andrewsmedina@gmail.com>
2014-01-31 23:06:43 -0200

    service: implemented GetPlansByServiceName.
    related to #669.

--- service/plan.go
+++ service/plan.go
@@ -4,8 +4,27 @@
 
 package service
 
+import "errors"
+
 // Plan represents a service plan
 type Plan struct {
 	Name        string
 	Description string
+}
+
+func GetPlansByServiceName(serviceName string) ([]Plan, error) {
+	s := Service{Name: serviceName}
+	err := s.Get()
+	if err != nil {
+		return nil, err
+	}
+	endpoint, err := s.getClient("production")
+	if err != nil {
+		return nil, errors.New("endpoint does not exists")
+	}
+	plans, err := endpoint.Plans()
+	if err != nil {
+		return nil, err
+	}
+	return plans, nil
 }
--- service/plan_test.go
+++ service/plan_test.go
@@ -6,6 +6,8 @@ package service
 
 import (
 	"launchpad.net/gocheck"
+	"net/http"
+	"net/http/httptest"
 )
 
 func (s *S) TestPlan(c *gocheck.C) {
@@ -15,4 +17,23 @@ func (s *S) TestPlan(c *gocheck.C) {
 	}
 	c.Assert("Ignite", gocheck.Equals, plan.Name)
 	c.Assert("A simple plan", gocheck.Equals, plan.Description)
+}
+
+func (s *S) TestGetPlansByServiceName(c *gocheck.C) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		content := `[{"name": "ignite", "description": "some value"}, {"name": "small", "description": "not space left for you"}]`
+		w.Write([]byte(content))
+	}))
+	defer ts.Close()
+	srvc := Service{Name: "mysql", Endpoint: map[string]string{"production": ts.URL}}
+	err := s.conn.Services().Insert(&srvc)
+	c.Assert(err, gocheck.IsNil)
+	defer s.conn.Services().RemoveId(srvc.Name)
+	plans, err := GetPlansByServiceName("mysql")
+	c.Assert(err, gocheck.IsNil)
+	expected := []Plan{
+		{Name: "ignite", Description: "some value"},
+		{Name: "small", Description: "not space left for you"},
+	}
+	c.Assert(plans, gocheck.DeepEquals, expected)
 }
