22f32ab8b7f2fca69d4fde006d3c3c8578a4f51b
Francisco Souza <fss@corp.globo.com>
2014-02-26 11:26:29 -0300

    api: remove captain obvious comment

--- api/healthcheck.go
+++ api/healthcheck.go
@@ -1,4 +1,4 @@
-// Copyright 2013 tsuru authors. All rights reserved.
+// Copyright 2014 tsuru authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -6,7 +6,6 @@ package api
 
 import "net/http"
 
-// healthcheck returns working"
 func healthcheck(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("WORKING"))
 }
