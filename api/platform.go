99b83cb0b931fc808e68c2eeec412a68890ccedc
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2014-04-29 17:34:25 -0300

    add content type header to run test

--- api/platform.go
+++ api/platform.go
@@ -9,11 +9,9 @@ import (
 func platformAdd(w http.ResponseWriter, r *http.Request, t *auth.Token) error {
 	name := r.FormValue("name")
 	dockerfile := r.FormValue("dockerfile")
-
 	err := app.PlatformAdd(name, dockerfile)
 	if err != nil {
 		return err
 	}
-
 	return nil
 }
--- api/platform_test.go
+++ api/platform_test.go
@@ -18,12 +18,10 @@ var _ = gocheck.Suite(&PlatformSuite{})
 
 func (p *PlatformSuite) TestPlatformAdd(c *gocheck.C) {
 	dockerfile_url := "http://localhost/Dockerfile"
-
 	body := fmt.Sprintf("name=%s&dockerfile=%s", "teste", dockerfile_url)
 	request, _ := http.NewRequest("PUT", "/platform/add", strings.NewReader(body))
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	recorder := httptest.NewRecorder()
-
 	result := platformAdd(recorder, request, nil)
-
 	c.Assert(result, gocheck.IsNil)
 }
