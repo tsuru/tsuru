100e115aee78d0111d098fc04581be83d41b039a
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2014-05-02 14:52:15 -0300

    set content type header to responseWriter

--- api/platform.go
+++ api/platform.go
@@ -12,9 +12,6 @@ func platformAdd(w http.ResponseWriter, r *http.Request, t *auth.Token) error {
 	for key, values := range r.Form {
 		args[key] = values[0]
 	}
-	err := app.PlatformAdd(name, args)
-	if err != nil {
-		return err
-	}
-	return nil
+	w.Header().Set("Content-Type", "text")
+	return app.PlatformAdd(name, args, w)
 }
