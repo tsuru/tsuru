f04e8cbc98c8954db8880ffdb189e0401505aad3
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2014-05-02 20:51:20 -0300

    cmd/tsuru-admin: add a platform-update command

---
+++ cmd/tsuru-admin/platform.go
@@ -0,0 +1,107 @@
+// Copyright 2014 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"github.com/tsuru/tsuru/cmd"
+	"io"
+	"launchpad.net/gnuflag"
+	"net/http"
+	"strings"
+)
+
+type platformAdd struct {
+	name       string
+	dockerfile string
+	fs         *gnuflag.FlagSet
+}
+
+func (p *platformAdd) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:    "platform-add",
+		Usage:   "platform-add <platform name> [--dockerfile/-d Dockerfile]",
+		Desc:    "Add new platform to tsuru.",
+		MinArgs: 1,
+	}
+}
+
+func (p *platformAdd) Run(context *cmd.Context, client *cmd.Client) error {
+	name := context.Args[0]
+	body := fmt.Sprintf("name=%s&dockerfile=%s", name, p.dockerfile)
+	url, err := cmd.GetURL("/platforms/add")
+	request, err := http.NewRequest("PUT", url, strings.NewReader(body))
+	if err != nil {
+		return err
+	}
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+    for n := int64(1); n > 0 && err == nil; n, err = io.Copy(context.Stdout, response.Body) {
+	}
+	fmt.Fprintf(context.Stdout, "Platform successfully added!\n")
+	return nil
+}
+
+func (p *platformAdd) Flags() *gnuflag.FlagSet {
+	message := "The dockerfile url to create a platform"
+	if p.fs == nil {
+		p.fs = gnuflag.NewFlagSet("platform-add", gnuflag.ExitOnError)
+		p.fs.StringVar(&p.dockerfile, "dockerfile", "", message)
+		p.fs.StringVar(&p.dockerfile, "d", "", message)
+	}
+	return p.fs
+}
+
+type platformUpdate struct {
+    name string
+    dockerfile string
+    forceUpdate bool
+    fs *gnuflag.FlagSet
+}
+
+func (p *platformUpdate) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:    "platform-platform",
+		Usage:   "platform-platform <platform name> [--dockerfile/-d Dockerfile]",
+		Desc:    "Update a platform to tsuru.",
+		MinArgs: 1,
+	}
+}
+
+func (p *platformUpdate) Flags() *gnuflag.FlagSet {
+    forceUpdateMessage := "Force apps to update your platform in next deploy"
+	dockerfileMessage := "The dockerfile url to update a platform"
+	if p.fs == nil {
+		p.fs = gnuflag.NewFlagSet("platform-update", gnuflag.ExitOnError)
+		p.fs.StringVar(&p.dockerfile, "dockerfile", "", dockerfileMessage)
+		p.fs.StringVar(&p.dockerfile, "d", "", dockerfileMessage)
+		p.fs.BoolVar(&p.forceUpdate, "force-update", false, forceUpdateMessage)
+	}
+	return p.fs
+}
+
+func (p *platformUpdate) Run(context *cmd.Context, client *cmd.Client) error {
+	name := context.Args[0]
+	body := fmt.Sprintf("name=%s&dockerfile=%s&forceUpdate=%t", name, p.dockerfile, p.forceUpdate)
+	url, err := cmd.GetURL("/platforms/update")
+	request, err := http.NewRequest("PUT", url, strings.NewReader(body))
+	if err != nil {
+		return err
+	}
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+    for n := int64(1); n > 0 && err == nil; n, err = io.Copy(context.Stdout, response.Body) {
+	}
+	fmt.Fprintf(context.Stdout, "Platform successfully updated!\n")
+	return nil
+}
---
+++ cmd/tsuru-admin/platform_test.go
@@ -0,0 +1,129 @@
+// Copyright 2014 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"github.com/tsuru/tsuru/cmd"
+	"github.com/tsuru/tsuru/cmd/testing"
+	"launchpad.net/gocheck"
+	"net/http"
+)
+
+func (s *S) TestPlatformAddInfo(c *gocheck.C) {
+	expected := &cmd.Info{
+		Name:    "platform-add",
+		Usage:   "platform-add <platform name> [--dockerfile/-d Dockerfile]",
+		Desc:    "Add new platform to tsuru.",
+		MinArgs: 1,
+	}
+
+	c.Assert((&platformAdd{}).Info(), gocheck.DeepEquals, expected)
+}
+
+func (s *S) TestPlatformAddRun(c *gocheck.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Stdout: &stdout,
+		Stderr: &stderr,
+		Args:   []string{"teste"},
+	}
+
+	expected := "Platform successfully added!\n"
+	trans := &testing.ConditionalTransport{
+		Transport: testing.Transport{Message: "", Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			c.Assert(req.Header.Get("Content-Type"), gocheck.Equals, "application/x-www-form-urlencoded")
+			return req.URL.Path == "/platforms/add" && req.Method == "PUT"
+		},
+	}
+
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, manager)
+	command := platformAdd{}
+	command.Flags().Parse(true, []string{"--dockerfile", "http://localhost/Dockerfile"})
+
+	err := command.Run(&context, client)
+
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(stdout.String(), gocheck.Equals, expected)
+}
+
+func (s *S) TestPlatformAddFlagSet(c *gocheck.C) {
+	message := "The dockerfile url to create a platform"
+	command := platformAdd{}
+	flagset := command.Flags()
+	flagset.Parse(true, []string{"--dockerfile", "dockerfile"})
+
+	dockerfile := flagset.Lookup("dockerfile")
+	c.Check(dockerfile.Name, gocheck.Equals, "dockerfile")
+	c.Check(dockerfile.Usage, gocheck.Equals, message)
+	c.Check(dockerfile.DefValue, gocheck.Equals, "")
+
+	sdockerfile := flagset.Lookup("d")
+	c.Check(sdockerfile.Name, gocheck.Equals, "d")
+	c.Check(sdockerfile.Usage, gocheck.Equals, message)
+	c.Check(sdockerfile.DefValue, gocheck.Equals, "")
+}
+
+func (s *S) TestPlatformUpdateInfo(c *gocheck.C) {
+	expected := &cmd.Info{
+		Name:    "platform-platform",
+		Usage:   "platform-platform <platform name> [--dockerfile/-d Dockerfile]",
+		Desc:    "Update a platform to tsuru.",
+		MinArgs: 1,
+	}
+	c.Assert((&platformUpdate{}).Info(), gocheck.DeepEquals, expected)
+}
+
+func (s *S) TestPlatformUpdateFlagSet(c *gocheck.C) {
+	dockerfile_message := "The dockerfile url to update a platform"
+	command := platformUpdate{}
+	flagset := command.Flags()
+	flagset.Parse(true, []string{"--dockerfile", "dockerfile"})
+	flagset.Parse(true, []string{"--force-update", "true"})
+
+	dockerfile := flagset.Lookup("dockerfile")
+	c.Check(dockerfile.Name, gocheck.Equals, "dockerfile")
+	c.Check(dockerfile.Usage, gocheck.Equals, dockerfile_message)
+	c.Check(dockerfile.DefValue, gocheck.Equals, "")
+
+	sdockerfile := flagset.Lookup("d")
+	c.Check(sdockerfile.Name, gocheck.Equals, "d")
+	c.Check(sdockerfile.Usage, gocheck.Equals, dockerfile_message)
+	c.Check(sdockerfile.DefValue, gocheck.Equals, "")
+
+    forceUpdateMessage := "Force apps to update your platform in next deploy"
+    fdockerfile := flagset.Lookup("force-update")
+    c.Check(fdockerfile.Name, gocheck.Equals, "force-update")
+	c.Check(fdockerfile.Usage, gocheck.Equals, forceUpdateMessage)
+	c.Check(fdockerfile.DefValue, gocheck.Equals, "false")
+}
+
+func (s *S) TestPlatformUpdateRun(c *gocheck.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Stdout: &stdout,
+		Stderr: &stderr,
+		Args:   []string{"teste"},
+	}
+	expected := "Platform successfully updated!\n"
+	trans := &testing.ConditionalTransport{
+		Transport: testing.Transport{Message: "", Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			c.Assert(req.Header.Get("Content-Type"), gocheck.Equals, "application/x-www-form-urlencoded")
+            c.Assert(req.FormValue("name"), gocheck.Equals, "teste")
+            c.Assert(req.FormValue("dockerfile"), gocheck.Equals, "http://localhost/Dockerfile")
+            c.Assert(req.FormValue("forceUpdate"), gocheck.Equals, "true")
+			return req.URL.Path == "/platforms/update" && req.Method == "PUT"
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, manager)
+	command := platformUpdate{}
+    command.Flags().Parse(true, []string{"--dockerfile", "http://localhost/Dockerfile"})
+	command.Flags().Parse(true, []string{"--force-update", "true"})
+	err := command.Run(&context, client)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(stdout.String(), gocheck.Equals, expected)
+}
