049940894c594befcfdb2d1bbbe8952a3336da4c
Andrews Medina <andrewsmedina@gmail.com>
2014-05-07 14:50:15 -0300

    cmd: support for more than one login scheme.

--- cmd/auth.go
+++ cmd/auth.go
@@ -141,7 +141,14 @@ func nativeLogin(context *Context, client *Client) error {
 	return writeToken(out["token"].(string))
 }
 
+func oauthLogin(context *Context, client *Client) error {
+	return nil
+}
+
 func (c *login) Run(context *Context, client *Client) error {
+	if scheme() == "oauth" {
+		return oauthLogin(context, client)
+	}
 	return nativeLogin(context, client)
 }
 
