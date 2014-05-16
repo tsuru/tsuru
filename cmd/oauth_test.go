f270442e0c4a920012930b4aabdedbf295b1b497
Andrews Medina <andrewsmedina@gmail.com>
2014-05-15 22:41:52 -0300

    cmd/oauth: added tests for callback handler.

--- cmd/oauth_test.go
+++ cmd/oauth_test.go
@@ -6,10 +6,13 @@ package cmd
 
 import (
 	etesting "github.com/tsuru/tsuru/exec/testing"
+	ftesting "github.com/tsuru/tsuru/fs/testing"
+	"io/ioutil"
 	"launchpad.net/gocheck"
 	"net/http"
 	"net/http/httptest"
 	"runtime"
+	"strings"
 )
 
 func (s *S) TestAuthorizeUrl(c *gocheck.C) {
@@ -46,4 +49,33 @@ func (s *S) TestOpen(c *gocheck.C) {
 	} else {
 		c.Assert(fexec.ExecutedCmd("open", []string{url}), gocheck.Equals, true)
 	}
+}
+
+func (s *S) TestCallbackHandler(c *gocheck.C) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{"token": "xpto"}`))
+	}))
+	defer ts.Close()
+	rfs := &ftesting.RecordingFs{}
+	fsystem = rfs
+	defer func() {
+		fsystem = nil
+	}()
+	writeTarget(ts.URL)
+	redirectUrl := "someurl"
+	finish := make(chan bool, 1)
+	handler := callback(redirectUrl, finish)
+	body := `{"code":"xpto"}`
+	request, err := http.NewRequest("GET", "/", strings.NewReader(body))
+	c.Assert(err, gocheck.IsNil)
+	recorder := httptest.NewRecorder()
+	handler(recorder, request)
+	c.Assert(<-finish, gocheck.Equals, true)
+	expected := "<html><head><script>window.close();</script></head></html>"
+	c.Assert(expected, gocheck.Equals, recorder.Body.String())
+	file, err := rfs.Open(JoinWithUserDir(".tsuru_token"))
+	c.Assert(err, gocheck.IsNil)
+	data, err := ioutil.ReadAll(file)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(string(data), gocheck.Equals, "xpto")
 }
