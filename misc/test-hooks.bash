96847b2bf2ea7763a4799ec81263e6b2cdb79960
Francisco Souza <fss@corp.globo.com>
2014-06-17 12:44:43 -0300

    misc/test-hooks: simplify test

--- misc/test-hooks.bash
+++ misc/test-hooks.bash
@@ -4,85 +4,9 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-function clean() {
-	rm tsr .copy.conf
-}
-
-echo "RUNNING GIT-HOOKS TESTS"
-mongo tsuru > /dev/null 2>.mongo.err <<END
-var today = new Date();
-db.users.insert({"email": "test@shellscript", "password": "irrelevant"});
-db.teams.insert({"_id": "test", "users": ["test@shellscript"]})
-db.tokens.insert({"token": "000secret123", "creation": today, "expires": 24 * 60 * 60 * 1e9, "useremail": "test@shellscript"})
-var app1 = {
-	"name": "shell",
-	"framework": "bash",
-	"ip": "10.10.10.10",
-	"cname": "myapp.com",
-	"units": [
-		{
-			"name": "shell/0",
-			"type": "bash",
-			"instanceid": "i-0800",
-			"ip": "10.10.10.10",
-			"state": "started"
-		}
-	],
-	"teams": ["test"]
-};
-var app2 = {
-	"name": "xeu",
-	"framework": "bash",
-	"ip": "10.10.10.11",
-	"cname": "myapp.com",
-	"units": [
-		{
-			"name": "xeu/0",
-			"type": "bash",
-			"instanceid": "i-0801",
-			"ip": "10.10.10.11",
-			"state": "pending"
-		}
-	],
-	"teams": ["test"]
-};
-db.apps.insert(app1);
-db.apps.insert(app2);
-END
-
-if [ $? != 0 ]
-then
-	echo "FAILURE: failed to insert data into the database"
-	cat .mongo.err
-	rm .mongo.err
-	exit 7
-fi
-rm .mongo.err
-
-go build -o tsr ./cmd/tsr
-sed -e 's/0.0.0.0:8080/0.0.0.0:5000/' etc/tsuru.conf > .copy.conf
-./tsr api --config .copy.conf > .api.out 2>&1 &
-
-if [ $? != 0 ]
-then
-	echo "FAILURE: failed to build api server"
-	cat .api.out
-	rm .api.out
-	clean
-	exit 1
-fi
-rm .api.out
-
-sleep 1
-nc -z localhost 5000 > /dev/null
-if [ $? != 0 ]
-then
-	echo "FAILURE: did not start the API server"
-	clean
-	exit 1
-fi
-
 status=0
+
+echo tsuru receiving push | nc -l 5000 > /tmp/netcat.out &
 
 mkdir /tmp/shell.git
 ln -s $PWD/misc/git-hooks /tmp/shell.git/hooks
@@ -94,17 +18,36 @@ oldref newref refname
 END`
 gout=`echo $out | grep "tsuru receiving push"`
 
-if [ $? = 0 ]
+if [ $? != 0 ]
 then
-	echo "PASS"
-else
 	echo "FAILURE: wrong output from post-receive command."
 	echo "$out"
 	status=1
 fi
 
+if [ "`tail -1 /tmp/netcat.out`" != "version=origin/master&commit=newref" ]
+then
+	echo "FAILURE: wrong request to the tsuru server."
+	cat /tmp/netcat.out
+	status=1
+fi
+
+header=`head -1 /tmp/netcat.out | sed -e "s/$(printf '\r')\$//"`
+if [ "${header}" != "POST /apps/shell/repository/clone HTTP/1.1" ]
+then
+	echo "FAILURE: wrong request to the tsuru server."
+	cat /tmp/netcat.out
+	status=1
+fi
+
+
+if [ $status == 0 ]
+then
+	echo "PASS"
+fi
+
 popd > /dev/null
-rm -rf /tmp/shell.git
+rm -rf /tmp/shell.git /tmp/netcat.out
 
 kill %1
 
@@ -112,5 +55,4 @@ mongo tsuru > /dev/null 2>&1 <<END
 db.dropDatabase();
 END
 
-clean
 exit $status
