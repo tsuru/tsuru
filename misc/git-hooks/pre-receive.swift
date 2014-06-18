61746ab4eeab007d535260a18ea9fa2f50df53d5
Francisco Souza <fss@corp.globo.com>
2014-06-18 15:39:30 -0300

    misc/git-hooks/swift: fix typo

--- misc/git-hooks/pre-receive.swift
+++ misc/git-hooks/pre-receive.swift
@@ -29,7 +29,7 @@ UUID=`python -c 'import uuid; print uuid.uuid4().hex'`
 ARCHIVE_FILE_NAME=${APP_NAME}_${COMMIT}_${UUID}.tar.gz
 git archive --format=tar.gz -o /tmp/$ARCHIVE_FILE_NAME $COMMIT
 swift -qA $AUTH_URL $AUTH_PARAMS upload $CONTAINER_NAME /tmp/$ARCHIVE_FILE_NAME --object-name $ARCHIVE_FILE_NAME
-swift -qA $AUTH_URL $AUTH_PARAMS post -r ".r:*" $CONTANER_NAME
+swift -qA $AUTH_URL $AUTH_PARAMS post -r ".r:*" $CONTAINER_NAME
 rm /tmp/$ARCHIVE_FILE_NAME
 ARCHIVE_URL=`swift -A $AUTH_URL $AUTH_PARAMS stat -v $CONTAINER_NAME $ARCHIVE_FILE_NAME | grep URL | awk -F': ' '{print $2}'`
 URL="${TSURU_HOST}/apps/${APP_NAME}/repository/clone"
