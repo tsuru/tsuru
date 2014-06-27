d7128e085329f396afd5886baf0a538320e4699d
Francisco Souza <fss@corp.globo.com>
2014-06-27 10:21:25 -0300

    misc/git-hooks/pre-receive.swift: use just AUTH_PARAMS
    Sometimes we will need the 1.0 scheme, other times the 2.0, and the
    params are entirely different.

--- misc/git-hooks/pre-receive.swift
+++ misc/git-hooks/pre-receive.swift
@@ -8,10 +8,8 @@
 #
 # It also depends on the following environment variables:
 #
-#   - AUTH_URL: the authentication URL (for example:
-#               https://yourswift.com/auth/v1.0)
-#   - AUTH_PARAMS: the parameters used in authentication (for example: "-K
-#                  yourkey -U youruser")
+#   - AUTH_PARAMS: the parameters used in authentication (for example:
+#                    "-A https://yourswift.com -K yourkey -U youruser")
 #   - CONTAINER_NAME: name of the container where the script will store the
 #                     archives
 #   - TSURU_HOST: URL to the Tsuru API (for example: http://yourtsuru:8080)
@@ -28,11 +26,11 @@ APP_NAME=${APP_DIR/.git/}
 UUID=`python -c 'import uuid; print uuid.uuid4().hex'`
 ARCHIVE_FILE_NAME=${APP_NAME}_${COMMIT}_${UUID}.tar.gz
 git archive --format=tar.gz -o /tmp/$ARCHIVE_FILE_NAME $COMMIT
-swift -qA $AUTH_URL $AUTH_PARAMS upload $CONTAINER_NAME /tmp/$ARCHIVE_FILE_NAME --object-name $ARCHIVE_FILE_NAME
-swift -qA $AUTH_URL $AUTH_PARAMS post -r ".r:*" $CONTAINER_NAME
+swift -q $AUTH_PARAMS upload $CONTAINER_NAME /tmp/$ARCHIVE_FILE_NAME --object-name $ARCHIVE_FILE_NAME
+swift -q $AUTH_PARAMS post -r ".r:*" $CONTAINER_NAME
 rm /tmp/$ARCHIVE_FILE_NAME
-ARCHIVE_URL=`swift -A $AUTH_URL $AUTH_PARAMS stat -v $CONTAINER_NAME $ARCHIVE_FILE_NAME | grep URL | awk -F': ' '{print $2}'`
+ARCHIVE_URL=`swift $AUTH_PARAMS stat -v $CONTAINER_NAME $ARCHIVE_FILE_NAME | grep URL | awk -F': ' '{print $2}'`
 URL="${TSURU_HOST}/apps/${APP_NAME}/repository/clone"
 curl -H "Authorization: bearer ${TSURU_TOKEN}" -d "archive-url=${ARCHIVE_URL}&commit=${COMMIT}" -s -N --max-time 1800 $URL | tee /tmp/deploy-${APP_NAME}.log
-swift -qA $AUTH_URL $AUTH_PARAMS delete $CONTAINER_NAME $ARCHIVE_FILE_NAME
+swift -q$AUTH_PARAMS delete $CONTAINER_NAME $ARCHIVE_FILE_NAME
 tail -1 /tmp/deploy-${APP_NAME}.log | grep -q "^OK$"
