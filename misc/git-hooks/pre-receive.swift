86cf113c79a692842f28a226e80ec0f7ec1b72cd
Francisco Souza <fss@corp.globo.com>
2014-06-27 10:45:10 -0300

    misc/git-hooks/pre-receive.swift: support use of CDN for the container

--- misc/git-hooks/pre-receive.swift
+++ misc/git-hooks/pre-receive.swift
@@ -10,6 +10,8 @@
 #
 #   - AUTH_PARAMS: the parameters used in authentication (for example:
 #                    "-A https://yourswift.com -K yourkey -U youruser")
+#   - CDN_URL: the URL of the CDN that serves content from your container (for
+#              example: something.cf5.rackcdn.com).
 #   - CONTAINER_NAME: name of the container where the script will store the
 #                     archives
 #   - TSURU_HOST: URL to the Tsuru API (for example: http://yourtsuru:8080)
@@ -29,7 +31,12 @@ git archive --format=tar.gz -o /tmp/$ARCHIVE_FILE_NAME $COMMIT
 swift -q $AUTH_PARAMS upload $CONTAINER_NAME /tmp/$ARCHIVE_FILE_NAME --object-name $ARCHIVE_FILE_NAME
 swift -q $AUTH_PARAMS post -r ".r:*" $CONTAINER_NAME
 rm /tmp/$ARCHIVE_FILE_NAME
-ARCHIVE_URL=`swift $AUTH_PARAMS stat -v $CONTAINER_NAME $ARCHIVE_FILE_NAME | grep URL | awk -F': ' '{print $2}'`
+if [ -z "${CDN_URL}" ]
+then
+	ARCHIVE_URL=`swift $AUTH_PARAMS stat -v $CONTAINER_NAME $ARCHIVE_FILE_NAME | grep URL | awk -F': ' '{print $2}'`
+else
+	ARCHIVE_URL=${CDN_URL}/${ARCHIVE_FILE_NAME}
+fi
 URL="${TSURU_HOST}/apps/${APP_NAME}/repository/clone"
 curl -H "Authorization: bearer ${TSURU_TOKEN}" -d "archive-url=${ARCHIVE_URL}&commit=${COMMIT}" -s -N --max-time 1800 $URL | tee /tmp/deploy-${APP_NAME}.log
 swift -q$AUTH_PARAMS delete $CONTAINER_NAME $ARCHIVE_FILE_NAME
