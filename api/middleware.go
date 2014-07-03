ad1d1d7384ca93a2a864a6532c1e69fc9b44bd87
Francisco Souza <fss@corp.globo.com>
2014-07-03 16:10:07 -0300

    api: require tsuru-admin 0.4.2

--- api/middleware.go
+++ api/middleware.go
@@ -19,7 +19,7 @@ import (
 const (
 	tsuruMin      = "0.10.0"
 	craneMin      = "0.5.1"
-	tsuruAdminMin = "0.4.0"
+	tsuruAdminMin = "0.4.2"
 )
 
 func validate(token string, r *http.Request) (auth.Token, error) {
