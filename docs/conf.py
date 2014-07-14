2b6d87d47836302a07d90643a551e04f42f2670f
Andrews Medina <andrewsmedina@gmail.com>
2014-07-14 16:01:37 -0300

    docs: added toctree on sidebar and navigation.

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'toctree'
 
 # General information about the project.
 project = u'tsuru'
