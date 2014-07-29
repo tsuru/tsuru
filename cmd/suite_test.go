814ef5ce9bd17baf56e10814cd4ca6f1d12198bc
Francisco Souza <fss@corp.globo.com>
2014-07-28 22:21:21 -0300

    cmd: fix call to RollbackFile
    I've renamed this function, so it makes more sense.

--- cmd/suite_test.go
+++ cmd/suite_test.go
@@ -27,7 +27,7 @@ func (s *S) SetUpSuite(c *gocheck.C) {
 }
 
 func (s *S) TearDownSuite(c *gocheck.C) {
-	tTesting.RollbackTargetFile(s.recover)
+	tTesting.RollbackFile(s.recover)
 }
 
 func (s *S) SetUpTest(c *gocheck.C) {
