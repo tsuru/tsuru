427091dfbd86f25f82c158794b870205f1663eef
Joao Paulo Vieira <vieira.joaopaulo@hotmail.com>
2014-08-06 19:08:00 -0300

    api/app: create deployInfo handler

--- app/app.go
+++ app/app.go
@@ -718,54 +718,6 @@ func (app *App) GetDeploys() uint {
 	return app.Deploys
 }
 
-type deploy struct {
-	App       string
-	Timestamp time.Time
-	Duration  time.Duration
-	Commit    string
-	Error     string
-}
-
-func (app *App) ListDeploys() ([]deploy, error) {
-	return listDeploys(app, nil)
-}
-
-func ListDeploys(s *service.Service) ([]deploy, error) {
-	return listDeploys(nil, s)
-}
-
-func listDeploys(app *App, s *service.Service) ([]deploy, error) {
-	var list []deploy
-	conn, err := db.Conn()
-	if err != nil {
-		return nil, err
-	}
-	defer conn.Close()
-	var qr bson.M
-	if app != nil {
-		qr = bson.M{"app": app.Name}
-	}
-	if s != nil {
-		var instances []service.ServiceInstance
-		q := bson.M{"service_name": s.Name}
-		err = conn.ServiceInstances().Find(q).All(&instances)
-		if err != nil {
-			return nil, err
-		}
-		var appNames []string
-		for _, instance := range instances {
-			for _, apps := range instance.Apps {
-				appNames = append(appNames, apps)
-			}
-		}
-		qr = bson.M{"app": bson.M{"$in": appNames}}
-	}
-	if err := conn.Deploys().Find(qr).Sort("-timestamp").All(&list); err != nil {
-		return nil, err
-	}
-	return list, err
-}
-
 // Env returns app.Env
 func (app *App) Envs() map[string]bind.EnvVar {
 	return app.Env
@@ -1007,69 +959,6 @@ func Swap(app1, app2 *App) error {
 	return updateCName(app2)
 }
 
-type DeployOptions struct {
-	App          *App
-	Version      string
-	Commit       string
-	ArchiveURL   string
-	OutputStream io.Writer
-}
-
-// Deploy runs a deployment of an application. It will first try to run an
-// archive based deploy (if opts.ArchiveURL is not empty), and then fallback to
-// the Git based deployment.
-func Deploy(opts DeployOptions) error {
-	var pipeline *action.Pipeline
-	start := time.Now()
-	if cprovisioner, ok := Provisioner.(provision.CustomizedDeployPipelineProvisioner); ok {
-		pipeline = cprovisioner.DeployPipeline()
-	} else {
-		actions := []*action.Action{&ProvisionerDeploy, &IncrementDeploy}
-		pipeline = action.NewPipeline(actions...)
-	}
-	logWriter := LogWriter{App: opts.App, Writer: opts.OutputStream}
-	err := pipeline.Execute(opts, &logWriter)
-	elapsed := time.Since(start)
-	if err != nil {
-		saveDeployData(opts.App.Name, opts.Commit, elapsed, err)
-		return err
-	}
-	if opts.App.UpdatePlatform == true {
-		opts.App.SetUpdatePlatform(false)
-	}
-	return saveDeployData(opts.App.Name, opts.Commit, elapsed, nil)
-}
-
-func saveDeployData(appName, commit string, duration time.Duration, deployError error) error {
-	conn, err := db.Conn()
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-	deploy := deploy{
-		App:       appName,
-		Timestamp: time.Now(),
-		Duration:  duration,
-		Commit:    commit,
-	}
-	if deployError != nil {
-		deploy.Error = deployError.Error()
-	}
-	return conn.Deploys().Insert(deploy)
-}
-
-func incrementDeploy(app *App) error {
-	conn, err := db.Conn()
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-	return conn.Apps().Update(
-		bson.M{"name": app.Name},
-		bson.M{"$inc": bson.M{"deploys": 1}},
-	)
-}
-
 // Start starts the app calling the provisioner.Start method and
 // changing the units state to StatusStarted.
 func (app *App) Start(w io.Writer) error {
@@ -1094,22 +983,4 @@ func (app *App) SetUpdatePlatform(check bool) error {
 
 func (app *App) GetUpdatePlatform() bool {
 	return app.UpdatePlatform
-}
-
-func GetDiffInDeploys(d *deploy) (string, error) {
-	var list []deploy
-	conn, err := db.Conn()
-	if err != nil {
-		return "", err
-	}
-	defer conn.Close()
-	if err := conn.Deploys().Find(bson.M{"app": d.App}).Sort("-timestamp").All(&list); err != nil {
-		return "", err
-	}
-	gandalfClient := gandalf.Client{Endpoint: repository.ServerURL()}
-	diffOutput, err := gandalfClient.GetDiff(d.App, list[1].Commit, list[0].Commit)
-	if err != nil {
-		return "", fmt.Errorf("Caught error getting repository metadata: %s", err.Error())
-	}
-	return diffOutput, nil
 }
---
+++ app/deploy_test.go
@@ -0,0 +1,313 @@
+// Copyright 2014 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package app
+
+import (
+    "bytes"
+    "github.com/tsuru/tsuru/service"
+    "github.com/tsuru/tsuru/testing"
+    "gopkg.in/mgo.v2/bson"
+    "launchpad.net/gocheck"
+    "time"
+)
+
+func (s *S) TestListAppDeploys(c *gocheck.C) {
+    s.conn.Deploys().RemoveAll(nil)
+    a := App{Name: "g1"}
+    err := s.conn.Apps().Insert(a)
+    c.Assert(err, gocheck.IsNil)
+    defer s.conn.Apps().Remove(bson.M{"name": a.Name})
+    insert := []interface{}{
+        deploy{App: "g1", Timestamp: time.Now().Add(-3600 * time.Second)},
+        deploy{App: "g1", Timestamp: time.Now()},
+    }
+    s.conn.Deploys().Insert(insert...)
+    defer s.conn.Deploys().RemoveAll(bson.M{"app": a.Name})
+    expected := []deploy{insert[1].(deploy), insert[0].(deploy)}
+    deploys, err := a.ListDeploys()
+    c.Assert(err, gocheck.IsNil)
+    for i := 0; i < 2; i++ {
+        ts := expected[i].Timestamp
+        expected[i].Timestamp = time.Date(ts.Year(), ts.Month(), ts.Day(), ts.Hour(), ts.Minute(), ts.Second(), 0, time.UTC)
+        ts = deploys[i].Timestamp
+        deploys[i].Timestamp = time.Date(ts.Year(), ts.Month(), ts.Day(), ts.Hour(), ts.Minute(), ts.Second(), 0, time.UTC)
+        expected[i].ID = deploys[i].ID
+    }
+    c.Assert(deploys, gocheck.DeepEquals, expected)
+}
+
+func (s *S) TestListServiceDeploys(c *gocheck.C) {
+    s.conn.Deploys().RemoveAll(nil)
+    srv := service.Service{Name: "mysql"}
+    instance := service.ServiceInstance{
+        Name:        "myinstance",
+        ServiceName: "mysql",
+        Apps:        []string{"g1"},
+    }
+    err := s.conn.ServiceInstances().Insert(instance)
+    err = s.conn.Services().Insert(srv)
+    c.Assert(err, gocheck.IsNil)
+    defer s.conn.ServiceInstances().Remove(bson.M{"apps": instance.Apps})
+    defer s.conn.Services().Remove(bson.M{"_id": srv.Name})
+    insert := []interface{}{
+        deploy{App: "g1", Timestamp: time.Now().Add(-3600 * time.Second)},
+        deploy{App: "g1", Timestamp: time.Now()},
+    }
+    s.conn.Deploys().Insert(insert...)
+    defer s.conn.Deploys().RemoveAll(bson.M{"apps": instance.Apps})
+    expected := []deploy{insert[1].(deploy), insert[0].(deploy)}
+    deploys, err := ListDeploys(&srv)
+    c.Assert(err, gocheck.IsNil)
+    for i := 0; i < 2; i++ {
+        ts := expected[i].Timestamp
+        expected[i].Timestamp = time.Date(ts.Year(), ts.Month(), ts.Day(), ts.Hour(), ts.Minute(), ts.Second(), 0, time.UTC)
+        ts = deploys[i].Timestamp
+        deploys[i].Timestamp = time.Date(ts.Year(), ts.Month(), ts.Day(), ts.Hour(), ts.Minute(), ts.Second(), 0, time.UTC)
+        expected[i].ID = deploys[i].ID
+    }
+    c.Assert(deploys, gocheck.DeepEquals, expected)
+}
+
+func (s *S) TestListAllDeploys(c *gocheck.C) {
+    s.conn.Deploys().RemoveAll(nil)
+    insert := []interface{}{
+        deploy{App: "g1", Timestamp: time.Now().Add(-3600 * time.Second)},
+        deploy{App: "ge", Timestamp: time.Now()},
+    }
+    s.conn.Deploys().Insert(insert...)
+    defer s.conn.Deploys().RemoveAll(nil)
+    expected := []deploy{insert[1].(deploy), insert[0].(deploy)}
+    deploys, err := ListDeploys(nil)
+    c.Assert(err, gocheck.IsNil)
+    for i := 0; i < 2; i++ {
+        ts := expected[i].Timestamp
+        expected[i].Timestamp = time.Date(ts.Year(), ts.Month(), ts.Day(), ts.Hour(), ts.Minute(), ts.Second(), 0, time.UTC)
+        ts = deploys[i].Timestamp
+        deploys[i].Timestamp = time.Date(ts.Year(), ts.Month(), ts.Day(), ts.Hour(), ts.Minute(), ts.Second(), 0, time.UTC)
+        expected[i].ID = deploys[i].ID
+    }
+    c.Assert(deploys, gocheck.DeepEquals, expected)
+}
+
+func (s *S) TestGetDeploy(c *gocheck.C) {
+    s.conn.Deploys().RemoveAll(nil)
+    newDeploy := deploy{ID: bson.NewObjectId(), App: "g1", Timestamp: time.Now()}
+    err := s.conn.Deploys().Insert(&newDeploy)
+    c.Assert(err, gocheck.IsNil)
+    defer s.conn.Deploys().Remove(bson.M{"name": newDeploy.App})
+    lastDeploy, err := GetDeploy(newDeploy.ID.Hex())
+    c.Assert(err, gocheck.IsNil)
+    ts := lastDeploy.Timestamp
+    lastDeploy.Timestamp = time.Date(ts.Year(), ts.Month(), ts.Day(), ts.Hour(), ts.Minute(), ts.Second(), 0, time.UTC)
+    ts = newDeploy.Timestamp
+    newDeploy.Timestamp = time.Date(ts.Year(), ts.Month(), ts.Day(), ts.Hour(), ts.Minute(), ts.Second(), 0, time.UTC)
+    c.Assert( lastDeploy.ID, gocheck.Equals, newDeploy.ID)
+    c.Assert(lastDeploy.App, gocheck.Equals, newDeploy.App)
+    c.Assert(lastDeploy.Timestamp, gocheck.Equals, newDeploy.Timestamp)
+}
+
+func (s *S) TestGetDeployNotFound(c *gocheck.C) {
+    id_test := bson.NewObjectId()
+    deploy, err := GetDeploy(id_test.Hex())
+    c.Assert(err.Error(), gocheck.Equals, "not found")
+    c.Assert(deploy, gocheck.IsNil)
+}
+
+func (s *S) TestGetDiffInDeploys(c *gocheck.C) {
+    s.conn.Deploys().RemoveAll(nil)
+    lastDeploy := deploy{App: "g1", Timestamp: time.Now(), Commit: "1b970b076bbb30d708e262b402d4e31910e1dc10"}
+    previousDeploy := deploy{App: "g1", Timestamp: time.Now().Add(-3600 * time.Second), Commit: "545b1904af34458704e2aa06ff1aaffad5289f8f"}
+    otherAppDeploy := deploy{App: "ge", Timestamp: time.Now(), Commit: "hwed834hf8y34h8fhn8rnr823nr238runh23x"}
+    s.conn.Deploys().Insert(previousDeploy)
+    s.conn.Deploys().Insert(lastDeploy)
+    s.conn.Deploys().Insert(otherAppDeploy)
+    defer s.conn.Deploys().RemoveAll(nil)
+    expected := "test_diff"
+    h := testHandler{content: expected}
+    ts := testing.StartGandalfTestServer(&h)
+    defer ts.Close()
+    diffOutput, err := GetDiffInDeploys(&lastDeploy)
+    c.Assert(err, gocheck.IsNil)
+    c.Assert(diffOutput, gocheck.DeepEquals, expected)
+}
+
+func (s *S) TestGetDiffInDeploysWithOneCommit(c *gocheck.C) {
+    s.conn.Deploys().RemoveAll(nil)
+    lastDeploy := deploy{App: "g1", Timestamp: time.Now(), Commit: "1b970b076bbb30d708e262b402d4e31910e1dc10"}
+    s.conn.Deploys().Insert(lastDeploy)
+    defer s.conn.Deploys().RemoveAll(nil)
+    expected := "test_diff"
+    h := testHandler{content: expected}
+    ts := testing.StartGandalfTestServer(&h)
+    defer ts.Close()
+    _, err := GetDiffInDeploys(&lastDeploy)
+    c.Assert(err.Error(), gocheck.Equals, "The deployment must have at least two commits for the diff.")
+}
+
+func (s *S) TestDeployApp(c *gocheck.C) {
+    a := App{
+        Name:     "someApp",
+        Platform: "django",
+        Teams:    []string{s.team.Name},
+    }
+    err := s.conn.Apps().Insert(a)
+    c.Assert(err, gocheck.IsNil)
+    defer s.conn.Apps().Remove(bson.M{"name": a.Name})
+    s.provisioner.Provision(&a)
+    defer s.provisioner.Destroy(&a)
+    writer := &bytes.Buffer{}
+    err = Deploy(DeployOptions{
+        App:          &a,
+        Version:      "version",
+        Commit:       "1ee1f1084927b3a5db59c9033bc5c4abefb7b93c",
+        OutputStream: writer,
+    })
+    c.Assert(err, gocheck.IsNil)
+    logs := writer.String()
+    c.Assert(logs, gocheck.Equals, "Git deploy called")
+}
+
+func (s *S) TestDeployAppWithUpdatePlatform(c *gocheck.C) {
+    a := App{
+        Name:           "someApp",
+        Platform:       "django",
+        Teams:          []string{s.team.Name},
+        UpdatePlatform: true,
+    }
+    err := s.conn.Apps().Insert(a)
+    c.Assert(err, gocheck.IsNil)
+    defer s.conn.Apps().Remove(bson.M{"name": a.Name})
+    s.provisioner.Provision(&a)
+    defer s.provisioner.Destroy(&a)
+    writer := &bytes.Buffer{}
+    err = Deploy(DeployOptions{
+        App:          &a,
+        Version:      "version",
+        Commit:       "1ee1f1084927b3a5db59c9033bc5c4abefb7b93c",
+        OutputStream: writer,
+    })
+    c.Assert(err, gocheck.IsNil)
+    logs := writer.String()
+    c.Assert(logs, gocheck.Equals, "Git deploy called")
+    var updatedApp App
+    s.conn.Apps().Find(bson.M{"name": "someApp"}).One(&updatedApp)
+    c.Assert(updatedApp.UpdatePlatform, gocheck.Equals, false)
+}
+
+func (s *S) TestDeployAppIncrementDeployNumber(c *gocheck.C) {
+    a := App{
+        Name:     "otherapp",
+        Platform: "zend",
+        Teams:    []string{s.team.Name},
+    }
+    err := s.conn.Apps().Insert(a)
+    c.Assert(err, gocheck.IsNil)
+    defer s.conn.Apps().Remove(bson.M{"name": a.Name})
+    s.provisioner.Provision(&a)
+    defer s.provisioner.Destroy(&a)
+    writer := &bytes.Buffer{}
+    err = Deploy(DeployOptions{
+        App:          &a,
+        Version:      "version",
+        Commit:       "1ee1f1084927b3a5db59c9033bc5c4abefb7b93c",
+        OutputStream: writer,
+    })
+    c.Assert(err, gocheck.IsNil)
+    s.conn.Apps().Find(bson.M{"name": a.Name}).One(&a)
+    c.Assert(a.Deploys, gocheck.Equals, uint(1))
+}
+
+func (s *S) TestDeployAppSaveDeployData(c *gocheck.C) {
+    a := App{
+        Name:     "otherapp",
+        Platform: "zend",
+        Teams:    []string{s.team.Name},
+    }
+    err := s.conn.Apps().Insert(a)
+    c.Assert(err, gocheck.IsNil)
+    defer s.conn.Apps().Remove(bson.M{"name": a.Name})
+    s.provisioner.Provision(&a)
+    defer s.provisioner.Destroy(&a)
+    writer := &bytes.Buffer{}
+    commit := "1ee1f1084927b3a5db59c9033bc5c4abefb7b93c"
+    err = Deploy(DeployOptions{
+        App:          &a,
+        Version:      "version",
+        Commit:       commit,
+        OutputStream: writer,
+    })
+    c.Assert(err, gocheck.IsNil)
+    s.conn.Apps().Find(bson.M{"name": a.Name}).One(&a)
+    c.Assert(a.Deploys, gocheck.Equals, uint(1))
+    var result map[string]interface{}
+    s.conn.Deploys().Find(bson.M{"app": a.Name}).One(&result)
+    c.Assert(result["app"], gocheck.Equals, a.Name)
+    now := time.Now()
+    diff := now.Sub(result["timestamp"].(time.Time))
+    c.Assert(diff < 60*time.Second, gocheck.Equals, true)
+    c.Assert(result["duration"], gocheck.Not(gocheck.Equals), 0)
+    c.Assert(result["commit"], gocheck.Equals, commit)
+}
+
+func (s *S) TestDeployCustomPipeline(c *gocheck.C) {
+    provisioner := testing.PipelineFakeProvisioner{
+        FakeProvisioner: testing.NewFakeProvisioner(),
+    }
+    Provisioner = &provisioner
+    defer func() {
+        Provisioner = s.provisioner
+    }()
+    a := App{
+        Name:     "otherapp",
+        Platform: "zend",
+        Teams:    []string{s.team.Name},
+    }
+    err := s.conn.Apps().Insert(a)
+    c.Assert(err, gocheck.IsNil)
+    defer s.conn.Apps().Remove(bson.M{"name": a.Name})
+    provisioner.Provision(&a)
+    defer provisioner.Destroy(&a)
+    writer := &bytes.Buffer{}
+    err = Deploy(DeployOptions{
+        App:          &a,
+        Version:      "version",
+        Commit:       "1ee1f1084927b3a5db59c9033bc5c4abefb7b93c",
+        OutputStream: writer,
+    })
+    c.Assert(err, gocheck.IsNil)
+    c.Assert(provisioner.ExecutedPipeline(), gocheck.Equals, true)
+}
+
+func (s *S) TestDeployAppSaveDeployErrorData(c *gocheck.C) {
+    provisioner := testing.PipelineErrorFakeProvisioner{
+        FakeProvisioner: testing.NewFakeProvisioner(),
+    }
+    Provisioner = &provisioner
+    defer func() {
+        Provisioner = s.provisioner
+    }()
+    a := App{
+        Name:     "testErrorApp",
+        Platform: "zend",
+        Teams:    []string{s.team.Name},
+    }
+    err := s.conn.Apps().Insert(a)
+    c.Assert(err, gocheck.IsNil)
+    defer s.conn.Apps().Remove(bson.M{"name": a.Name})
+    provisioner.Provision(&a)
+    defer provisioner.Destroy(&a)
+    writer := &bytes.Buffer{}
+    err = Deploy(DeployOptions{
+        App:          &a,
+        Version:      "version",
+        Commit:       "1ee1f1084927b3a5db59c9033bc5c4abefb7b93c",
+        OutputStream: writer,
+    })
+    c.Assert(err, gocheck.NotNil)
+    var result map[string]interface{}
+    s.conn.Deploys().Find(bson.M{"app": a.Name}).One(&result)
+    c.Assert(result["app"], gocheck.Equals, a.Name)
+    c.Assert(result["error"], gocheck.NotNil)
+}
