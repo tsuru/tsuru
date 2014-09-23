aaf0540e51b57ebadd6e82189620e0126c5ec772
Cezar Sa Espinola <cezar.sa@corp.globo.com>
2014-09-23 18:19:50 -0300

    bump to 0.7.1

--- cmd/tsr/main.go
+++ cmd/tsr/main.go
@@ -20,7 +20,7 @@ const defaultConfigPath = "/etc/tsuru/tsuru.conf"
 var configPath = defaultConfigPath
 
 func buildManager() *cmd.Manager {
-	m := cmd.NewManager("tsr", "0.7.0", "", os.Stdout, os.Stderr, os.Stdin, nil)
+	m := cmd.NewManager("tsr", "0.7.1", "", os.Stdout, os.Stderr, os.Stdin, nil)
 	m.Register(&tsrCommand{Command: &apiCmd{}})
 	m.Register(&tsrCommand{Command: tokenCmd{}})
 	m.Register(&tsrCommand{Command: &healerCmd{}})
