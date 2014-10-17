443dc1d1a8428b4cf920740145df7b97f30edb6a
Francisco Souza <fss@corp.globo.com>
2014-10-17 11:46:05 -0300

    cmd/testing: add FakeGuesser
    We need this in multiple packages, ceasing replication.

---
+++ cmd/testing/guess.go
@@ -0,0 +1,41 @@
+// Copyright 2014 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package testing
+
+import "errors"
+
+// FakeGuesser represents a fake implementation of the Guesser described in the
+// cmd package.
+type FakeGuesser struct {
+	Name    string
+	guesses []string
+}
+
+// HasGuess checks whether a guess call has been recorded with the given path.
+func (f *FakeGuesser) HasGuess(path string) bool {
+	for _, g := range f.guesses {
+		if g == path {
+			return true
+		}
+	}
+	return false
+}
+
+func (f *FakeGuesser) GuessName(path string) (string, error) {
+	f.guesses = append(f.guesses, path)
+	return f.Name, nil
+}
+
+// FailingFakeGuesser is a guesser that fails in the GuessName call, with the
+// given error message.
+type FailingFakeGuesser struct {
+	FakeGuesser
+	ErrorMessage string
+}
+
+func (f *FailingFakeGuesser) GuessName(path string) (string, error) {
+	f.FakeGuesser.GuessName(path)
+	return "", errors.New(f.ErrorMessage)
+}
