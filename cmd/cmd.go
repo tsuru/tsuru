9e1f9f12169874cb03052060c82e4c732353e1e0
Marc Abramowitz <marc@marc-abramowitz.com>
2014-12-26 10:32:35 -0800

    Fuzzy string match for unknown command
    plus display commands in order for easier human scanning and automated
    testing

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -14,6 +14,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/sajari/fuzzy"
 	"github.com/tsuru/tsuru/errors"
 	"github.com/tsuru/tsuru/fs"
 	"launchpad.net/gnuflag"
@@ -130,10 +131,16 @@ func (m *Manager) Run(args []string) {
 				msg := ""
 				if os.IsNotExist(err) {
 					msg = fmt.Sprintf("%s: %q is not a tsuru command. See %q.\n", os.Args[0], args[0], "tsuru help")
-					msg = fmt.Sprintf("%s\nDid you mean?\n", msg)
+					msg += fmt.Sprintf("\nDid you mean?\n")
+					var keys []string
 					for key, _ := range m.Commands {
-						if strings.HasPrefix(key, args[0]) {
-							msg = fmt.Sprintf("%s	%s\n", msg, key)
+						keys = append(keys, key)
+					}
+					sort.Strings(keys)
+					for _, key := range keys {
+						levenshtein := fuzzy.Levenshtein(&key, &args[0])
+						if levenshtein < 3 || strings.HasPrefix(key, args[0]) {
+							msg += fmt.Sprintf("\t%s\n", key)
 						}
 					}
 				} else {
