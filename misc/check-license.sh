917ee2f818b5c5ba4e310adb5d8ff71e834096e8
Andrews Medina <andrewsmedina@gmail.com>
2015-01-05 14:30:38 -0200

    updated check-license.

--- misc/check-license.sh
+++ misc/check-license.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -e
 
-# Copyright 2014 tsuru authors. All rights reserved.
+# Copyright 2015 tsuru authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
 status=0
 
-for f in `git ls-files | xargs grep "Copyright 201[23]" -l | grep -v check-license.sh`
+for f in `git ls-files | xargs grep "Copyright 201[234]" -l | grep -v check-license.sh`
 do
 	date=`git log -1 --format="%ad" --date=short -- $f`
-	if [ `echo "$date" | grep ^2014` ]
+	if [ `echo "$date" | grep ^2015` ]
 	then
 		echo $f $date
 		status=1
