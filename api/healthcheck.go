391f2479ed2d524c5368265ac970a4eba9f1fffc
Francisco Souza <fss@corp.globo.com>
2015-01-30 17:05:46 -0200

    api: simplify healthcheck
    Now it's clever :-) Time to implement and register all
    
    Related to #967.

--- api/healthcheck.go
+++ api/healthcheck.go
@@ -9,13 +9,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/tsuru/config"
-	"github.com/tsuru/go-gandalfclient"
-	"github.com/tsuru/tsuru/app"
-	"github.com/tsuru/tsuru/db"
-	"github.com/tsuru/tsuru/iaas"
-	"github.com/tsuru/tsuru/repository"
-	"github.com/tsuru/tsuru/router"
+	"github.com/tsuru/tsuru/hc"
 )
 
 const hcOk = "WORKING"
@@ -34,92 +28,12 @@ func healthcheck(w http.ResponseWriter, r *http.Request) {
 
 func fullHealthcheck(w http.ResponseWriter, r *http.Request) {
 	var buf bytes.Buffer
+	results := hc.Check()
 	status := http.StatusOK
-	mongoDBStatus := hcOk
-	fmt.Fprint(&buf, "MongoDB: ")
-	conn, err := db.Conn()
-	if err != nil {
-		status = http.StatusInternalServerError
-		mongoDBStatus = fmt.Sprintf("failed to connect - %s", err)
-	} else {
-		defer conn.Close()
-		err = conn.Apps().Database.Session.Ping()
-		if err != nil {
+	for _, result := range results {
+		fmt.Fprintf(&buf, "%s: %s\n", result.Name, result.Status)
+		if result.Status != hc.HealthCheckOK {
 			status = http.StatusInternalServerError
-			mongoDBStatus = fmt.Sprintf("failed to ping - %s", err)
-		}
-	}
-	fmt.Fprintln(&buf, mongoDBStatus)
-	server, err := repository.ServerURL()
-	if err != nil && err != repository.ErrGandalfDisabled {
-		status = http.StatusInternalServerError
-		fmt.Fprintf(&buf, "Gandalf: %s\n", err)
-	} else if err == nil {
-		gandalfStatus := hcOk
-		fmt.Fprint(&buf, "Gandalf: ")
-		c := gandalf.Client{Endpoint: server}
-		_, err = c.GetHealthCheck()
-		if err != nil {
-			gandalfStatus = fmt.Sprintf("%s", err)
-			status = http.StatusInternalServerError
-		}
-		fmt.Fprintln(&buf, gandalfStatus)
-	}
-	if routers, err := config.Get("routers"); err == nil {
-		if routersMap, ok := routers.(map[string]interface{}); ok {
-			for routerName := range routersMap {
-				r, _ := router.Get(routerName)
-				if hrouter, ok := r.(healthchecker); ok {
-					fmt.Fprintf(&buf, "Router %q: ", routerName)
-					routerStatus := hcOk
-					if err := hrouter.Healthcheck(); err != nil {
-						status = http.StatusInternalServerError
-						routerStatus = fmt.Sprintf("fail - %s", err)
-					}
-					fmt.Fprintln(&buf, routerStatus)
-				}
-			}
-		}
-	}
-	if hprovisioner, ok := app.Provisioner.(healthchecker); ok {
-		fmt.Fprint(&buf, "Provisioner: ")
-		provisionerStatus := hcOk
-		if err := hprovisioner.Healthcheck(); err != nil {
-			status = http.StatusInternalServerError
-			provisionerStatus = fmt.Sprintf("fail - %s", err)
-		}
-		fmt.Fprintln(&buf, provisionerStatus)
-	}
-	if iaases, err := config.Get("iaas"); err == nil {
-		if iaasesMap, ok := iaases.(map[string]interface{}); ok {
-			var names []string
-			for iaasname := range iaasesMap {
-				switch iaasname {
-				case "default":
-					names = append(names, iaasesMap[iaasname].(string))
-				case "custom":
-					customIaas, ok := iaasesMap[iaasname].(map[string]interface{})
-					if ok {
-						for name := range customIaas {
-							names = append(names, name)
-						}
-					}
-				default:
-					names = append(names, iaasname)
-				}
-			}
-			for _, name := range names {
-				provider, _ := iaas.GetIaasProvider(name)
-				if hprovider, ok := provider.(healthchecker); ok {
-					fmt.Fprintf(&buf, "IaaS %q: ", name)
-					iaasStatus := hcOk
-					if err := hprovider.Healthcheck(); err != nil {
-						status = http.StatusInternalServerError
-						iaasStatus = fmt.Sprintf("fail - %s", err)
-					}
-					fmt.Fprintln(&buf, iaasStatus)
-				}
-			}
 		}
 	}
 	w.WriteHeader(status)
--- api/healthcheck_test.go
+++ api/healthcheck_test.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"net/http/httptest"
 
-	"github.com/tsuru/config"
 	"github.com/tsuru/tsuru/testing"
 	"launchpad.net/gocheck"
 )
@@ -36,42 +35,4 @@ func (s *HealthCheckSuite) TestHealthCheck(c *gocheck.C) {
 	healthcheck(recorder, request)
 	c.Assert(recorder.Code, gocheck.Equals, http.StatusOK)
 	c.Assert(recorder.Body.String(), gocheck.Equals, "WORKING")
-}
-
-func (s *HealthCheckSuite) TestFullHealthCheck(c *gocheck.C) {
-	recorder := httptest.NewRecorder()
-	request, err := http.NewRequest("GET", "/healthcheck?check=all", nil)
-	c.Assert(err, gocheck.IsNil)
-	healthcheck(recorder, request)
-	c.Assert(recorder.Code, gocheck.Equals, http.StatusOK)
-	expected := `MongoDB: WORKING
-Gandalf: WORKING
-`
-	c.Assert(recorder.Body.String(), gocheck.Equals, expected)
-}
-
-func (s *HealthCheckSuite) TestFullHealthCheckMongoAccess(c *gocheck.C) {
-	config.Set("database:url", "localhost:34456")
-	defer config.Unset("database:url")
-	recorder := httptest.NewRecorder()
-	request, err := http.NewRequest("GET", "/healthcheck?check=all", nil)
-	c.Assert(err, gocheck.IsNil)
-	healthcheck(recorder, request)
-	c.Assert(recorder.Code, gocheck.Equals, http.StatusInternalServerError)
-	expected := "MongoDB: failed to connect - no reachable servers\n"
-	c.Assert(recorder.Body.String(), gocheck.Equals, expected)
-}
-
-func (s *HealthCheckSuite) TestFullHealthCheckGandalfAccess(c *gocheck.C) {
-	config.Set("git:api-server", "localhost:0")
-	defer config.Unset("git:api-server")
-	recorder := httptest.NewRecorder()
-	request, err := http.NewRequest("GET", "/healthcheck?check=all", nil)
-	c.Assert(err, gocheck.IsNil)
-	healthcheck(recorder, request)
-	c.Assert(recorder.Code, gocheck.Equals, http.StatusInternalServerError)
-	expected := `MongoDB: WORKING
-Gandalf: Failed to connect to Gandalf server, it's probably down.
-`
-	c.Assert(recorder.Body.String(), gocheck.Equals, expected)
 }
