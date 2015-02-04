c49bb468796bcc5fb7cff93cac3df55c97f085d9
Francisco Souza <fss@corp.globo.com>
2015-02-04 20:37:02 -0200

    queue: move fake queue to the package queue/queuetest
    Let's undo the package testing :-)

--- queue/queuetest/queue_test.go
+++ queue/queuetest/queue_test.go
@@ -1,14 +1,23 @@
-// Copyright 2014 tsuru authors. All rights reserved.
+// Copyright 2015 tsuru authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package testing
+package queuetest
 
 import (
+	"testing"
 	"time"
 
 	"launchpad.net/gocheck"
 )
+
+func Test(t *testing.T) {
+	gocheck.TestingT(t)
+}
+
+type S struct{}
+
+var _ = gocheck.Suite(&S{})
 
 func (s *S) TestFakeQPubSub(c *gocheck.C) {
 	q := FakePubSubQ{}
