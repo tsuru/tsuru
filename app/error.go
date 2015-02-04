c5961bd1143f683a21e31f93f95627f461a1e975
Francisco Souza <fss@corp.globo.com>
2015-02-04 18:54:28 -0200

    app: simplify import lines

--- app/error.go
+++ app/error.go
@@ -1,12 +1,10 @@
-// Copyright 2014 tsuru authors. All rights reserved.
+// Copyright 2015 tsuru authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package app
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type AppCreationError struct {
 	app string
