6b11b49ffdeffefcc4a2da567f5f348d5f35de26
Francisco Souza <fss@corp.globo.com>
2015-02-06 17:18:34 -0200

    app: remove unused error

--- app/app.go
+++ app/app.go
@@ -37,7 +37,6 @@ var AuthScheme auth.Scheme
 var (
 	nameRegexp      = regexp.MustCompile(`^[a-z][a-z0-9-]{0,62}$`)
 	cnameRegexp     = regexp.MustCompile(`^(\*\.)?[a-zA-Z0-9][\w-.]+$`)
-	ErrAppNotEqual  = stderr.New("Apps are not equal.")
 	ErrUnitNotFound = stderr.New("unit not found")
 )
 
