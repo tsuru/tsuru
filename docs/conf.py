22e18b881c577f6910500b55b3ad76376af29c62
Francisco Souza <fss@corp.globo.com>
2015-02-23 19:09:40 -0300

    docs/conf: update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'tsuru'
-copyright = u'2014, Globo.com'
+copyright = u'2015, Globo.com'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
