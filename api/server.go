2adb106d289beb705ddc81811d75840850adea95
Francisco Souza <fss@corp.globo.com>
2015-02-23 18:19:33 -0300

    api: change version string to 0.10.0
    Not bumping the release, yet.

--- api/server.go
+++ api/server.go
@@ -22,7 +22,7 @@ import (
 	"github.com/tsuru/tsuru/router"
 )
 
-const Version = "0.9.1"
+const Version = "0.10.0"
 
 func getProvisioner() (string, error) {
 	provisioner, err := config.GetString("provisioner")
