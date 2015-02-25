6d4c0e528be8aa162a782df9a14fa8b2f21e02c0
Lucas Weiblen <lucas.weiblen@gmail.com>
2015-02-25 12:54:27 -0300

    api: improve package description

--- api/doc.go
+++ api/doc.go
@@ -1,10 +1,10 @@
-// Copyright 2013 tsuru authors. All rights reserved.
+// Copyright 2015 tsuru authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 /*
 Package api implements the REST api for tsuru.
 
-It provides the REST api for manage users, teams, apps and services.
+It provides the REST api for managing users, teams, apps and services.
 */
 package api
