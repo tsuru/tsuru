0032d2c8109fae1ab505af7d6740d2a2dff4713d
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2015-03-02 18:45:02 -0300

    IaaS Digital Ocean:
    CreateMachine via api

---
+++ iaas/digitalocean/iaas.go
@@ -0,0 +1,54 @@
+package digitalocean
+
+import (
+    "strconv"
+
+    "github.com/tarsisazevedo/godo"
+    "code.google.com/p/goauth2/oauth"
+
+    "github.com/tsuru/tsuru/iaas"
+)
+
+type DigitalOceanIaas struct {
+    base iaas.UserDataIaaS
+    client *godo.Client
+}
+
+func NewDigitalOceanIaas() *DigitalOceanIaas {
+    return &DigitalOceanIaas{base: iaas.UserDataIaaS{NamedIaaS: iaas.NamedIaaS{BaseIaaSName: "digitalocean"}}}
+}
+
+func (i *DigitalOceanIaas) Auth() error {
+    url, err :=  i.base.GetConfigString("url")
+    token, err :=  i.base.GetConfigString("token")
+    if err != nil{
+        return err
+    }
+    t := &oauth.Transport{
+        Token: &oauth.Token{AccessToken: token},
+    }
+    i.client = godo.NewClient(t.Client(), url)
+    return nil
+}
+
+func (i *DigitalOceanIaas) CreateMachine(params map[string]string) (*iaas.Machine, error) {
+    i.Auth()
+    createRequest := &godo.DropletCreateRequest{
+        Name:   params["name"],
+        Region: params["region"],
+        Size:   params["size"],
+        Image:  params["image"],
+    }
+    newDroplet, _, err := i.client.Droplets.Create(createRequest)
+    if err != nil {
+        return nil, err
+    }
+    droplet := newDroplet.Droplet
+
+    m := &iaas.Machine{
+        Address: droplet.Networks.V4[0].IPAddress,
+        Id: strconv.Itoa(droplet.ID),
+        Status: droplet.Status,
+    }
+    return m, nil
+}
---
+++ iaas/digitalocean/iaas_test.go
@@ -0,0 +1,45 @@
+// Copyright 2015 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package digitalocean
+
+import (
+    "fmt"
+    "net/http"
+    "net/http/httptest"
+
+    "testing"
+    "gopkg.in/check.v1"
+
+    "github.com/tsuru/config"
+)
+
+func Test(t *testing.T) { check.TestingT(t) }
+
+func (s *digitaloceanSuite) SetUpSuite(c *check.C) {
+    config.Set("iaas:digitalocean:token", "test")
+}
+
+type digitaloceanSuite struct{}
+
+var _ = check.Suite(&digitaloceanSuite{})
+
+func (s *digitaloceanSuite) TestCreateMachine(c *check.C) {
+    fakeServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintln(w, `{"droplet": {"id": 1, "status": "active", "networks": {"v4": [{"ip_address": "104.131.186.241", "netmask": "255.255.240.0", "gateway": "104.131.176.1", "type": "public"}], "v6": [{"ip_address": "2604:A880:0800:0010:0000:0000:031D:2001", "netmask": 64, "gateway": "2604:A880:0800:0010:0000:0000:0000:0001", "type": "public"}]}}}`)
+    }))
+    config.Set("iaas:digitalocean:url", fakeServer.URL)
+    do := NewDigitalOceanIaas()
+    params := map[string]string{"name": "example.com",
+                              "region": "nyc3",
+                              "size": "512mb",
+                              "image": "ubuntu-14-04-x64"}
+
+    m, err := do.CreateMachine(params)
+    c.Assert(err, check.IsNil)
+    c.Assert(m, check.NotNil)
+    c.Assert(m.Address, check.Equals, "104.131.186.241")
+    c.Assert(m.Id, check.Equals, "1")
+    c.Assert(m.Status, check.Equals, "active")
+}
