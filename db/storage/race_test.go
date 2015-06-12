07909a17c567026de3398b88fccf56b2e5b45d9f
Francisco Souza <fss@corp.globo.com>
2015-06-12 15:55:00 -0300

    db/storage: use assert instead of check in error checks
    Also fixing the test.

--- db/storage/race_test.go
+++ db/storage/race_test.go
@@ -19,22 +19,25 @@ func (s *S) TestOpenIsThreadSafe(c *check.C) {
 	var wg sync.WaitGroup
 	wg.Add(3)
 	go func() {
+		defer wg.Done()
 		st1, err := Open("127.0.0.1:27017", "tsuru_db_race_tests_2")
-		c.Check(err, check.IsNil)
+		c.Assert(err, check.IsNil)
+		defer st1.Close()
 		c.Check(st1.session.LiveServers(), check.DeepEquals, storage.session.LiveServers())
-		wg.Done()
 	}()
 	go func() {
-		st2, err := Open("127.0.0.1:27018", "tsuru_db_race_tests_3")
-		c.Check(err, check.IsNil)
+		defer wg.Done()
+		st2, err := Open("127.0.0.1:27017", "tsuru_db_race_tests_3")
+		c.Assert(err, check.IsNil)
+		defer st2.Close()
 		c.Check(st2.session.LiveServers(), check.DeepEquals, storage.session.LiveServers())
-		wg.Done()
 	}()
 	go func() {
+		defer wg.Done()
 		st3, err := Open("127.0.0.1:27017", "tsuru_db_race_tests_4")
-		c.Check(err, check.IsNil)
+		c.Assert(err, check.IsNil)
+		defer st3.Close()
 		c.Check(st3.session.LiveServers(), check.DeepEquals, storage.session.LiveServers())
-		wg.Done()
 	}()
 	wg.Wait()
 }
