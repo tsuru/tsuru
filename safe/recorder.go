f4c3b34e3d11f10bf54f168eb6234f971e17bc36
Francisco Souza <fss@corp.globo.com>
2015-09-10 15:54:33 -0300

    safe: add a SafeReponseRecorder
    It's safe, and slow :-D

---
+++ safe/recorder.go
@@ -0,0 +1,31 @@
+// Copyright 2015 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package safe
+
+import (
+	"net/http/httptest"
+	"sync"
+)
+
+type SafeResponseRecorder struct {
+	*httptest.ResponseRecorder
+	mut sync.Mutex
+}
+
+func NewSafeResponseRecorder() *SafeResponseRecorder {
+	return &SafeResponseRecorder{ResponseRecorder: httptest.NewRecorder()}
+}
+
+func (r *SafeResponseRecorder) Write(buf []byte) (int, error) {
+	r.mut.Lock()
+	defer r.mut.Unlock()
+	return r.ResponseRecorder.Write(buf)
+}
+
+func (r *SafeResponseRecorder) WriteHeader(code int) {
+	r.mut.Lock()
+	defer r.mut.Unlock()
+	r.ResponseRecorder.WriteHeader(code)
+}
---
+++ safe/recorder_test.go
@@ -0,0 +1,42 @@
+// Copyright 2015 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package safe
+
+import (
+	"net/http"
+	"runtime"
+	"strings"
+	"sync"
+
+	"gopkg.in/check.v1"
+)
+
+func (s *S) TestSafeResponseRecorderWrite(c *check.C) {
+	recorder := NewSafeResponseRecorder()
+	recorder.Write([]byte("some test"))
+	c.Assert(recorder.Body.String(), check.Equals, "some test")
+}
+
+func (s *S) TestSafeResponseRecorderWriteHeader(c *check.C) {
+	recorder := NewSafeResponseRecorder()
+	recorder.WriteHeader(http.StatusOK)
+	c.Assert(recorder.Code, check.Equals, http.StatusOK)
+	c.Assert(recorder.Body.String(), check.Equals, "")
+}
+
+func (s *S) TestSafeResponseRecorderWriteIsSafe(c *check.C) {
+	defer runtime.GOMAXPROCS(runtime.GOMAXPROCS(8))
+	recorder := NewSafeResponseRecorder()
+	var wg sync.WaitGroup
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			recorder.Write([]byte("test\n"))
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	c.Assert(recorder.Body.String(), check.Equals, strings.Repeat("test\n", 1000))
+}
