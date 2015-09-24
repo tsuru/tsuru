7df82009510c034b7e63588ba9002147b7b1208d
Francisco Souza <fss@corp.globo.com>
2015-09-24 16:26:39 -0300

    api: bump to 0.12.3-rc5

--- api/server.go
+++ api/server.go
@@ -27,7 +27,7 @@ import (
 	"gopkg.in/tylerb/graceful.v1"
 )
 
-const Version = "0.12.3-rc4"
+const Version = "0.12.3-rc5"
 
 func getProvisioner() (string, error) {
 	provisioner, err := config.GetString("provisioner")
