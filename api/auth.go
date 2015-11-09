90319508cc358b48520087a26cf39e81dbeb098c
Denis Aoki <dnsaoki@gmail.com>
2015-11-09 18:04:18 -0200

    api: add a parameter in token-show and token-regenerate to display/regenerate tokens for third users. Usage: tsuru token-show [--user/-u useremail]        tsuru token-show [--user/-u useremail] Obs.: only admin users can display/regenerate tokens for third users. Fix #1316.

--- api/auth.go
+++ api/auth.go
@@ -563,6 +563,15 @@ func regenerateAPIToken(w http.ResponseWriter, r *http.Request, t auth.Token) er
 	if err != nil {
 		return err
 	}
+	email := r.URL.Query().Get("user")
+	if email != "" && u.IsAdmin() {
+		u, err = auth.GetUserByEmail(email)
+		if err != nil {
+			return &errors.HTTP{Code: http.StatusNotFound, Message: err.Error()}
+		}
+	} else if email != "" {
+		return &errors.HTTP{Code: http.StatusForbidden, Message: "you're not an admin user"}
+	}
 	apiKey, err := u.RegenerateAPIKey()
 	if err != nil {
 		return err
@@ -574,6 +583,15 @@ func showAPIToken(w http.ResponseWriter, r *http.Request, t auth.Token) error {
 	u, err := t.User()
 	if err != nil {
 		return err
+	}
+	email := r.URL.Query().Get("user")
+	if email != "" && u.IsAdmin() {
+		u, err = auth.GetUserByEmail(email)
+		if err != nil {
+			return &errors.HTTP{Code: http.StatusNotFound, Message: err.Error()}
+		}
+	} else if email != "" {
+		return &errors.HTTP{Code: http.StatusForbidden, Message: "you're not an admin user"}
 	}
 	apiKey, err := u.ShowAPIKey()
 	if err != nil {
