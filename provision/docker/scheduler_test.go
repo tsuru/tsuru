fc3b64757875e1febad87f9cf6d75b057d307f38
Francisco Souza <fss@corp.globo.com>
2016-01-06 18:22:16 -0200

    provision/docker: fix vet errors

--- provision/docker/scheduler_test.go
+++ provision/docker/scheduler_test.go
@@ -358,8 +358,8 @@ func (s *S) TestSchedulerScheduleWithMemoryAwareness(c *check.C) {
 		opts := docker.CreateContainerOptions{
 			Name: cont.Name,
 		}
-		node, err := segSched.Schedule(clusterInstance, opts, []string{cont.AppName, "web"})
-		c.Assert(err, check.IsNil)
+		node, schedErr := segSched.Schedule(clusterInstance, opts, []string{cont.AppName, "web"})
+		c.Assert(schedErr, check.IsNil)
 		c.Assert(node, check.NotNil)
 	}
 	n, err := contColl.Find(bson.M{"hostaddr": "127.0.0.1"}).Count()
@@ -441,8 +441,8 @@ func (s *S) TestSchedulerScheduleWithMemoryAwarenessWithAutoScale(c *check.C) {
 		opts := docker.CreateContainerOptions{
 			Name: cont.Name,
 		}
-		node, err := segSched.Schedule(clusterInstance, opts, []string{cont.AppName, "web"})
-		c.Assert(err, check.IsNil)
+		node, schedErr := segSched.Schedule(clusterInstance, opts, []string{cont.AppName, "web"})
+		c.Assert(schedErr, check.IsNil)
 		c.Assert(node, check.NotNil)
 	}
 	n, err := contColl.Find(bson.M{"hostaddr": "127.0.0.1"}).Count()
