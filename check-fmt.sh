72782e0ae68a066b964780e243d6ce9d0ac62885
Francisco Souza <fss@corp.globo.com>
2016-01-06 19:12:59 -0200

    check-fmt: disable goimports
    See golang/go#13644.

--- check-fmt.sh
+++ check-fmt.sh
@@ -19,24 +19,26 @@ then
     status=1
 fi
 
-go get golang.org/x/tools/cmd/goimports
-out=`goimports -d -l . | grep -v vendor/`
-
-echo "*** DEBUG ***"
-echo "${out}"
-echo "*** DEBUG ***"
-
-if [ "${out}" != "" ]
-then
-    echo "ERROR: there are files that need to be formatted with goimports"
-    echo
-    echo "Files:"
-    for file in $out
-    do
-        echo "- ${file}"
-    done
-    status=1
-fi
+#
+# TODO(fss): re-enable goimports when golang/go#13644 is fixed
+#            (https://github.com/golang/go/issues/13644). There's already a
+#            patch sent by Russ Cox, available at
+#            https://go-review.googlesource.com/#/c/17728/.
+#
+# go get golang.org/x/tools/cmd/goimports
+# out=`goimports -l . | grep -v vendor/`
+#
+# if [ "${out}" != "" ]
+# then
+#     echo "ERROR: there are files that need to be formatted with goimports"
+#     echo
+#     echo "Files:"
+#     for file in $out
+#     do
+#         echo "- ${file}"
+#     done
+#     status=1
+# fi
 
 out=`go tool vet -shadow -all . 2>&1 | grep -v vendor/`
 if [ "${out}" != "" ]
