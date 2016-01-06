2a3709c1ec1fcbab340eb48f8c7aca7c9260049a
Francisco Souza <fss@corp.globo.com>
2016-01-06 18:16:38 -0200

    check-fmt: ignore files in vendor directory

--- check-fmt.sh
+++ check-fmt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Copyright 2015 tsuru authors. All rights reserved.
+# Copyright 2016 tsuru authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
 status=0
-out=`gofmt -s -l .`
+out=`gofmt -s -l . | grep -v vendor/`
 if [ "${out}" != "" ]
 then
     echo "ERROR: there are files that need to be formatted with gofmt"
@@ -20,7 +20,7 @@ then
 fi
 
 go get golang.org/x/tools/cmd/goimports
-out=`goimports -l .`
+out=`goimports -l . | grep -v vendor/`
 if [ "${out}" != "" ]
 then
     echo "ERROR: there are files that need to be formatted with goimports"
@@ -33,7 +33,7 @@ then
     status=1
 fi
 
-out=`go tool vet -shadow -all . 2>&1`
+out=`go tool vet -shadow -all . 2>&1 | grep -v vendor/`
 if [ "${out}" != "" ]
 then
     echo "ERROR: go vet failures:"
