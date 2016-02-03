0ac4fb29b99269010de4dd530ea8cce65ab3786e
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2016-02-03 20:06:38 -0200

    misc/check-license.sh: check copyright header in go files.

--- misc/check-license.sh
+++ misc/check-license.sh
@@ -5,6 +5,16 @@
 # license that can be found in the LICENSE file.
 
 status=0
+for f in `git ls-files | xargs grep -L "Copyright" | grep ".go" | grep -v vendor/`
+do
+    echo $f
+    status=1
+done
+
+if [ $status != 0 ]
+then
+   exit $status 
+fi
 
 for f in `git ls-files | xargs grep "Copyright 201[2345]" -l | grep -v check-license.sh | grep -v vendor/`
 do
