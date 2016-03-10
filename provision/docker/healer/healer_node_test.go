dd947b0b5592d9a6022fd8a855c174011567eee1
Cezar Sa Espinola <cezarsa@gmail.com>
2016-03-10 17:43:54 -0300

    provision/docker/healer: ensure node healer uses event for locking

--- provision/docker/healer/healer_node_test.go
+++ provision/docker/healer/healer_node_test.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 	"net"
 	"net/url"
+	"runtime"
+	"sync"
 	"time"
 
 	"github.com/fsouza/go-dockerclient/testing"
@@ -806,6 +808,83 @@ func (s *S) TestCheckActiveHealing(c *check.C) {
 	c.Assert(machines, check.HasLen, 1)
 	c.Assert(machines[0].Address, check.Equals, "localhost")
 
+	healingColl, err := healingCollection()
+	c.Assert(err, check.IsNil)
+	defer healingColl.Close()
+	var events []HealingEvent
+	err = healingColl.Find(nil).All(&events)
+	c.Assert(err, check.IsNil)
+	c.Assert(events, check.HasLen, 1)
+	c.Assert(events[0].Action, check.Equals, "node-healing")
+	c.Assert(events[0].StartTime, check.Not(check.DeepEquals), time.Time{})
+	c.Assert(events[0].EndTime, check.Not(check.DeepEquals), time.Time{})
+	c.Assert(events[0].Error, check.Equals, "")
+	c.Assert(events[0].Successful, check.Equals, true)
+	c.Assert(events[0].FailingNode.Address, check.Equals, fmt.Sprintf("http://127.0.0.1:%d/", dockertest.URLPort(node1.URL())))
+	c.Assert(events[0].CreatedNode.Address, check.Equals, fmt.Sprintf("http://localhost:%d", dockertest.URLPort(node2.URL())))
+}
+
+func (s *S) TestTryHealingNodeConcurrent(c *check.C) {
+	defer runtime.GOMAXPROCS(runtime.GOMAXPROCS(10))
+	factory, iaasInst := dockertest.NewHealerIaaSConstructorWithInst("127.0.0.1")
+	iaas.RegisterIaasProvider("my-healer-iaas", factory)
+	_, err := iaas.CreateMachineForIaaS("my-healer-iaas", map[string]string{})
+	c.Assert(err, check.IsNil)
+	iaasInst.Addr = "localhost"
+	node1, err := testing.NewServer("127.0.0.1:0", nil, nil)
+	c.Assert(err, check.IsNil)
+	node2, err := testing.NewServer("127.0.0.1:0", nil, nil)
+	c.Assert(err, check.IsNil)
+	config.Set("iaas:node-protocol", "http")
+	config.Set("iaas:node-port", dockertest.URLPort(node2.URL()))
+	defer config.Unset("iaas:node-protocol")
+	defer config.Unset("iaas:node-port")
+	p, err := s.newFakeDockerProvisioner(node1.URL())
+	c.Assert(err, check.IsNil)
+	defer p.Destroy()
+	app := provisiontest.NewFakeApp("myapp", "python", 0)
+	_, err = p.StartContainers(dockertest.StartContainersArgs{
+		Endpoint:  node1.URL(),
+		App:       app,
+		Amount:    map[string]int{"web": 1},
+		Image:     "tsuru/python",
+		PullImage: true,
+	})
+	c.Assert(err, check.IsNil)
+	healer := NewNodeHealer(NodeHealerArgs{
+		Provisioner:           p,
+		FailuresBeforeHealing: 1,
+		WaitTimeNewMachine:    time.Minute,
+	})
+	nodes, err := p.Cluster().UnfilteredNodes()
+	c.Assert(err, check.IsNil)
+	c.Assert(nodes, check.HasLen, 1)
+	c.Assert(dockertest.URLPort(nodes[0].Address), check.Equals, dockertest.URLPort(node1.URL()))
+	c.Assert(tsurunet.URLToHost(nodes[0].Address), check.Equals, "127.0.0.1")
+	nodes[0].Metadata["iaas"] = "my-healer-iaas"
+	machines, err := iaas.ListMachines()
+	c.Assert(err, check.IsNil)
+	c.Assert(machines, check.HasLen, 1)
+	c.Assert(machines[0].Address, check.Equals, "127.0.0.1")
+	wg := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err := healer.tryHealingNode(&nodes[0], "something")
+			c.Assert(err, check.IsNil)
+		}()
+	}
+	wg.Wait()
+	nodes, err = p.Cluster().Nodes()
+	c.Assert(err, check.IsNil)
+	c.Assert(nodes, check.HasLen, 1)
+	c.Assert(tsurunet.URLToHost(nodes[0].Address), check.Equals, "localhost")
+	c.Assert(dockertest.URLPort(nodes[0].Address), check.Equals, dockertest.URLPort(node2.URL()))
+	machines, err = iaas.ListMachines()
+	c.Assert(err, check.IsNil)
+	c.Assert(machines, check.HasLen, 1)
+	c.Assert(machines[0].Address, check.Equals, "localhost")
 	healingColl, err := healingCollection()
 	c.Assert(err, check.IsNil)
 	defer healingColl.Close()
