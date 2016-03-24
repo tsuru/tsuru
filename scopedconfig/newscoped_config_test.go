40869fec678c42023aff387dbf33ec920cfea281
Cezar Sa Espinola <cezarsa@gmail.com>
2016-03-24 11:55:35 -0300

    scopedconfig: remove old scopedconfig

--- scopedconfig/newscoped_config_test.go
+++ scopedconfig/newscoped_config_test.go
@@ -9,10 +9,42 @@ import (
 	"reflect"
 	"runtime"
 	"sync"
+	"testing"
 	"time"
 
+	"github.com/tsuru/config"
+	"github.com/tsuru/tsuru/db"
+	"github.com/tsuru/tsuru/db/dbtest"
 	"gopkg.in/check.v1"
 )
+
+type S struct {
+	storage *db.Storage
+}
+
+var _ = check.Suite(&S{})
+
+func Test(t *testing.T) {
+	check.TestingT(t)
+}
+
+func (s *S) SetUpSuite(c *check.C) {
+	config.Set("database:url", "127.0.0.1:27017")
+	config.Set("database:name", "provision_tests_s")
+	var err error
+	s.storage, err = db.Conn()
+	c.Assert(err, check.IsNil)
+}
+
+func (s *S) TearDownSuite(c *check.C) {
+	s.storage.Apps().Database.DropDatabase()
+	s.storage.Close()
+}
+
+func (s *S) SetUpTest(c *check.C) {
+	err := dbtest.ClearAllCollections(s.storage.Apps().Database)
+	c.Assert(err, check.IsNil)
+}
 
 type TestStd struct {
 	A  string
