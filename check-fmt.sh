3bf5c929a67bab325f6642e5e70f798fbf201024
Cezar Sa Espinola <cezarsa@gmail.com>
2016-04-13 15:46:43 -0300

    Revert "check-fmt: re-enable goimports check"
    This reverts commit 5bdcbcfe8e03bc5501e4820f02210153e0867738.

--- check-fmt.sh
+++ check-fmt.sh
@@ -4,10 +4,8 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-dirs=$(go list -f '{{.Dir}}/*.go' ./... | grep -v vendor)
-
 status=0
-out=`gofmt -s -l $dirs`
+out=`gofmt -s -l . | grep -v vendor/`
 if [ "${out}" != "" ]
 then
     echo "ERROR: there are files that need to be formatted with gofmt"
@@ -21,22 +19,28 @@ then
     status=1
 fi
 
-go get golang.org/x/tools/cmd/goimports
-out=`goimports -l $dirs`
+#
+# TODO(fss): re-enable goimports when golang/go#13644 is fixed
+#            (https://github.com/golang/go/issues/13644). There's already a
+#            patch sent by Russ Cox, available at
+#            https://go-review.googlesource.com/#/c/17728/.
+#
+# go get golang.org/x/tools/cmd/goimports
+# out=`goimports -l . | grep -v vendor/`
+#
+# if [ "${out}" != "" ]
+# then
+#     echo "ERROR: there are files that need to be formatted with goimports"
+#     echo
+#     echo "Files:"
+#     for file in $out
+#     do
+#         echo "- ${file}"
+#     done
+#     status=1
+# fi
 
-if [ "${out}" != "" ]
-then
-    echo "ERROR: there are files that need to be formatted with goimports"
-    echo
-    echo "Files:"
-    for file in $out
-    do
-        echo "- ${file}"
-    done
-    status=1
-fi
-
-out=`go tool vet -shadow -all $dirs 2>&1`
+out=`go tool vet -shadow -all . 2>&1 | grep -v vendor/`
 if [ "${out}" != "" ]
 then
     echo "ERROR: go vet failures:"
