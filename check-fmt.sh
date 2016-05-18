db79b75c09a73cf7329dc40496c765dd38e94de3
Cezar Sa Espinola <cezarsa@gmail.com>
2016-05-18 20:34:55 -0300

    check-fmt: do not run goimports check with go 1.5

--- check-fmt.sh
+++ check-fmt.sh
@@ -19,19 +19,22 @@ then
     status=1
 fi
 
-go get -u golang.org/x/tools/cmd/goimports
-out=`goimports -l . | grep -v vendor/`
+version=$(go version | grep -o 'go1.5')
+if [ -z ${version} ]; then
+    go get -u golang.org/x/tools/cmd/goimports
+    out=`goimports -l . | grep -v vendor/`
 
-if [ "${out}" != "" ]
-then
-    echo "ERROR: there are files that need to be formatted with goimports"
-    echo
-    echo "Files:"
-    for file in $out
-    do
-        echo "- ${file}"
-    done
-    status=1
+    if [ "${out}" != "" ]
+    then
+        echo "ERROR: there are files that need to be formatted with goimports"
+        echo
+        echo "Files:"
+        for file in $out
+        do
+            echo "- ${file}"
+        done
+        status=1
+    fi
 fi
 
 out=`go tool vet -shadow -all . 2>&1 | grep -v vendor/`
