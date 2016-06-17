c63714633bafbc35053e181686bc3ca5d56cf2a6
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2016-06-17 19:06:27 -0300

    build api docker image and send it to docker hub.

---
+++ send_to_docker_hub.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DOCKER_TAG="latest"
+
+if [ -n "${DOCKER_TAG}" ] && [ "${TRAVIS_GO_VERSION}" = "${GO_FOR_RELEASE}" ]; then
+	cat > ~/.dockercfg <<EOF
+{
+  "https://index.docker.io/v1/": {
+    "auth": "${HUB_AUTH}",
+    "email": "${HUB_EMAIL}"
+  }
+}
+EOF
+	docker build -t tsuru/api:${DOCKER_TAG} .
+	docker push tsuru/api:${DOCKER_TAG}
+else
+	echo "No image to build"
+fi
