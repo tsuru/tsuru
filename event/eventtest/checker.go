46159c2e6e06ec4f3afa9603545dee0c5b081794
Cezar Sa Espinola <cezarsa@gmail.com>
2016-06-21 17:42:36 -0300

    event/eventtest: check for endCustomData and empty events

--- event/eventtest/checker.go
+++ event/eventtest/checker.go
@@ -21,12 +21,33 @@ type EventDesc struct {
 	EndCustomData   interface{}
 	LogMatches      string
 	ErrorMatches    string
+	IsEmpty         bool
 }
 
 type hasEventChecker struct{}
 
 func (hasEventChecker) Info() *check.CheckerInfo {
 	return &check.CheckerInfo{Name: "HasEvent", Params: []string{"event desc"}}
+}
+
+func queryPartCustom(query map[string]interface{}, name string, value interface{}) {
+	if value == nil {
+		return
+	}
+	switch data := value.(type) {
+	case map[string]interface{}:
+		for k, v := range data {
+			query[name+"."+k] = v
+		}
+	case []map[string]interface{}:
+		queryPart := []bson.M{}
+		for _, el := range data {
+			queryPart = append(queryPart, bson.M{
+				name: bson.M{"$elemMatch": el},
+			})
+		}
+		query["$and"] = queryPart
+	}
 }
 
 func (hasEventChecker) Check(params []interface{}, names []string) (bool, string) {
@@ -44,33 +65,31 @@ func (hasEventChecker) Check(params []interface{}, names []string) (bool, string
 		return false, err.Error()
 	}
 	defer conn.Close()
+	if evt.IsEmpty {
+		n, err := conn.Events().Find(nil).Count()
+		if err != nil {
+			return false, err.Error()
+		}
+		if n != 0 {
+			return false, fmt.Sprintf("expected 0 events, got %d", n)
+		}
+		return true, ""
+	}
 	query := map[string]interface{}{
 		"target":     evt.Target,
 		"kind.name":  evt.Kind,
 		"owner.name": evt.Owner,
 		"running":    false,
 	}
-	if evt.StartCustomData != nil {
-		switch data := evt.StartCustomData.(type) {
-		case map[string]interface{}:
-			for k, v := range data {
-				query["startcustomdata."+k] = v
-			}
-		case []map[string]interface{}:
-			queryPart := []bson.M{}
-			for _, el := range data {
-				queryPart = append(queryPart, bson.M{
-					"startcustomdata": bson.M{"$elemMatch": el},
-				})
-			}
-			query["$and"] = queryPart
-		}
-	}
+	queryPartCustom(query, "startcustomdata", evt.StartCustomData)
+	queryPartCustom(query, "endcustomdata", evt.EndCustomData)
 	if evt.LogMatches != "" {
 		query["log"] = bson.M{"$regex": evt.LogMatches}
 	}
 	if evt.ErrorMatches != "" {
 		query["error"] = bson.M{"$regex": evt.ErrorMatches}
+	} else {
+		query["error"] = ""
 	}
 	n, err := conn.Events().Find(query).Count()
 	if err != nil {
