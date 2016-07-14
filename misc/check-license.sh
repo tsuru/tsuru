202fd38479ec14eab16445635fec5712628fedc2
Cezar Sa Espinola <cezarsa@gmail.com>
2016-07-14 10:42:27 -0300

    misc/check-license.sh now has a --fix parameter to find and fix files

--- misc/check-license.sh
+++ misc/check-license.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 # Copyright 2016 tsuru authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
@@ -13,17 +13,30 @@ done
 
 if [ $status != 0 ]
 then
-   exit $status 
+   exit $status
 fi
 
-for f in `git ls-files | xargs grep "Copyright 201[2345]" -l | grep -v check-license.sh | grep -v vendor/`
+curYear=$(date +"%Y")
+
+tofix=()
+for f in `git ls-files | grep -v vendor/ | grep -v check-license.sh | xargs -I{} bash -c "(egrep -o \"Copyright [0-9]+\" {} | grep -v ${curYear} >/dev/null) && echo {}"`
 do
 	date=`git log -1 --format="%ad" --date=short -- $f`
-	if [ `echo "$date" | grep ^2016` ]
+	if [ `echo "$date" | grep "^${curYear}"` ]
 	then
+        tofix+=($f)
 		echo $f $date
 		status=1
 	fi
 done
+
+case $1 in
+    "-f" | "--fix")
+        for f in ${tofix[@]}; do
+            sed -E -i "" "s/Copyright [0-9]+/Copyright ${curYear}/g" $f
+        done
+        exit 0
+        ;;
+esac
 
 exit $status
