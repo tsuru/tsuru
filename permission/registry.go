199702371a2d7c1ea3b55567b0dcc09694c1cb36
Andrews Medina <andrewsmedina@gmail.com>
2016-08-23 17:31:54 -0300

    update license year

--- permission/registry.go
+++ permission/registry.go
@@ -1,6 +1,4 @@
-// Copyright 2015 tsuruWithContextType authors. All rights reserved.
-
-// Copyright 2015 tsuru authors. All rights reserved.}
+// Copyright 2016 tsuru authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
