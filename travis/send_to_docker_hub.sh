b5f3de70848dbf01c17b6dc481dadb16ff052cc3
André Carvalho <andre.carvalho@corp.globo.com>
2016-08-25 11:23:03 -0300

    travis: adds versioning to tsuru/api docker image

--- travis/send_to_docker_hub.sh
+++ travis/send_to_docker_hub.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-DOCKER_TAG="latest"
+function tag_and_push {
+	if [ -n "$1" ]; then
+		echo "Pushing docker image to hub tagged as $1"
+		docker build -t tsuru/api:$1 .
+		docker push tsuru/api:$1
+	fi
+}
 
-if [ -n "$TRAVIS_TAG" ] && [[ "${TRAVIS_TAG}" =~ ([0-9]+). ]] && [[ $TRAVIS_TAG != *"rc"* ]]
-then
-    DOCKER_TAG=v${BASH_REMATCH[1]}
+LATEST_TAG="latest"
+MAJOR_TAG=""
+VERSION_TAG=""
+
+if [ -n "$TRAVIS_TAG" ] && [[ "${TRAVIS_TAG}" =~ ([0-9]+). ]]; then
+	VERSION_TAG=v$TRAVIS_TAG
+	if [[ $TRAVIS_TAG != *"rc"* ]]; then
+		MAJOR_TAG=v${BASH_REMATCH[1]}
+	fi
 fi
 
-if [ -n "${DOCKER_TAG}" ] && [ "${TRAVIS_GO_VERSION}" = "${GO_FOR_RELEASE}" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+if [ "${TRAVIS_GO_VERSION}" = "${GO_FOR_RELEASE}" ]; then
 	cat > ~/.dockercfg <<EOF
 {
   "https://index.docker.io/v1/": {
@@ -16,9 +28,11 @@ if [ -n "${DOCKER_TAG}" ] && [ "${TRAVIS_GO_VERSION}" = "${GO_FOR_RELEASE}" ] &&
   }
 }
 EOF
-	echo "Pushing docker image to hub tagged as $DOCKER_TAG"
-	docker build -t tsuru/api:${DOCKER_TAG} .
-	docker push tsuru/api:${DOCKER_TAG}
+	if [ "${TRAVIS_BRANCH}" = "master" ] || [ -n "$TRAVIS_TAG" ]; then
+		tag_and_push $LATEST_TAG
+		tag_and_push $MAJOR_TAG
+		tag_and_push $VERSION_TAG
+	fi
 else
 	echo "No image to build"
 fi
