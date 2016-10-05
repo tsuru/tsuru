58f06825dbe6e1daef82abec6ca174879effa558
Andrews Medina <andrewsmedina@gmail.com>
2016-10-05 18:44:29 -0300

    kubernetes: initial provisioner

---
+++ provision/kubernetes/provisioner.go
@@ -0,0 +1,107 @@
+// Copyright 2016 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package kubernetes
+
+import (
+	"errors"
+	"io"
+
+	"github.com/tsuru/tsuru/event"
+	"github.com/tsuru/tsuru/provision"
+)
+
+const (
+	provisionerName = "kubernetes"
+)
+
+var errNotImplemented = errors.New("not implemented")
+
+type kubernetesProvisioner struct{}
+
+func init() {
+	provision.Register(provisionerName, func() (provision.Provisioner, error) {
+		return &kubernetesProvisioner{}, nil
+	})
+}
+
+func (p *kubernetesProvisioner) GetName() string {
+	return provisionerName
+}
+
+func (p *kubernetesProvisioner) Provision(provision.App) error {
+	return errNotImplemented
+}
+
+func (p *kubernetesProvisioner) Destroy(provision.App) error {
+	return errNotImplemented
+}
+
+func (p *kubernetesProvisioner) AddUnits(provision.App, uint, string, io.Writer) ([]provision.Unit, error) {
+	return nil, errNotImplemented
+}
+
+func (p *kubernetesProvisioner) RemoveUnits(provision.App, uint, string, io.Writer) error {
+	return errNotImplemented
+}
+
+func (p *kubernetesProvisioner) SetUnitStatus(provision.Unit, provision.Status) error {
+	return errNotImplemented
+}
+
+func (p *kubernetesProvisioner) Restart(provision.App, string, io.Writer) error {
+	return errNotImplemented
+}
+
+func (p *kubernetesProvisioner) Start(provision.App, string) error {
+	return errNotImplemented
+}
+
+func (p *kubernetesProvisioner) Stop(provision.App, string) error {
+	return errNotImplemented
+}
+
+func (p *kubernetesProvisioner) Units(app provision.App) ([]provision.Unit, error) {
+	return nil, errNotImplemented
+}
+
+func (p *kubernetesProvisioner) RoutableUnits(app provision.App) ([]provision.Unit, error) {
+	return nil, errNotImplemented
+}
+
+func (p *kubernetesProvisioner) RegisterUnit(unit provision.Unit, customData map[string]interface{}) error {
+	return errNotImplemented
+}
+
+func (p *kubernetesProvisioner) SetNodeStatus(provision.NodeStatusData) error {
+	return errNotImplemented
+}
+
+func (p *kubernetesProvisioner) ListNodes(addressFilter []string) ([]provision.Node, error) {
+	return nil, errNotImplemented
+}
+
+func (p *kubernetesProvisioner) GetNode(address string) (provision.Node, error) {
+	return nil, errNotImplemented
+}
+
+func (p *kubernetesProvisioner) AddNode(opts provision.AddNodeOptions) error {
+	return errNotImplemented
+}
+
+func (p *kubernetesProvisioner) RemoveNode(opts provision.RemoveNodeOptions) error {
+	return errNotImplemented
+}
+
+func (p *kubernetesProvisioner) UpdateNode(provision.UpdateNodeOptions) error {
+	return errNotImplemented
+}
+
+func (p *kubernetesProvisioner) ArchiveDeploy(app provision.App, archiveURL string, evt *event.Event) (imgID string, err error) {
+	return "", errNotImplemented
+}
+
+func (p *kubernetesProvisioner) ImageDeploy(a provision.App, imgID string, evt *event.Event) (string, error) {
+	return "", errNotImplemented
+}
