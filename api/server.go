cb784ea1c46e6a3ed24cf63afa5723f9f4943548
Francisco Souza <f@souza.cc>
2016-10-10 10:50:36 -0400

    api: bump to 1.1.1

--- api/server.go
+++ api/server.go
@@ -32,7 +32,7 @@ import (
 	"gopkg.in/tylerb/graceful.v1"
 )
 
-const Version = "1.1.0"
+const Version = "1.1.1"
 
 func getProvisioner() (string, error) {
 	provisioner, err := config.GetString("provisioner")
