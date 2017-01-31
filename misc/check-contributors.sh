456a675a3aba0163195d66c14688f9a59c2c7b6c
Cezar Sa Espinola <cezarsa@gmail.com>
2017-01-31 17:20:07 -0200

    all: use year of file creation in copyright notice

--- misc/check-contributors.sh
+++ misc/check-contributors.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright 2013 2014 tsuru authors. All rights reserved.
+# Copyright 2013 tsuru authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
