bad6d2b72102dcd4c68e0459dfd42ceef850b638
André Carvalho <asantostc@gmail.com>
2017-02-23 11:43:35 -0300

    api: add handler to set constraint

--- api/server.go
+++ api/server.go
@@ -238,6 +238,7 @@ func RunServer(dry bool) http.Handler {
 	m.Add("1.0", "Delete", "/pools/{name}/team", AuthorizationRequiredHandler(removeTeamToPoolHandler))
 
 	m.Add("1.3", "Get", "/constraints", AuthorizationRequiredHandler(poolConstraintList))
+	m.Add("1.3", "Put", "/constraints", AuthorizationRequiredHandler(poolConstraintSet))
 
 	m.Add("1.0", "Get", "/roles", AuthorizationRequiredHandler(listRoles))
 	m.Add("1.0", "Post", "/roles", AuthorizationRequiredHandler(addRole))
