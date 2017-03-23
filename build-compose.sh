d850f51c76dbf6286dd20da3ae0e0c94bf1bf62e
André Carvalho <asantostc@gmail.com>
2017-03-23 15:40:34 -0300

    compose: uses alpine golang to build and alpine to run

--- build-compose.sh
+++ build-compose.sh
@@ -2,6 +2,6 @@
 
 # This script builds tsurud inside container and call compose to build and run a new api image.
 
-docker run --rm -v "$PWD":/go/src/github.com/tsuru/tsuru -w /go/src/github.com/tsuru/tsuru golang:1.8 sh -c 'go build -o build/tsurud ./cmd/tsurud' && \
+docker run --rm -v "$PWD":/go/src/github.com/tsuru/tsuru -w /go/src/github.com/tsuru/tsuru golang:1.8-alpine sh -c 'CGO_ENABLED=0 go build -o build/tsurud ./cmd/tsurud' && \
   docker-compose build api && \
   docker-compose up -d
