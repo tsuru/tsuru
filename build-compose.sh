dcb6cf5ed809908833a13ab2b351a7860379e994
André Carvalho <asantostc@gmail.com>
2017-03-23 14:33:31 -0300

    compose: uses golang:1.8 to build and run the api

--- build-compose.sh
+++ build-compose.sh
@@ -2,6 +2,6 @@
 
 # This script builds tsurud inside container and call compose to build and run a new api image.
 
-docker run --rm -v "$PWD":/go/src/github.com/tsuru/tsuru -w /go/src/github.com/tsuru/tsuru iron/go:dev sh -c 'go build -o build/tsurud ./cmd/tsurud' && \
+docker run --rm -v "$PWD":/go/src/github.com/tsuru/tsuru -w /go/src/github.com/tsuru/tsuru golang:1.8 sh -c 'go build -o build/tsurud ./cmd/tsurud' && \
   docker-compose build api && \
   docker-compose up -d
