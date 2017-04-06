2c3543192ab605aade693c4449482a91a80f7354
Cezar Sa Espinola <cezarsa@gmail.com>
2017-04-06 18:26:20 -0300

    vendor: latest aws-sdk-go didn't play well with dockermachine
    (cherry picked from commit ea3efa47a074c54646f8c211d620bb7cf6242431)

--- vendor/github.com/aws/aws-sdk-go/private/protocol/query/queryutil/queryutil.go
+++ vendor/github.com/aws/aws-sdk-go/private/protocol/query/queryutil/queryutil.go
@@ -76,9 +76,6 @@ func (q *queryParser) parseStruct(v url.Values, value reflect.Value, prefix stri
 		if field.PkgPath != "" {
 			continue // ignore unexported fields
 		}
-		if field.Tag.Get("ignore") != "" {
-			continue
-		}
 
 		if protocol.CanSetIdempotencyToken(value.Field(i), field) {
 			token := protocol.GetIdempotencyToken()
@@ -123,11 +120,7 @@ func (q *queryParser) parseList(v url.Values, value reflect.Value, prefix string
 
 	// check for unflattened list member
 	if !q.isEC2 && tag.Get("flattened") == "" {
-		if listName := tag.Get("locationNameList"); listName == "" {
-			prefix += ".member"
-		} else {
-			prefix += "." + listName
-		}
+		prefix += ".member"
 	}
 
 	for i := 0; i < value.Len(); i++ {
