43e7b61c12253365445dd00b67d7bb469c60c811
Guilherme Garnier <guilherme.garnier@gmail.com>
2017-05-09 15:43:34 -0300

    compose: check if ip responds before adding configuration

--- build-compose.sh
+++ build-compose.sh
@@ -4,14 +4,18 @@
 
 set -e
 
-CURRENT_OS="$(uname)"
-if [[ "$CURRENT_OS" == "Linux" ]]; then
-  sudo ip addr add dev docker0 10.200.10.1/24 || true
-elif [[ "$CURRENT_OS" == "Darwin" ]]; then
-  sudo ifconfig lo0 alias 10.200.10.1/24 || true
-else
-  echo "Unsupported OS"
-  exit 1
+IP="10.200.10.1"
+if ! `ping -c 1 -t 1 "$IP" > /dev/null`
+then
+  CURRENT_OS="$(uname)"
+  if [[ "$CURRENT_OS" == "Linux" ]]; then
+    sudo ip addr add dev docker0 $IP/24 || true
+  elif [[ "$CURRENT_OS" == "Darwin" ]]; then
+    sudo ifconfig lo0 alias $IP/24 || true
+  else
+    echo "Unsupported OS"
+    exit 1
+  fi
 fi
 
 docker run --rm -v "$PWD":/go/src/github.com/tsuru/tsuru -w /go/src/github.com/tsuru/tsuru golang:1.8-alpine sh -c 'CGO_ENABLED=0 go build -o build/tsurud ./cmd/tsurud'
