a80716875884905463b30485de091215c26ee30a
Cezar Sa Espinola <cezarsa@gmail.com>
2017-05-31 10:11:05 -0300

    db: ensure index in events' uniqueid

--- db/storage.go
+++ db/storage.go
@@ -222,10 +222,12 @@ func (s *Storage) Events() *storage.Collection {
 	ownerIndex := mgo.Index{Key: []string{"owner"}}
 	kindIndex := mgo.Index{Key: []string{"kind"}}
 	startTimeIndex := mgo.Index{Key: []string{"-starttime"}}
+	uniqueIdIndex := mgo.Index{Key: []string{"uniqueid"}}
 	c := s.Collection("events")
 	c.EnsureIndex(ownerIndex)
 	c.EnsureIndex(kindIndex)
 	c.EnsureIndex(startTimeIndex)
+	c.EnsureIndex(uniqueIdIndex)
 	return c
 }
 
