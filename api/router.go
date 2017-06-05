f3a5a7778f7e478533b4e899f61ebe81db5806c3
André Carvalho <asantostc@gmail.com>
2017-06-05 17:10:59 -0300

    api/router: filters routers based on permission
    Routers are filtered based on which pools the
    user teams have access to.

--- api/router.go
+++ api/router.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/tsuru/tsuru/auth"
 	"github.com/tsuru/tsuru/permission"
+	"github.com/tsuru/tsuru/provision"
 	"github.com/tsuru/tsuru/router"
 )
 
@@ -21,18 +22,49 @@ import (
 //   200: OK
 //   204: No content
 func listRouters(w http.ResponseWriter, r *http.Request, t auth.Token) error {
-	allowed := permission.Check(t, permission.PermAppCreate)
-	if !allowed {
-		return permission.ErrUnauthorized
+	contexts := permission.ContextsForPermission(t, permission.PermAppCreate)
+	var teams []string
+	var global bool
+	for _, c := range contexts {
+		switch c.CtxType {
+		case permission.CtxGlobal:
+			global = true
+			break
+		case permission.CtxTeam:
+			teams = append(teams, c.Value)
+		}
 	}
 	routers, err := router.List()
 	if err != nil {
 		return err
 	}
-	if len(routers) == 0 {
+	filteredRouters := routers
+	if !global {
+		routersAllowed := make(map[string]struct{})
+		filteredRouters = []router.PlanRouter{}
+		pools, err := provision.ListPossiblePools(teams)
+		if err != nil {
+			return err
+		}
+		for _, p := range pools {
+			rs, err := p.GetRouters()
+			if err != nil {
+				return err
+			}
+			for _, r := range rs {
+				routersAllowed[r] = struct{}{}
+			}
+		}
+		for _, r := range routers {
+			if _, ok := routersAllowed[r.Name]; ok {
+				filteredRouters = append(filteredRouters, r)
+			}
+		}
+	}
+	if len(filteredRouters) == 0 {
 		w.WriteHeader(http.StatusNoContent)
 		return nil
 	}
 	w.Header().Set("Content-Type", "application/json")
-	return json.NewEncoder(w).Encode(routers)
+	return json.NewEncoder(w).Encode(filteredRouters)
 }
--- api/router_test.go
+++ api/router_test.go
@@ -10,6 +10,8 @@ import (
 	"net/http/httptest"
 
 	"github.com/tsuru/config"
+	"github.com/tsuru/tsuru/permission"
+	"github.com/tsuru/tsuru/provision"
 	"github.com/tsuru/tsuru/router"
 	check "gopkg.in/check.v1"
 )
@@ -52,17 +54,29 @@ func (s *S) TestRoutersList(c *check.C) {
 	c.Assert(routers, check.DeepEquals, expected)
 }
 
-func (s *S) TestRoutersListNoAppCreatePermission(c *check.C) {
+func (s *S) TestRoutersListAppCreatePermissionTeam(c *check.C) {
 	config.Set("routers:router1:type", "foo")
 	config.Set("routers:router2:type", "bar")
 	defer config.Unset("routers:router1:type")
 	defer config.Unset("routers:router2:type")
-	token := userWithPermission(c)
+	token := userWithPermission(c, permission.Permission{
+		Scheme:  permission.PermAppCreate,
+		Context: permission.Context(permission.CtxTeam, "tsuruteam"),
+	})
+	err := provision.SetPoolConstraint(&provision.PoolConstraint{PoolExpr: "test1", Field: "router", Values: []string{"router1", "router2"}})
+	c.Assert(err, check.IsNil)
 	recorder := httptest.NewRecorder()
 	request, err := http.NewRequest("GET", "/routers", nil)
 	c.Assert(err, check.IsNil)
 	request.Header.Set("Authorization", "bearer "+token.GetValue())
 	m := RunServer(true)
 	m.ServeHTTP(recorder, request)
-	c.Assert(recorder.Code, check.Equals, http.StatusForbidden)
+	var routers []router.PlanRouter
+	c.Assert(recorder.Code, check.Equals, http.StatusOK)
+	err = json.Unmarshal(recorder.Body.Bytes(), &routers)
+	c.Assert(err, check.IsNil)
+	c.Assert(routers, check.DeepEquals, []router.PlanRouter{
+		{Name: "router1", Type: "foo"},
+		{Name: "router2", Type: "bar"},
+	})
 }
