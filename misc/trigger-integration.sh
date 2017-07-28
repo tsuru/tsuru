30ee65eef6c36cbde107ad6589e2a45c4df100cb
André Carvalho <asantostc@gmail.com>
2017-07-28 16:51:44 -0300

    integration: triggers integration tests once a tag is pushed

---
+++ misc/trigger-integration.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+# Copyright 2017 tsuru authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+if [ -n "$TRAVIS_TAG" ]
+then 
+    echo "triggering ec2 integration test for $TRAVIS_TAG"
+    curl --header "Content-Type: application/json" --data '{"build_parameters": {"TSURUVERSION": "'"$TRAVIS_TAG"'"}}' \
+    --request POST https://circleci.com/api/v1.1/project/github/tsuru/integration_ec2/tree/master?circle-token=$CIRCLE_EC2_TOKEN
+fi
