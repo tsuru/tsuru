3a98b598064c6547764fc42744ff36eb631ab69e
André Carvalho <asantostc@gmail.com>
2017-08-01 12:31:12 -0300

    misc: adds "v" prefix to tag for the integration build params

--- misc/trigger-integration.sh
+++ misc/trigger-integration.sh
@@ -7,6 +7,6 @@
 if [ -n "$TRAVIS_TAG" ]
 then 
     echo "triggering ec2 integration test for $TRAVIS_TAG"
-    curl --header "Content-Type: application/json" --data '{"build_parameters": {"TSURUVERSION": "'"$TRAVIS_TAG"'"}}' \
+    curl --header "Content-Type: application/json" --data '{"build_parameters": {"TSURUVERSION": "'"v$TRAVIS_TAG"'"}}' \
     --request POST https://circleci.com/api/v1.1/project/github/tsuru/integration_ec2/tree/master?circle-token=$CIRCLE_EC2_TOKEN
 fi
