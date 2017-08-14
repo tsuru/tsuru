2d3f480e7ded41e39bea7dcbf65cb343fea6429c
arthur <arthur.001.002@gmail.com>
2017-08-14 13:53:41 -0300

    image: modifies suite_test from master
        also modifies how the API sees the disable parameter,
        allowing It to be direct on It's intention

--- api/deploy_test.go
+++ api/deploy_test.go
@@ -1330,7 +1330,7 @@ func (s *DeploySuite) TestRollbackUpdate(c *check.C) {
 	c.Assert(d.DisableRollback, check.Equals, false)
 	v := url.Values{}
 	v.Set("disable", "true")
-	v.Set("reason", "")
+	v.Set("reason", "because of reasons")
 	v.Set("image", "v1")
 	url := fmt.Sprintf("/apps/%s/deploy/rollback/update", fakeApp.Name)
 	request, err := http.NewRequest(http.MethodPut, url, strings.NewReader(v.Encode()))
@@ -1363,7 +1363,7 @@ func (s *DeploySuite) TestRollbackUpdateInvalidImage(c *check.C) {
 	err = data.Save()
 	c.Assert(err, check.IsNil)
 	v := url.Values{}
-	v.Set("disable", "true")
+	v.Set("disable", "false")
 	v.Set("reason", "")
 	v.Set("image", "v10")
 	url := fmt.Sprintf("/apps/%s/deploy/rollback/update", fakeApp.Name)
@@ -1388,7 +1388,7 @@ func (s *DeploySuite) TestRollbackUpdateImageNotFound(c *check.C) {
 	err := app.CreateApp(&fakeApp, user)
 	c.Assert(err, check.IsNil)
 	v := url.Values{}
-	v.Set("disable", "true")
+	v.Set("disable", "false")
 	v.Set("reason", "")
 	v.Set("image", "v1")
 	url := fmt.Sprintf("/apps/%s/deploy/rollback/update", fakeApp.Name)
@@ -1436,7 +1436,7 @@ func (s *DeploySuite) TestRollbackUpdateErrEmptyReason(c *check.C) {
 	err := app.CreateApp(&fakeApp, user)
 	c.Assert(err, check.IsNil)
 	v := url.Values{}
-	v.Set("disable", "false")
+	v.Set("disable", "true")
 	v.Set("reason", "")
 	v.Set("image", "v1")
 	url := fmt.Sprintf("/apps/%s/deploy/rollback/update", fakeApp.Name)
