5be130e260c6e250f481ef936ebf760c5a4a71f5
André Carvalho <asantostc@gmail.com>
2017-08-22 10:43:28 -0300

    misc: trigger gce integration when tags are pushed

--- misc/trigger-integration.sh
+++ misc/trigger-integration.sh
@@ -9,4 +9,8 @@ then
     echo "triggering ec2 integration test for $TRAVIS_TAG"
     curl --header "Content-Type: application/json" --data '{"build_parameters": {"TSURUVERSION": "'"v$TRAVIS_TAG"'"}}' \
     --request POST https://circleci.com/api/v1.1/project/github/tsuru/integration_ec2/tree/master?circle-token=$CIRCLE_EC2_TOKEN
+
+    echo "triggering gce integration test for $TRAVIS_TAG"
+    curl --header "Content-Type: application/json" --data '{"build_parameters": {"TSURUVERSION": "'"v$TRAVIS_TAG"'"}}' \
+    --request POST https://circleci.com/api/v1.1/project/github/tsuru/integration_gce/tree/master?circle-token=$CIRCLE_GCE_TOKEN
 fi
