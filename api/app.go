59b37a77592cc7ebaabc7239652776f0ff344f9b
Vitor Falcao <vitor.falcaor@gmail.com>
2017-10-18 15:54:03 -0200

    Fixing typo and comment misformatting

--- api/app.go
+++ api/app.go
@@ -392,7 +392,7 @@ func createApp(w http.ResponseWriter, r *http.Request, t auth.Token) (err error)
 // produce: application/x-json-stream
 // responses:
 //   200: App updated
-//	 400: Invalid new pool
+//   400: Invalid new pool
 //   401: Unauthorized
 //   404: Not found
 func updateApp(w http.ResponseWriter, r *http.Request, t auth.Token) (err error) {
@@ -470,7 +470,7 @@ func updateApp(w http.ResponseWriter, r *http.Request, t auth.Token) (err error)
 			return err
 		}
 		if currentPool.Provisioner != newPool.Provisioner {
-			return &errors.HTTP{Code: http.StatusBadRequest, Message: "Moving apps between pools belonging to different provisioners is not alllowed"}
+			return &errors.HTTP{Code: http.StatusBadRequest, Message: "Moving apps between pools belonging to different provisioners is not allowed"}
 		}
 	}
 	evt, err := event.New(&event.Opts{
