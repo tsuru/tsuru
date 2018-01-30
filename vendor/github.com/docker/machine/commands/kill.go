7f76d486bcb47f72fccd8629a46447b015c4e323
André Carvalho <asantostc@gmail.com>
2018-01-30 15:25:29 -0200

    vendor: runs dep ensure with latest dep version

--- vendor/github.com/docker/machine/commands/kill.go
+++
@@ -1,7 +0,0 @@
-package commands
-
-import "github.com/docker/machine/libmachine"
-
-func cmdKill(c CommandLine, api libmachine.API) error {
-	return runAction("kill", c, api)
-}
--- vendor/github.com/vulcand/vulcand/Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM golang:1.7-onbuild
-EXPOSE 8181 8182
-RUN make install
-ENTRYPOINT ["/go/bin/vulcand"]
