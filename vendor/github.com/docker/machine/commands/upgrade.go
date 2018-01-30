b441b35ec3204d458f1378d88eb093ffcb886a9d
André Carvalho <asantostc@gmail.com>
2018-01-30 15:25:29 -0200

    vendor: runs dep ensure with latest dep version

--- vendor/github.com/docker/machine/commands/upgrade.go
+++
@@ -1,7 +0,0 @@
-package commands
-
-import "github.com/docker/machine/libmachine"
-
-func cmdUpgrade(c CommandLine, api libmachine.API) error {
-	return runAction("upgrade", c, api)
-}
--- vendor/github.com/mailgun/multibuf/README.md
+++
@@ -1,6 +0,0 @@
-Multibuf
-========
-
-Bytes buffer that implements seeking and partially persisting to disk.
-
-Please read http://godoc.org/github.com/mailgun/multibuf
