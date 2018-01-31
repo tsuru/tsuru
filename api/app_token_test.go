68c22f9194b2764cc20bdad419615bc4e467f329
Guilherme Garnier <guilherme.garnier@gmail.com>
2018-01-31 17:18:52 -0200

    api,permission: add app token delete api

--- api/app_token_test.go
+++ api/app_token_test.go
@@ -110,3 +110,62 @@ func (s *S) TestAppTokenCreateNoPermission(c *check.C) {
 	s.testServer.ServeHTTP(recorder, request)
 	c.Assert(recorder.Code, check.Equals, http.StatusForbidden)
 }
+
+func (s *S) TestAppTokenDelete(c *check.C) {
+	app1 := app.App{Name: "app1", Platform: "go", TeamOwner: s.team.Name}
+	err := app.CreateApp(&app1, s.user)
+	c.Assert(err, check.IsNil)
+	appToken := authTypes.AppToken{Token: "12345", AppName: app1.Name, CreatorEmail: s.team.Name}
+	err = auth.AppTokenService().Insert(appToken)
+	c.Assert(err, check.IsNil)
+	url := fmt.Sprintf("/1.6/apps/%s/tokens/%s", app1.Name, appToken.Token)
+	request, err := http.NewRequest("DELETE", url, nil)
+	c.Assert(err, check.IsNil)
+	request.Header.Set("authorization", "bearer "+s.token.GetValue())
+	recorder := httptest.NewRecorder()
+	s.testServer.ServeHTTP(recorder, request)
+	c.Assert(recorder.Code, check.Equals, http.StatusOK)
+
+	results, err := auth.AppTokenService().FindByAppName(app1.Name)
+	c.Assert(err, check.IsNil)
+	c.Assert(results, check.HasLen, 0)
+
+	c.Assert(eventtest.EventDesc{
+		Target: event.Target{Type: event.TargetTypeApp, Value: app1.Name},
+		Owner:  s.user.Email,
+		Kind:   "app.update.token",
+	}, eventtest.HasEvent)
+}
+
+func (s *S) TestAppTokenDeleteTokenNotFound(c *check.C) {
+	app1 := app.App{Name: "app1", Platform: "go", TeamOwner: s.team.Name}
+	err := app.CreateApp(&app1, s.user)
+	c.Assert(err, check.IsNil)
+	url := fmt.Sprintf("/1.6/apps/%s/tokens/abc123", app1.Name)
+	request, err := http.NewRequest("DELETE", url, nil)
+	c.Assert(err, check.IsNil)
+	request.Header.Set("authorization", "bearer "+s.token.GetValue())
+	recorder := httptest.NewRecorder()
+	s.testServer.ServeHTTP(recorder, request)
+	c.Assert(recorder.Code, check.Equals, http.StatusNotFound)
+}
+
+func (s *S) TestAppTokenDeleteNoPermission(c *check.C) {
+	app1 := app.App{Name: "app1", Platform: "go", TeamOwner: s.team.Name}
+	err := app.CreateApp(&app1, s.user)
+	c.Assert(err, check.IsNil)
+	appToken := authTypes.AppToken{Token: "12345", AppName: app1.Name, CreatorEmail: s.team.Name}
+	err = auth.AppTokenService().Insert(appToken)
+	c.Assert(err, check.IsNil)
+	url := fmt.Sprintf("/1.6/apps/%s/tokens/%s", app1.Name, appToken.Token)
+	request, err := http.NewRequest("DELETE", url, nil)
+	c.Assert(err, check.IsNil)
+	token := userWithPermission(c, permission.Permission{
+		Scheme:  permission.PermAppRead,
+		Context: permission.Context(permission.CtxPool, "otherpool"),
+	})
+	request.Header.Set("authorization", "bearer "+token.GetValue())
+	recorder := httptest.NewRecorder()
+	s.testServer.ServeHTTP(recorder, request)
+	c.Assert(recorder.Code, check.Equals, http.StatusForbidden)
+}
