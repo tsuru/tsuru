e2f8f2007d0c258916c95f394f8dbc2ee67cd686
Guilherme Garnier <guilherme.garnier@gmail.com>
2018-02-07 14:25:20 -0200

    api: change url for assigning role to app token

--- api/server.go
+++ api/server.go
@@ -285,7 +285,7 @@ func RunServer(dry bool) http.Handler {
 	m.Add("1.0", "Post", "/role/default", AuthorizationRequiredHandler(addDefaultRole))
 	m.Add("1.0", "Delete", "/role/default", AuthorizationRequiredHandler(removeDefaultRole))
 	m.Add("1.0", "Get", "/permissions", AuthorizationRequiredHandler(listPermissions))
-	m.Add("1.6", "Post", "/roles/{name}/apptoken", AuthorizationRequiredHandler(assignRoleToAppToken))
+	m.Add("1.6", "Post", "/roles/{name}/apptoken/{token}", AuthorizationRequiredHandler(assignRoleToAppToken))
 	m.Add("1.6", "Delete", "/roles/{name}/apptoken/{token}", AuthorizationRequiredHandler(dissociateRoleFromAppToken))
 
 	m.Add("1.0", "Get", "/debug/goroutines", AuthorizationRequiredHandler(dumpGoroutines))
