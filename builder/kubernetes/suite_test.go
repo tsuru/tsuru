83b8b9f1036d334b922d948863859ed50f5e3f59
Guilherme Garnier <guilherme.garnier@gmail.com>
2018-03-02 17:05:58 -0300

    builder/kubernetes: fix tests

--- builder/kubernetes/suite_test.go
+++ builder/kubernetes/suite_test.go
@@ -118,8 +118,6 @@ func (s *S) SetUpTest(c *check.C) {
 	_, err = nativeScheme.Create(s.user)
 	c.Assert(err, check.IsNil)
 	s.team = &authTypes.Team{Name: "admin"}
-	err = auth.TeamService().Insert(*s.team)
-	c.Assert(err, check.IsNil)
 	s.token, err = nativeScheme.Login(map[string]string{"email": s.user.Email, "password": "123456"})
 	c.Assert(err, check.IsNil)
 }
