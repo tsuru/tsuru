e10fe7bc6763a692530af0c4cc15037731f6db70
Joao Guio <joao.guio@corp.globo.com>
2018-05-18 17:05:53 -0300

    api,docs: changing update method to PUT and fixing wrong api version on specs

--- api/cluster.go
+++ api/cluster.go
@@ -89,7 +89,7 @@ func createCluster(w http.ResponseWriter, r *http.Request, t auth.Token) (err er
 
 // title: update provisioner cluster
 // path: /provisioner/clusters/{name}
-// method: POST
+// method: PUT
 // consume: application/x-www-form-urlencoded
 // produce: application/json
 // responses:
