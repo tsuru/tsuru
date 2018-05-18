5e426d2519761decc612c2dc6b4eac2d1f93237c
Joao Guio <joao.guio@corp.globo.com>
2018-05-18 17:05:53 -0300

    api,docs: changing update method to PUT and fixing wrong api version on specs

--- api/server.go
+++ api/server.go
@@ -403,7 +403,7 @@ func RunServer(dry bool) http.Handler {
 	m.Add("1.2", "GET", "/metrics", promhttp.Handler())
 
 	m.Add("1.3", "POST", "/provisioner/clusters", AuthorizationRequiredHandler(createCluster))
-	m.Add("1.4", "POST", "/provisioner/clusters/{name}", AuthorizationRequiredHandler(updateCluster))
+	m.Add("1.4", "PUT", "/provisioner/clusters/{name}", AuthorizationRequiredHandler(updateCluster))
 	m.Add("1.3", "GET", "/provisioner/clusters", AuthorizationRequiredHandler(listClusters))
 	m.Add("1.3", "DELETE", "/provisioner/clusters/{name}", AuthorizationRequiredHandler(deleteCluster))
 
