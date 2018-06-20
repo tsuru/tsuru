d80e4ed62fd924045f88fd7c466e09dc4a287d64
Guilherme Rezende <guilhermebr@gmail.com>
2018-06-20 15:26:31 -0300

    provision/kubernetes: updates default deploy-agent to 0.6.0

--- provision/kubernetes/provisioner_test.go
+++ provision/kubernetes/provisioner_test.go
@@ -1623,8 +1623,8 @@ func (s *S) TestGetKubeConfigDefaults(c *check.C) {
 	config.Unset("kubernetes")
 	kubeConf := getKubeConfig()
 	c.Assert(kubeConf, check.DeepEquals, kubernetesConfig{
-		DeploySidecarImage:        "tsuru/deploy-agent:0.5.0",
-		DeployInspectImage:        "tsuru/deploy-agent:0.5.0",
+		DeploySidecarImage:        "tsuru/deploy-agent:0.6.0",
+		DeployInspectImage:        "tsuru/deploy-agent:0.6.0",
 		APITimeout:                60 * time.Second,
 		APIShortTimeout:           5 * time.Second,
 		PodReadyTimeout:           time.Minute,
