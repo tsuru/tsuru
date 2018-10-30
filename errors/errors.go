2e8f45cece14b04714c91d15e5ec61aa4d97716a
jrcole2884 <43038957+jrcole2884@users.noreply.github.com>
2018-10-30 10:09:35 -0400

    Update errors.go

--- errors/errors.go
+++ errors/errors.go
@@ -1,6 +1,7 @@
 // Copyright 2012 tsuru authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+//If possible, add a screenshot here (you can drag and drop, png, jpg, gif, tif, etc. in this box):
 
 // Package errors provides facilities with error handling.
 package errors
