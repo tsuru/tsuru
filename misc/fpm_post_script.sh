4ff7494cdb1feb7af5be0ea6eaad72643951e195
Paulo Sousa <paulo.sousa@corp.globo.com>
2018-11-06 11:28:40 -0200

    misc/fpm: add option to disable pleaserun on postinst

--- misc/fpm_post_script.sh
+++ misc/fpm_post_script.sh
@@ -4,11 +4,17 @@ create_tsuru_user() {
   user="tsuru"
   exists=true
   getent passwd $user > /dev/null 2>&1 || exists=false
-  if ! $exists
-  then
+  if ! $exists; then
       echo "Creating user \"$user\" within group \"$user\""...
       useradd --system -md /var/lib/$user $user
   fi
 }
 
 create_tsuru_user
+
+if [ -f /etc/default/tsurud ]; then
+  . /etc/default/tsurud
+  if [ "$DISABLE_PLEASERUN" == "1" ]; then
+    exit 0
+  fi
+fi
