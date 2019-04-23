72e1fb0dc7aa7649fa1bdc4098f419cdeb91f572
Wilson Júnior <wilsonpjunior@gmail.com>
2019-04-23 19:34:12 -0300

    Integrate with HTTP API

--- api/app.go
+++ api/app.go
@@ -258,6 +258,12 @@ func appInfo(w http.ResponseWriter, r *http.Request, t auth.Token) error {
 	if !canRead {
 		return permission.ErrUnauthorized
 	}
+
+	err = a.FillInternalAddresses(r.Context())
+	if err != nil {
+		return err
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	return json.NewEncoder(w).Encode(&a)
 }
--- app/app.go
+++ app/app.go
@@ -5,6 +5,7 @@
 package app
 
 import (
+	"context"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
@@ -112,6 +113,7 @@ type App struct {
 	UUID string
 
 	// InterApp Properties implemented by provision.InterAppProvisioner
+	// it is lazy generated on the first call to FillInternalAddresses
 	InternalAddresses []*provision.AppInternalAddress `json:",omitempty" bson:"-"`
 
 	Quota       quota.Quota
@@ -134,6 +136,22 @@ func (app *App) getBuilder() (builder.Builder, error) {
 	}
 	app.builder, err = builder.GetForProvisioner(p)
 	return app.builder, err
+}
+
+func (app *App) FillInternalAddresses(ctx context.Context) error {
+	provisioner, err := app.getProvisioner()
+	if err != nil {
+		return err
+	}
+
+	if interAppProvisioner, ok := provisioner.(provision.InterAppProvisioner); ok {
+		app.InternalAddresses, err = interAppProvisioner.InternalAddresses(ctx, app)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (app *App) CleanImage(img string) error {
