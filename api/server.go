2732f00ed92d3a4c90f8e020a3b12041022e051b
Cezar Sa Espinola <cezarsa@gmail.com>
2019-05-15 14:18:53 -0300

    api: add route to retrieve a single pool

--- api/server.go
+++ api/server.go
@@ -339,6 +339,7 @@ func RunServer(dry bool) http.Handler {
 	m.Add("1.0", "Put", "/pools/{name}", AuthorizationRequiredHandler(poolUpdateHandler))
 	m.Add("1.0", "Post", "/pools/{name}/team", AuthorizationRequiredHandler(addTeamToPoolHandler))
 	m.Add("1.0", "Delete", "/pools/{name}/team", AuthorizationRequiredHandler(removeTeamToPoolHandler))
+	m.Add("1.8", "Get", "/pools/{name}", AuthorizationRequiredHandler(getPoolHandler))
 
 	m.Add("1.3", "Get", "/constraints", AuthorizationRequiredHandler(poolConstraintList))
 	m.Add("1.3", "Put", "/constraints", AuthorizationRequiredHandler(poolConstraintSet))
