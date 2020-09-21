8533ae7ec4632a844de8d4e96ed9ff500ad26968
Claudio Netto <nettinhorama@gmail.com>
2020-09-21 19:10:09 -0300

    refactor(pool): create base pool storage interface

---
+++ storage/mongodb/pool_test.go
@@ -0,0 +1,15 @@
+// Copyright 2020 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package mongodb
+
+import (
+	"github.com/tsuru/tsuru/storage/storagetest"
+	"gopkg.in/check.v1"
+)
+
+var _ = check.Suite(&storagetest.PoolSuite{
+	PoolStorage: &PoolStorage{},
+	SuiteHooks:  &mongodbBaseTest{},
+})
---
+++ types/provision/pool.go
@@ -0,0 +1,26 @@
+// Copyright 2020 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package provision
+
+import (
+	"context"
+	"errors"
+)
+
+var (
+	ErrPoolNotFound      = errors.New("pool does not exist")
+	ErrTooManyPoolsFound = errors.New("too many pools found")
+)
+
+type Pool struct {
+	Name        string `bson:"_id"`
+	Provisioner string
+	Default     bool
+}
+
+type PoolStorage interface {
+	FindAll(ctx context.Context) ([]Pool, error)
+	FindByName(ctx context.Context, name string) (*Pool, error)
+}
