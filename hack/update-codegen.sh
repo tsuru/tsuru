627dbd065b99d37f6d9495806286e0ef20c8ba28
Wilson Júnior <wilsonpjunior@gmail.com>
2020-10-19 09:54:44 -0300

    hack/update-codegen.sh: Auto install code-gen when it is necessary

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -8,6 +8,8 @@ CODE_GENERATOR_VERSION=$(cat go.sum | grep k8s.io/code-generator | awk '{print $
 echo "k8s.io/code-generator version is" $CODE_GENERATOR_VERSION
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
+go get k8s.io/code-generator@$CODE_GENERATOR_VERSION || test ok
+
 bash ~/go/pkg/mod/k8s.io/code-generator@${CODE_GENERATOR_VERSION}/generate-groups.sh all \
   github.com/tsuru/tsuru/provision/kubernetes/pkg/client github.com/tsuru/tsuru/provision/kubernetes/pkg/apis \
   tsuru:v1 \
