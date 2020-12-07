7d13f134a95ea036870f0caa7d60726f7d5d1cf3
Wilson Júnior <wilsonpjunior@gmail.com>
2020-12-07 15:44:30 -0300

    Revert "provisioner/kubernetes: simplify internal address priority using sort.SliceStable"
    This reverts commit 630aef3c7644390e13192a2d760ca5e5d7fe74a6.

--- provision/kubernetes/provisioner.go
+++ provision/kubernetes/provisioner.go
@@ -875,8 +875,39 @@ func (p *kubernetesProvisioner) InternalAddresses(ctx context.Context, a provisi
 		return nil, err
 	}
 
-	addresses := []provision.AppInternalAddress{}
+	priorizedSVCs := make([]apiv1.Service, 0, len(svcs))
+
+	n := 0
 	for _, service := range svcs {
+		// we priorize the web process without versioning
+		// in the most cases will be address used to bind related services
+		// the list of services will send to tsuru services, then they uses the first address to automatic bind
+		if service.ObjectMeta.Labels["tsuru.io/app-process"] == "web" && service.ObjectMeta.Labels["tsuru.io/app-version"] == "" {
+			priorizedSVCs = append(priorizedSVCs, service)
+			continue
+		}
+		svcs[n] = service
+		n++
+	}
+	svcs = svcs[:n]
+
+	sort.Slice(svcs, func(i, j int) bool {
+		iVersion := svcs[i].ObjectMeta.Labels["tsuru.io/app-version"]
+		jVersion := svcs[j].ObjectMeta.Labels["tsuru.io/app-version"]
+
+		if iVersion != jVersion {
+			return iVersion < jVersion
+		}
+
+		iProcess := svcs[i].ObjectMeta.Labels["tsuru.io/app-process"]
+		jProcess := svcs[j].ObjectMeta.Labels["tsuru.io/app-process"]
+
+		return iProcess < jProcess
+	})
+
+	priorizedSVCs = append(priorizedSVCs, svcs...)
+	addresses := []provision.AppInternalAddress{}
+	for _, service := range priorizedSVCs {
 		// we can't show headless services
 		if service.Spec.ClusterIP == "None" {
 			continue
@@ -891,24 +922,6 @@ func (p *kubernetesProvisioner) InternalAddresses(ctx context.Context, a provisi
 			})
 		}
 	}
-
-	sort.SliceStable(addresses, func(i, j int) (x bool) {
-		if addresses[i].Process == addresses[j].Process &&
-			addresses[i].Version == addresses[j].Version {
-			return false
-		}
-		// we priorize the web process without versioning
-		// in the most cases will be address used to bind related services
-		// the list of services will send to tsuru services, then they uses the first address to automatic bind
-		if addresses[i].Process == "web" && addresses[i].Version == "" {
-			return true
-		}
-		if addresses[i].Version != addresses[j].Version {
-			return addresses[i].Version < addresses[j].Version
-		}
-
-		return addresses[i].Process < addresses[j].Process
-	})
 	return addresses, nil
 }
 
