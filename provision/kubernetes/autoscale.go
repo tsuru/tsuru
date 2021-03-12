9d66ba9be3af3c22d1af279f53f1f9b31fad29f2
Cezar Sa Espinola <cezarsa@gmail.com>
2021-03-12 15:27:51 -0300

    kubernetes: Allow creation of VPA objects for apps using a custom annotation

--- provision/kubernetes/autoscale.go
+++ provision/kubernetes/autoscale.go
@@ -6,16 +6,25 @@ package kubernetes
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/pkg/errors"
 	tsuruErrors "github.com/tsuru/tsuru/errors"
 	"github.com/tsuru/tsuru/provision"
 	appsv1 "k8s.io/api/apps/v1"
+	autoscalingv1 "k8s.io/api/autoscaling/v1"
 	autoscalingv2 "k8s.io/api/autoscaling/v2beta2"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	vpav1 "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1"
+)
+
+const (
+	annotationEnableVPA = "app.tsuru.io/enable-vpa"
+
+	vpaCRDName = "verticalpodautoscalers.autoscaling.k8s.io"
 )
 
 var errNoDeploy = errors.New("no routable version found for app, at least one deploy is required before configuring autoscale")
@@ -104,7 +113,7 @@ func (p *kubernetesProvisioner) SetAutoScale(ctx context.Context, a provision.Ap
 }
 
 func setAutoScale(ctx context.Context, client *ClusterClient, a provision.App, spec provision.AutoScaleSpec) error {
-	depInfo, err := minimumAutoScaleVersion(ctx, client, a, spec)
+	depInfo, err := minimumAutoScaleVersion(ctx, client, a, spec.Process)
 	if err != nil {
 		return err
 	}
@@ -180,7 +189,7 @@ func setAutoScale(ctx context.Context, client *ClusterClient, a provision.App, s
 	return nil
 }
 
-func minimumAutoScaleVersion(ctx context.Context, client *ClusterClient, a provision.App, spec provision.AutoScaleSpec) (*deploymentInfo, error) {
+func minimumAutoScaleVersion(ctx context.Context, client *ClusterClient, a provision.App, process string) (*deploymentInfo, error) {
 	depGroups, err := deploymentsDataForApp(ctx, client, a)
 	if err != nil {
 		return nil, err
@@ -191,11 +200,11 @@ func minimumAutoScaleVersion(ctx context.Context, client *ClusterClient, a provi
 	var depInfoRoutable, depInfoNonRoutable *deploymentInfo
 
 	for version, deps := range depGroups.versioned {
-		if spec.Process == "" && len(deps) > 1 {
+		if process == "" && len(deps) > 1 {
 			return nil, provision.InvalidProcessError{Msg: "process argument is required"}
 		}
 		for i, dep := range deps {
-			if spec.Process != "" && spec.Process != dep.process {
+			if process != "" && process != dep.process {
 				continue
 			}
 			if dep.dep.Spec.Replicas == nil || *dep.dep.Spec.Replicas == 0 {
@@ -225,7 +234,122 @@ func minimumAutoScaleVersion(ctx context.Context, client *ClusterClient, a provi
 	return depInfo, nil
 }
 
+func vpaCRDExists(ctx context.Context, client *ClusterClient) (bool, error) {
+	extClient, err := ExtensionsClientForConfig(client.restConfig)
+	if err != nil {
+		return false, err
+	}
+	_, err = extClient.ApiextensionsV1beta1().CustomResourceDefinitions().Get(ctx, vpaCRDName, metav1.GetOptions{})
+	if k8sErrors.IsNotFound(err) {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
 func ensureAutoScale(ctx context.Context, client *ClusterClient, a provision.App, process string) error {
+	multiErr := tsuruErrors.NewMultiError()
+
+	err := ensureHPA(ctx, client, a, process)
+	if err != nil {
+		multiErr.Add(err)
+	}
+
+	hasVPA, err := vpaCRDExists(ctx, client)
+	if err != nil {
+		multiErr.Add(err)
+	}
+
+	if !hasVPA {
+		return multiErr.ToError()
+	}
+
+	rawEnableVPA, _ := a.GetMetadata().Annotation(annotationEnableVPA)
+	if enableVPA, _ := strconv.ParseBool(rawEnableVPA); enableVPA {
+		err = ensureVPA(ctx, client, a, process)
+		if err != nil {
+			multiErr.Add(err)
+		}
+	} else {
+		err = ensureVPADeleted(ctx, client, a, process)
+		if err != nil {
+			multiErr.Add(err)
+		}
+	}
+
+	return multiErr.ToError()
+}
+
+func ensureVPA(ctx context.Context, client *ClusterClient, a provision.App, process string) error {
+	cli, err := VPAClientForConfig(client.RestConfig())
+	if err != nil {
+		return err
+	}
+	ns, err := client.AppNamespace(ctx, a)
+	if err != nil {
+		return err
+	}
+	depInfo, err := minimumAutoScaleVersion(ctx, client, a, process)
+	if err != nil {
+		return err
+	}
+
+	vpaUpdateOff := vpav1.UpdateModeOff
+	vpa := &vpav1.VerticalPodAutoscaler{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: vpaNameForApp(a, process),
+		},
+		Spec: vpav1.VerticalPodAutoscalerSpec{
+			TargetRef: &autoscalingv1.CrossVersionObjectReference{
+				APIVersion: appsv1.SchemeGroupVersion.String(),
+				Kind:       "Deployment",
+				Name:       depInfo.dep.Name,
+			},
+			UpdatePolicy: &vpav1.PodUpdatePolicy{
+				UpdateMode: &vpaUpdateOff,
+			},
+		},
+	}
+
+	existingVPA, err := cli.AutoscalingV1().VerticalPodAutoscalers(ns).Get(ctx, vpa.Name, metav1.GetOptions{})
+	if k8sErrors.IsNotFound(err) {
+		existingVPA = nil
+	} else if err != nil {
+		return errors.WithStack(err)
+	}
+
+	if existingVPA != nil {
+		vpa.ResourceVersion = existingVPA.ResourceVersion
+		_, err = cli.AutoscalingV1().VerticalPodAutoscalers(ns).Update(ctx, vpa, metav1.UpdateOptions{})
+	} else {
+		_, err = cli.AutoscalingV1().VerticalPodAutoscalers(ns).Create(ctx, vpa, metav1.CreateOptions{})
+	}
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
+}
+
+func ensureVPADeleted(ctx context.Context, client *ClusterClient, a provision.App, process string) error {
+	cli, err := VPAClientForConfig(client.RestConfig())
+	if err != nil {
+		return err
+	}
+	ns, err := client.AppNamespace(ctx, a)
+	if err != nil {
+		return err
+	}
+	err = cli.AutoscalingV1().VerticalPodAutoscalers(ns).Delete(ctx, vpaNameForApp(a, process), metav1.DeleteOptions{})
+	if err != nil && !k8sErrors.IsNotFound(err) {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+func ensureHPA(ctx context.Context, client *ClusterClient, a provision.App, process string) error {
 	autoScaleSpecs, err := getAutoScale(ctx, client, a, process)
 	if err != nil {
 		return err
