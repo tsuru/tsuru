69c6f57c99594a528311f0d438a58a0e4fee52a1
Wilson Júnior <wilsonpjunior@gmail.com>
2022-05-20 10:22:59 -0300

    Add k8s tracker support

--- api/tracker/tracker_test.go
+++ api/tracker/tracker_test.go
@@ -12,7 +12,11 @@ import (
 	"github.com/tsuru/tsuru/db"
 	"github.com/tsuru/tsuru/db/dbtest"
 	_ "github.com/tsuru/tsuru/storage/mongodb"
+	"github.com/tsuru/tsuru/types/tracker"
 	check "gopkg.in/check.v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
 )
 
 func Test(t *testing.T) { check.TestingT(t) }
@@ -55,6 +59,67 @@ func (s *S) Test_InstanceService_CurrentInstance(c *check.C) {
 	svc, err := InstanceService()
 	c.Assert(err, check.IsNil)
 	svc.(*instanceTracker).Shutdown(context.Background())
+	instance, err := svc.CurrentInstance(context.TODO())
+	c.Assert(err, check.IsNil)
+	c.Assert(instance.Name, check.Not(check.Equals), "")
+	c.Assert(len(instance.Addresses) > 0, check.Equals, true)
+}
+
+func (s *S) Test_K8sInstanceService(c *check.C) {
+	cli := fake.NewSimpleClientset()
+	svc := &k8sInstanceTracker{
+		cli:     cli,
+		ns:      "tsuru-system",
+		service: "tsuru-api",
+		currentInstance: tracker.TrackedInstance{
+			Name:      "tsuru-api-0",
+			Addresses: []string{"10.9.9.9"},
+		},
+	}
+
+	cli.CoreV1().Endpoints(svc.ns).Create(context.TODO(), &v1.Endpoints{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: svc.service,
+		},
+		Subsets: []v1.EndpointSubset{
+			{
+				Addresses: []v1.EndpointAddress{
+					{
+						IP: "10.1.1.1",
+						TargetRef: &v1.ObjectReference{
+							Name: "tsuru-api-0",
+						},
+					},
+					{
+						IP: "10.1.1.2",
+						TargetRef: &v1.ObjectReference{
+							Name: "tsuru-api-1",
+						},
+					},
+				},
+				Ports: []v1.EndpointPort{
+					{
+						Name: "http",
+						Port: 8080,
+					},
+					{
+						Name: "https",
+						Port: 8043,
+					},
+				},
+			},
+		},
+	}, metav1.CreateOptions{})
+
+	instances, err := svc.LiveInstances(context.TODO())
+	c.Assert(err, check.IsNil)
+	c.Assert(instances, check.HasLen, 2)
+	c.Assert(instances[0].Name, check.Equals, "tsuru-api-0")
+	c.Assert(instances[1].Name, check.Equals, "tsuru-api-1")
+
+	c.Assert(instances[0].Addresses[0], check.Equals, "10.1.1.1")
+	c.Assert(instances[1].Addresses[0], check.Equals, "10.1.1.2")
+
 	instance, err := svc.CurrentInstance(context.TODO())
 	c.Assert(err, check.IsNil)
 	c.Assert(instance.Name, check.Not(check.Equals), "")
