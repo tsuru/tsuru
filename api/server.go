fca6d9c589378a484eaf7e72fb576892fdc3082b
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2022-08-19 12:00:41 -0300

    add api "/teams/{name}/quota"

--- api/server.go
+++ api/server.go
@@ -360,6 +360,8 @@ func RunServer(dry bool) http.Handler {
 	m.Add("1.0", http.MethodDelete, "/teams/{name}", AuthorizationRequiredHandler(removeTeam))
 	m.Add("1.6", http.MethodPut, "/teams/{name}", AuthorizationRequiredHandler(updateTeam))
 	m.Add("1.4", http.MethodGet, "/teams/{name}", AuthorizationRequiredHandler(teamInfo))
+	m.Add("1.12", http.MethodGet, "/teams/{name}/quota", AuthorizationRequiredHandler(getTeamQuota))
+	m.Add("1.12", http.MethodPut, "/teams/{name}/quota", AuthorizationRequiredHandler(changeTeamQuota))
 
 	m.Add("1.0", http.MethodPost, "/swap", AuthorizationRequiredHandler(swap))
 
