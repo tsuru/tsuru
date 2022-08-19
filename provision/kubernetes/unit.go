c9471f86920864dc5ce756f26163610b9b6885e3
Wilson Júnior <wilsonpjunior@gmail.com>
2022-08-19 17:23:52 -0300

    kubernetes: Implement KillUnitProvisioner

--- provision/kubernetes/unit.go
+++ provision/kubernetes/unit.go
@@ -6,10 +6,51 @@ package kubernetes
 
 import (
 	"context"
+	"fmt"
 
-	appTypes "github.com/tsuru/tsuru/types/app"
+	"github.com/pkg/errors"
+	"github.com/tsuru/tsuru/provision"
+	policyV1Beta1 "k8s.io/api/policy/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func (p *kubernetesProvisioner) KillUnit(ctx context.Context, app appTypes.App, unitName string, force bool) error {
+func (p *kubernetesProvisioner) KillUnit(ctx context.Context, app provision.App, unitName string, force bool) error {
+	clusterClient, err := clusterForPool(ctx, app.GetPool())
+	if err != nil {
+		return err
+	}
+
+	ns, err := clusterClient.AppNamespace(ctx, app)
+	if err != nil {
+		return err
+	}
+
+	pod, err := clusterClient.CoreV1().Pods(ns).Get(ctx, unitName, metav1.GetOptions{})
+	if err != nil {
+		return errors.Wrap(err, "Unable to find pod")
+	}
+
+	appName := app.GetName()
+	if pod.Labels["tsuru.io/app-name"] != appName {
+		return fmt.Errorf("Unit %q does not belong to app %q", unitName, appName)
+	}
+
+	if force {
+		err = clusterClient.CoreV1().Pods(ns).Delete(ctx, pod.Name, metav1.DeleteOptions{})
+		if err != nil {
+			return errors.Wrap(err, "Unable to delete pod")
+		}
+		return nil
+	}
+
+	err = clusterClient.CoreV1().Pods(ns).Evict(ctx, &policyV1Beta1.Eviction{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      pod.Name,
+			Namespace: pod.Namespace,
+		},
+	})
+	if err != nil {
+		return errors.Wrap(err, "Unable to evict pod")
+	}
 	return nil
 }
