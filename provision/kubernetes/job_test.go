e2bcb864b772deb906383b8a5053d8ba45b9c7bd
Arthur Cavalcante Gomes Coelho <arthur.cavalcante.puc@gmail.com>
2023-03-14 10:43:59 -0300

    chore: add create job tests

--- provision/kubernetes/job_test.go
+++ provision/kubernetes/job_test.go
@@ -17,46 +17,48 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func (s *S) TestProvisionerCreateJob(c *check.C) {
+func (s *S) TestProvisionerCreateCronJob(c *check.C) {
 	waitCron := s.mock.CronJobReactions(c)
 	defer waitCron()
 
 	tests := []struct {
+		name           string
 		scenario       func()
 		expectedTarget *apiv1beta1.CronJob
 	}{
 		{
+			name: "simple create cronjob",
 			scenario: func() {
 				cj := job.Job{
-						Name:      "myjob",
-						TeamOwner: s.team.Name,
-						Pool:      "test-default",
-						Metadata: app.Metadata{
-							Labels: []app.MetadataItem{
-								{
-									Name: "label1",
-									Value: "value1",
-								},
-							},
-							Annotations: []app.MetadataItem{
-								{
-									Name: "annotation1",
-									Value: "value2",
-								},
+					Name:      "myjob",
+					TeamOwner: s.team.Name,
+					Pool:      "test-default",
+					Metadata: app.Metadata{
+						Labels: []app.MetadataItem{
+							{
+								Name:  "label1",
+								Value: "value1",
 							},
 						},
-						Spec: job.JobSpec{
-							Schedule: "* * * * *",
-							Parallelism: func() *int32 { r := int32(3); return &r}(),
-							Completions: func() *int32 { r := int32(1); return &r}(),
-							ActiveDeadlineSeconds: func() *int64 { r := int64(5*60); return &r}(),
-							BackoffLimit: func() *int32 { r := int32(7); return &r}(),
-							Container: jobTypes.ContainerInfo{
-								Name:    "c1",
-								Image:   "ubuntu:latest",
-								Command: []string{"echo", "hello world"},
+						Annotations: []app.MetadataItem{
+							{
+								Name:  "annotation1",
+								Value: "value2",
 							},
 						},
+					},
+					Spec: job.JobSpec{
+						Schedule:              "* * * * *",
+						Parallelism:           func() *int32 { r := int32(3); return &r }(),
+						Completions:           func() *int32 { r := int32(1); return &r }(),
+						ActiveDeadlineSeconds: func() *int64 { r := int64(5 * 60); return &r }(),
+						BackoffLimit:          func() *int32 { r := int32(7); return &r }(),
+						Container: jobTypes.ContainerInfo{
+							Name:    "c1",
+							Image:   "ubuntu:latest",
+							Command: []string{"echo", "hello world"},
+						},
+					},
 				}
 				_, err := s.p.CreateJob(context.TODO(), &cj)
 				waitCron()
@@ -64,62 +66,166 @@ func (s *S) TestProvisionerCreateJob(c *check.C) {
 			},
 			expectedTarget: &apiv1beta1.CronJob{
 				ObjectMeta: v1.ObjectMeta{
-					Name: "myjob",
+					Name:      "myjob",
 					Namespace: "default",
 					Labels: map[string]string{
-					"job.kubernetes.io/component":"tsuru-job",
-					"job.kubernetes.io/managed-by":"tsuru",
-					"job.kubernetes.io/name":"myjob",
-					"tsuru.io/is-tsuru":"true",
-					"tsuru.io/job-name":"myjob",
-					"tsuru.io/job-pool":"test-default",
-					"tsuru.io/job-team":"admin",
-					"label1":"value1",
+						"job.kubernetes.io/component":  "tsuru-job",
+						"job.kubernetes.io/managed-by": "tsuru",
+						"job.kubernetes.io/name":       "myjob",
+						"tsuru.io/is-tsuru":            "true",
+						"tsuru.io/job-name":            "myjob",
+						"tsuru.io/job-pool":            "test-default",
+						"tsuru.io/job-team":            "admin",
+						"label1":                       "value1",
+					},
+					Annotations: map[string]string{"annotation1": "value2"},
 				},
-				Annotations: map[string]string{"annotation1":"value2"},
-			},
-			Spec: apiv1beta1.CronJobSpec{
-				Schedule: "* * * * *",
-				JobTemplate: apiv1beta1.JobTemplateSpec{
-					Spec: batchv1.JobSpec{
-						Parallelism: func() *int32 { r := int32(3); return &r}(),
-						Completions: func() *int32 { r := int32(1); return &r}(),
-						ActiveDeadlineSeconds: func() *int64 { r := int64(5*60); return &r}(),
-						BackoffLimit: func() *int32 { r := int32(7); return &r}(),
-						Template: corev1.PodTemplateSpec{
-							ObjectMeta: v1.ObjectMeta{
-								Labels: map[string]string{
-									"job.kubernetes.io/component":"tsuru-job",
-									"job.kubernetes.io/managed-by":"tsuru",
-									"job.kubernetes.io/name":"myjob",
-									"tsuru.io/is-tsuru":"true",
-									"tsuru.io/job-name":"myjob",
-									"tsuru.io/job-pool":"test-default",
-									"tsuru.io/job-team":"admin",
-									"label1":"value1",
-								},
-								Annotations: map[string]string{"annotation1":"value2"},
-							},
-							Spec: corev1.PodSpec{
-								Containers: []corev1.Container{
-									{
-										Name: "c1",
-										Image: "ubuntu:latest",
-										Command: []string{"echo", "hello world"},
+				Spec: apiv1beta1.CronJobSpec{
+					Schedule: "* * * * *",
+					JobTemplate: apiv1beta1.JobTemplateSpec{
+						Spec: batchv1.JobSpec{
+							Parallelism:           func() *int32 { r := int32(3); return &r }(),
+							Completions:           func() *int32 { r := int32(1); return &r }(),
+							ActiveDeadlineSeconds: func() *int64 { r := int64(5 * 60); return &r }(),
+							BackoffLimit:          func() *int32 { r := int32(7); return &r }(),
+							Template: corev1.PodTemplateSpec{
+								ObjectMeta: v1.ObjectMeta{
+									Labels: map[string]string{
+										"job.kubernetes.io/component":  "tsuru-job",
+										"job.kubernetes.io/managed-by": "tsuru",
+										"job.kubernetes.io/name":       "myjob",
+										"tsuru.io/is-tsuru":            "true",
+										"tsuru.io/job-name":            "myjob",
+										"tsuru.io/job-pool":            "test-default",
+										"tsuru.io/job-team":            "admin",
+										"label1":                       "value1",
 									},
+									Annotations: map[string]string{"annotation1": "value2"},
 								},
-								RestartPolicy: "OnFailure",
+								Spec: corev1.PodSpec{
+									Containers: []corev1.Container{
+										{
+											Name:    "c1",
+											Image:   "ubuntu:latest",
+											Command: []string{"echo", "hello world"},
+										},
+									},
+									RestartPolicy: "OnFailure",
+								},
 							},
 						},
 					},
 				},
 			},
 		},
-	},
-}
+	}
 	for _, tt := range tests {
 		tt.scenario()
-		gotCron, err := s.client.BatchV1beta1().CronJobs("default").Get(context.TODO(), "myjob", v1.GetOptions{})
+		gotCron, err := s.client.BatchV1beta1().CronJobs(tt.expectedTarget.Namespace).Get(context.TODO(), tt.expectedTarget.Name, v1.GetOptions{})
+		c.Assert(err, check.IsNil)
+		c.Assert(*gotCron, check.DeepEquals, *tt.expectedTarget)
+	}
+}
+
+func (s *S) TestProvisionerCreateJob(c *check.C) {
+	waitCron := s.mock.CronJobReactions(c)
+	defer waitCron()
+
+	tests := []struct {
+		name           string
+		scenario       func()
+		expectedTarget *batchv1.Job
+	}{
+		{
+			name: "simple create job",
+			scenario: func() {
+				j := job.Job{
+					Name:      "myjob",
+					TeamOwner: s.team.Name,
+					Pool:      "test-default",
+					Metadata: app.Metadata{
+						Labels: []app.MetadataItem{
+							{
+								Name:  "label1",
+								Value: "value1",
+							},
+						},
+						Annotations: []app.MetadataItem{
+							{
+								Name:  "annotation1",
+								Value: "value2",
+							},
+						},
+					},
+					Spec: job.JobSpec{
+						Parallelism:           func() *int32 { r := int32(3); return &r }(),
+						Completions:           func() *int32 { r := int32(1); return &r }(),
+						ActiveDeadlineSeconds: func() *int64 { r := int64(5 * 60); return &r }(),
+						BackoffLimit:          func() *int32 { r := int32(7); return &r }(),
+						Container: jobTypes.ContainerInfo{
+							Name:    "c1",
+							Image:   "ubuntu:latest",
+							Command: []string{"echo", "hello world"},
+						},
+					},
+				}
+				_, err := s.p.CreateJob(context.TODO(), &j)
+				waitCron()
+				c.Assert(err, check.IsNil)
+			},
+			expectedTarget: &batchv1.Job{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "myjob",
+					Namespace: "default",
+					Labels: map[string]string{
+						"job.kubernetes.io/component":  "tsuru-job",
+						"job.kubernetes.io/managed-by": "tsuru",
+						"job.kubernetes.io/name":       "myjob",
+						"tsuru.io/is-tsuru":            "true",
+						"tsuru.io/job-name":            "myjob",
+						"tsuru.io/job-pool":            "test-default",
+						"tsuru.io/job-team":            "admin",
+						"label1":                       "value1",
+					},
+					Annotations: map[string]string{"annotation1": "value2"},
+				},
+				Spec: batchv1.JobSpec{
+					Parallelism:           func() *int32 { r := int32(3); return &r }(),
+					Completions:           func() *int32 { r := int32(1); return &r }(),
+					ActiveDeadlineSeconds: func() *int64 { r := int64(5 * 60); return &r }(),
+					BackoffLimit:          func() *int32 { r := int32(7); return &r }(),
+					Template: corev1.PodTemplateSpec{
+						ObjectMeta: v1.ObjectMeta{
+							Labels: map[string]string{
+								"job.kubernetes.io/component":  "tsuru-job",
+								"job.kubernetes.io/managed-by": "tsuru",
+								"job.kubernetes.io/name":       "myjob",
+								"tsuru.io/is-tsuru":            "true",
+								"tsuru.io/job-name":            "myjob",
+								"tsuru.io/job-pool":            "test-default",
+								"tsuru.io/job-team":            "admin",
+								"label1":                       "value1",
+							},
+							Annotations: map[string]string{"annotation1": "value2"},
+						},
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:    "c1",
+									Image:   "ubuntu:latest",
+									Command: []string{"echo", "hello world"},
+								},
+							},
+							RestartPolicy: "OnFailure",
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt.scenario()
+		gotCron, err := s.client.BatchV1().Jobs(tt.expectedTarget.Namespace).Get(context.TODO(), tt.expectedTarget.Name, v1.GetOptions{})
 		c.Assert(err, check.IsNil)
 		c.Assert(*gotCron, check.DeepEquals, *tt.expectedTarget)
 	}
