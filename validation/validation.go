89f55da956e7913bdf3e84e3d919285558f9e8e3
Arthur Cavalcante Gomes Coelho <arthur.cavalcante.puc@gmail.com>
2023-03-14 15:10:43 -0300

    fix: gofmted -s -w /validation/validation.go

--- validation/validation.go
+++ validation/validation.go
@@ -23,9 +23,9 @@ func ValidateEmail(email string) bool {
 // It checks if the value has more or equal `min` chars and less or equal `max`
 // chars. If you don't want to check both, just pass a zero value:
 //
-//     ValidateLength(value, 0, 100) // Checks if value has at most 100 characters
-//     ValidateLength(value, 100, 0) // Checks if value has at least 100 characters
-//     ValidateLength(value, 20, 100) // Checks if value has at least 20 characters and at most 100 characters
+//	ValidateLength(value, 0, 100) // Checks if value has at most 100 characters
+//	ValidateLength(value, 100, 0) // Checks if value has at least 100 characters
+//	ValidateLength(value, 20, 100) // Checks if value has at least 20 characters and at most 100 characters
 func ValidateLength(value string, min, max int) bool {
 	l := len(value)
 	if min > 0 && l < min {
