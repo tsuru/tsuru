f5db6ae6599bc9f18774ff9b51374d8dc6b2d8bb
Wilson Júnior <wilsonpjunior@gmail.com>
2023-03-31 16:20:42 -0300

    server: Fix path of unbind job

--- api/server.go
+++ api/server.go
@@ -236,7 +236,7 @@ func RunServer(dry bool) http.Handler {
 	m.Add("1.13", http.MethodPut, "/services/{service}/instances/{instance}/apps/{app}", AuthorizationRequiredHandler(bindServiceInstance))
 	m.Add("1.13", http.MethodDelete, "/services/{service}/instances/{instance}/apps/{app}", AuthorizationRequiredHandler(unbindServiceInstance))
 	m.Add("1.13", http.MethodPut, "/services/{service}/instances/{instance}/jobs/{job}", AuthorizationRequiredHandler(bindJobServiceInstance))
-	m.Add("1.13", http.MethodDelete, "/services/{service}/instances/{instance}/job/{job}", AuthorizationRequiredHandler(unbindJobServiceInstance))
+	m.Add("1.13", http.MethodDelete, "/services/{service}/instances/{instance}/jobs/{job}", AuthorizationRequiredHandler(unbindJobServiceInstance))
 
 	m.Add("1.0", http.MethodPut, "/services/{service}/instances/permission/{instance}/{team}", AuthorizationRequiredHandler(serviceInstanceGrantTeam))
 	m.Add("1.0", http.MethodDelete, "/services/{service}/instances/permission/{instance}/{team}", AuthorizationRequiredHandler(serviceInstanceRevokeTeam))
