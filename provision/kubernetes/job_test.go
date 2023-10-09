7085c2bf91fdbe529651f8fe2f393c02b98cf316
Paulo Sousa <paulo.sousa@corp.globo.com>
2023-10-09 16:19:36 -0300

    provision/k8s/job: fix TestProvisionerTriggerCron test

--- provision/kubernetes/job_test.go
+++ provision/kubernetes/job_test.go
@@ -544,6 +544,7 @@ func (s *S) TestProvisionerTriggerCron(c *check.C) {
 					},
 					Spec: batchv1.JobSpec{
 						TTLSecondsAfterFinished: func() *int32 { defaultTTL := int32(86400); return &defaultTTL }(),
+						ActiveDeadlineSeconds:   func() *int64 { r := int64(60 * 60); return &r }(),
 						Template: corev1.PodTemplateSpec{
 							ObjectMeta: v1.ObjectMeta{
 								Labels: map[string]string{
