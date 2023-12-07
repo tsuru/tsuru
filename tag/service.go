b0549afd21134a9c0e35d71160aff43db7b0be51
Wilson Júnior <wilsonpjunior@gmail.com>
2023-12-07 16:42:42 -0300

    Add support to call tag service via GRPC

--- tag/service.go
+++ tag/service.go
@@ -4,9 +4,22 @@
 
 package tag
 
-import tagTypes "github.com/tsuru/tsuru/types/tag"
+import (
+	"github.com/tsuru/config"
+	tagTypes "github.com/tsuru/tsuru/types/tag"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
 
 func TagService() (tagTypes.TagServiceClient, error) {
-	// TODO: check conf and initialize correct client
+	tagServiceAddr, _ := config.GetString("tag:service-addr")
+	if tagServiceAddr != "" {
+		conn, err := grpc.Dial(tagServiceAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			return nil, err
+		}
+
+		return tagTypes.NewTagServiceClient(conn), nil
+	}
 	return &noopTagClient{}, nil
 }
