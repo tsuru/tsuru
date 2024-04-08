bacb5944f8ba4adc3ff397ab50445015ea51d130
crgisch <claudio.gisch@hotmail.com>
2024-04-08 12:34:19 -0300

    test: keda prometheus autoscaler

--- provision/provision_test.go
+++ provision/provision_test.go
@@ -160,6 +160,13 @@ func (ProvisionSuite) TestValidate(c *check.C) {
 			},
 			"maximum units cannot be greater than quota limit",
 		},
+		{
+			AutoScaleSpec{
+				MinUnits: 1,
+				MaxUnits: 2,
+			},
+			"you have to configure at least one trigger between cpu, schedule and prometheus",
+		},
 	}
 
 	for _, test := range tests {
