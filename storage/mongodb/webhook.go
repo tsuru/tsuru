e3661c43d26e567aafb0ef9b8d7ebe60fc5758c2
Wilson Júnior <wilsonpjunior@gmail.com>
2024-06-14 14:11:24 -0300

    Read webhooks now uses new mongodb driver

--- storage/mongodb/webhook.go
+++ storage/mongodb/webhook.go
@@ -5,19 +5,25 @@
 package mongodb
 
 import (
+	"context"
 	"strings"
 
 	mgo "github.com/globalsign/mgo"
 	"github.com/globalsign/mgo/bson"
 	"github.com/tsuru/tsuru/db"
 	dbStorage "github.com/tsuru/tsuru/db/storage"
+	"github.com/tsuru/tsuru/db/storagev2"
 	"github.com/tsuru/tsuru/types/event"
+	mongoBSON "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+const webhookCollectionName = "webhook"
 
 type webhookStorage struct{}
 
 func webhookCollection(conn *db.Storage) *dbStorage.Collection {
-	coll := conn.Collection("webhook")
+	coll := conn.Collection(webhookCollectionName)
 	coll.EnsureIndex(mgo.Index{
 		Key:    []string{"name"},
 		Unique: true,
@@ -53,26 +59,34 @@ func (s *webhookStorage) Update(w event.Webhook) error {
 	return err
 }
 
-func (s *webhookStorage) findQuery(query bson.M) ([]event.Webhook, error) {
-	conn, err := db.Conn()
+func (s *webhookStorage) findQuery(ctx context.Context, query mongoBSON.M) ([]event.Webhook, error) {
+	collection, err := storagev2.Collection(webhookCollectionName)
 	if err != nil {
 		return nil, err
 	}
-	defer conn.Close()
-	var webhooks []event.Webhook
-	err = webhookCollection(conn).Find(query).All(&webhooks)
-	return webhooks, err
-}
 
-func (s *webhookStorage) FindAllByTeams(teams []string) ([]event.Webhook, error) {
-	var query bson.M
-	if teams != nil {
-		query = bson.M{"teamowner": bson.M{"$in": teams}}
+	cursor, err := collection.Find(ctx, query)
+	if err != nil {
+		return nil, err
 	}
-	return s.findQuery(query)
+
+	var webhooks []event.Webhook
+	err = cursor.All(ctx, &webhooks)
+	if err != nil {
+		return nil, err
+	}
+	return webhooks, nil
 }
 
-func (s *webhookStorage) FindByEvent(f event.WebhookEventFilter, isSuccess bool) ([]event.Webhook, error) {
+func (s *webhookStorage) FindAllByTeams(ctx context.Context, teams []string) ([]event.Webhook, error) {
+	var query mongoBSON.M
+	if teams != nil {
+		query = mongoBSON.M{"teamowner": bson.M{"$in": teams}}
+	}
+	return s.findQuery(ctx, query)
+}
+
+func (s *webhookStorage) FindByEvent(ctx context.Context, f event.WebhookEventFilter, isSuccess bool) ([]event.Webhook, error) {
 	for _, name := range f.KindNames {
 		parts := strings.Split(name, ".")
 		parts = parts[:len(parts)-1]
@@ -81,30 +95,32 @@ func (s *webhookStorage) FindByEvent(f event.WebhookEventFilter, isSuccess bool)
 		}
 		f.KindNames = append(f.KindNames, parts...)
 	}
-	andBlock := []bson.M{
-		{"$or": []bson.M{{"eventfilter.targettypes": bson.M{"$in": f.TargetTypes}}, {"eventfilter.targettypes": []string{}}}},
-		{"$or": []bson.M{{"eventfilter.targetvalues": bson.M{"$in": f.TargetValues}}, {"eventfilter.targetvalues": []string{}}}},
-		{"$or": []bson.M{{"eventfilter.kindtypes": bson.M{"$in": f.KindTypes}}, {"eventfilter.kindtypes": []string{}}}},
-		{"$or": []bson.M{{"eventfilter.kindnames": bson.M{"$in": f.KindNames}}, {"eventfilter.kindnames": []string{}}}},
+	andBlock := []mongoBSON.M{
+		{"$or": []mongoBSON.M{{"eventfilter.targettypes": mongoBSON.M{"$in": f.TargetTypes}}, {"eventfilter.targettypes": []string{}}}},
+		{"$or": []mongoBSON.M{{"eventfilter.targetvalues": mongoBSON.M{"$in": f.TargetValues}}, {"eventfilter.targetvalues": []string{}}}},
+		{"$or": []mongoBSON.M{{"eventfilter.kindtypes": mongoBSON.M{"$in": f.KindTypes}}, {"eventfilter.kindtypes": []string{}}}},
+		{"$or": []mongoBSON.M{{"eventfilter.kindnames": mongoBSON.M{"$in": f.KindNames}}, {"eventfilter.kindnames": []string{}}}},
 	}
 	if isSuccess {
-		andBlock = append(andBlock, bson.M{"eventfilter.erroronly": false})
+		andBlock = append(andBlock, mongoBSON.M{"eventfilter.erroronly": false})
 	} else {
-		andBlock = append(andBlock, bson.M{"eventfilter.successonly": false})
+		andBlock = append(andBlock, mongoBSON.M{"eventfilter.successonly": false})
 	}
-	return s.findQuery(bson.M{"$and": andBlock})
+	return s.findQuery(ctx, mongoBSON.M{"$and": andBlock})
 }
 
-func (s *webhookStorage) FindByName(name string) (*event.Webhook, error) {
-	conn, err := db.Conn()
+func (s *webhookStorage) FindByName(ctx context.Context, name string) (*event.Webhook, error) {
+	collection, err := storagev2.Collection(webhookCollectionName)
 	if err != nil {
 		return nil, err
 	}
-	defer conn.Close()
+
 	var result event.Webhook
-	err = webhookCollection(conn).Find(bson.M{"name": name}).One(&result)
+
+	err = collection.FindOne(ctx, mongoBSON.M{"name": name}).Decode(&result)
+
 	if err != nil {
-		if err == mgo.ErrNotFound {
+		if err == mongo.ErrNoDocuments {
 			err = event.ErrWebhookNotFound
 		}
 		return nil, err
