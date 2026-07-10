51e145dd84f1b310aaebdf59058a4ca0e3081608
Wilson Júnior <wilsonpjunior@gmail.com>
2026-07-10 14:52:12 -0300

    Drop unused method

--- auth/team_token.go
+++ auth/team_token.go
@@ -62,10 +62,6 @@ func (t *teamToken) Permissions(ctx context.Context) ([]permTypes.Permission, er
 	return expandRolePermissions(ctx, t.Roles)
 }
 
-func (t *teamToken) DynamicPermissions(ctx context.Context) ([]permTypes.Permission, error) {
-	return expandDynamicRolePermissions(ctx, t.Roles)
-}
-
 type teamTokenService struct {
 	storage authTypes.TeamTokenStorage
 }
