4514d700715b03dd8d12e0fac4558e20c3146686
ravilock <ravi.me.professional@hotmail.com>
2026-08-03 19:22:48 -0300

    test: adjust get raw app info with curl to see healthcheck data

--- integration/install_test.go
+++ integration/install_test.go
@@ -5,6 +5,7 @@
 package integration
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/fs"
 	"os"
@@ -497,8 +498,8 @@ func testApps() ExecFlow {
 
 		switch env.Get("case") {
 		case "multi-health":
-			appInfo := getAppInfo(c, appName, env)
-			assertTestAppCheckConfigs(c, appInfo, false)
+			appInfo := getRawAppInfo(c, appName, env)
+			assertAppTsuruHealthcheckConfigs(c, appInfo, false)
 			c.Assert(appInfo.InternalAddresses, check.HasLen, 2)
 			c.Assert("multi-health-ipool-kubernetes-iapp-web.default.svc.cluster.local", check.Equals, appInfo.InternalAddresses[0].Domain)
 			c.Assert("multi-health-ipool-kubernetes-iapp-web-secondary.default.svc.cluster.local", check.Equals, appInfo.InternalAddresses[1].Domain)
@@ -510,20 +511,18 @@ func testApps() ExecFlow {
 				c.Assert(internalAddress.Port, check.Equals, int32(80))
 			}
 		case "multi-startup":
-			appInfo := getAppInfo(c, appName, env)
-			assertTestAppCheckConfigs(c, appInfo, true)
+			appInfo := getRawAppInfo(c, appName, env)
+			assertAppTsuruHealthcheckConfigs(c, appInfo, true)
 
 			res = T("app", "stop", "-a", appName).Run(env)
 			c.Assert(res, ResultOk)
 
-			var stoppedAppInfo *appTypes.AppInfo
 			ok = retry(3*time.Minute, func() bool {
-				var stopped bool
-				stoppedAppInfo, stopped = checkAppStopped(c, appName, env)
+				_, stopped := checkAppStopped(c, appName, env)
 				return stopped
 			})
 			c.Assert(ok, check.Equals, true, check.Commentf("app not stopped after 3 minutes: %v", res))
-			assertTestAppCheckConfigs(c, stoppedAppInfo, true)
+			assertAppTsuruHealthcheckConfigs(c, getRawAppInfo(c, appName, env), true)
 
 			res = T("app", "start", "-a", appName).Run(env)
 			c.Assert(res, ResultOk)
@@ -542,7 +541,30 @@ func testApps() ExecFlow {
 	return flow
 }
 
-func assertTestAppCheckConfigs(c *check.C, appInfo *appTypes.AppInfo, withStartupcheck bool) {
+func getRawAppInfo(c *check.C, appName string, env *Environment) *appTypes.AppInfo {
+	// Keep wire-contract assertions independent from the installed client's AppInfo schema.
+	if env.Get("apitoken") == "" {
+		res := T("token", "show").Run(env)
+		c.Assert(res, ResultOk)
+		env.Set("apitoken", parseAPIToken(c, res.Stdout.String()))
+	}
+
+	res := NewCommand(
+		"curl",
+		"-sSf",
+		fmt.Sprintf("{{.targetaddr}}/1.0/apps/%s", appName),
+		"-H",
+		"'Authorization: Bearer {{.apitoken}}'",
+	).Run(env)
+	c.Assert(res, ResultOk)
+
+	appInfo := new(appTypes.AppInfo)
+	err := json.NewDecoder(&res.Stdout).Decode(appInfo)
+	c.Assert(err, check.IsNil)
+	return appInfo
+}
+
+func assertAppTsuruHealthcheckConfigs(c *check.C, appInfo *appTypes.AppInfo, withStartupcheck bool) {
 	healthcheck := func() *provTypes.TsuruYamlHealthcheck {
 		return &provTypes.TsuruYamlHealthcheck{
 			Path:            "/",
